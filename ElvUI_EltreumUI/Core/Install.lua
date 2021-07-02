2f82d9ff0edea33f3d001563ef7abe07b57aafc8
Eltreum <eltreum2048@gmail.com>
2021-07-02 20:31:58 -0300

    remove gap here

--- ElvUI_EltreumUI/Core/Install.lua
+++ ElvUI_EltreumUI/Core/Install.lua
@@ -1,8 +1,6 @@
 local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 local _G = _G
 local ReloadUI = _G.ReloadUI
-
-
 local IsAddOnLoaded = _G.IsAddOnLoaded
 
 -- Set version & reload on "Skip" and "Finished"
