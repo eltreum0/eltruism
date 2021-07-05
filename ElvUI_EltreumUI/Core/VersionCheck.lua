547253e1859a7e22fa90d634045a079de9c8a9d9
Eltreum <eltreum2048@gmail.com>
2021-07-04 21:00:46 -0300

    v2.4.1

--- ElvUI_EltreumUI/Core/VersionCheck.lua
+++ ElvUI_EltreumUI/Core/VersionCheck.lua
@@ -19,7 +19,7 @@ end
 
 function ElvUI_EltreumUI:ElvUIVersionCheck()
 	if ElvUI_EltreumUI.Retail then
-		if E.version < 12.32 then
+		if E.version < 12.33 then
 			ElvUI_EltreumUI:Print("Your ElvUI version is out of date, please update to avoid issues!")
 		end
 	end
