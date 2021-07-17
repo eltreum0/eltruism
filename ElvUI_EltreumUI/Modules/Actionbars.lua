a836b8cbd8aef3181696a3124b6b77ca016644f5
Eltreum <eltreum2048@gmail.com>
2021-07-17 04:12:53 -0300

    pet autocast glow

--- ElvUI_EltreumUI/Modules/Actionbars.lua
+++ ElvUI_EltreumUI/Modules/Actionbars.lua
@@ -3,7 +3,6 @@ local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 -- Skill Glow
 local LCG = LibStub('LibCustomGlow-1.0')
 function ElvUI_EltreumUI:SkillGlow()
-
 	local skillglowcolor
 	if not E.db.ElvUI_EltreumUI.glow.colorclass then
 		local glowcustomcolor = E.db.ElvUI_EltreumUI.glowcustomcolor
@@ -54,3 +53,49 @@ function ElvUI_EltreumUI:SkillGlow()
 		end
 	end
 end
+
+
+local AB = E:GetModule('ActionBars')
+function ElvUI_EltreumUI:UpdatePet(event, unit)
+	local skillglowcolor
+	if not E.db.ElvUI_EltreumUI.glow.colorclass then
+		local glowcustomcolor = E.db.ElvUI_EltreumUI.glowcustomcolor
+		local r, g, b = glowcustomcolor.r, glowcustomcolor.g, glowcustomcolor.b
+		skillglowcolor = {r, g, b, 1}
+	end
+	if E.db.ElvUI_EltreumUI.glow.colorclass then
+		local r, g, b = unpack(E.media.rgbvaluecolor)
+		skillglowcolor = {r, g, b, 1}
+	end
+	for i = 1, NUM_PET_ACTION_SLOTS, 1 do
+		local _, _, _, _, _, autoCastEnabled, _ = GetPetActionInfo(i)
+		local buttonName = 'PetActionButton'..i
+		local button = _G[buttonName]
+
+		if autoCastEnabled then
+			AutoCastShine_AutoCastStop(button.AutoCastShine)
+			--if E.db.ElvUI_EltreumUI.glow.pixel then
+				--PixelGlow_Start(frame[, color[, N[, frequency[, length[, th[, xOffset[, yOffset[, border[ ,key]]]]]]]])
+				LCG.PixelGlow_Start(button, skillglowcolor, 10, 0.25, 5, 2, 0, 0, false, nil, high)
+			--end
+			--if E.db.ElvUI_EltreumUI.glow.autocast then
+				--LCG.AutoCastGlow_Start(button, skillglowcolor, 4, 0.4, 1, 0, 0)
+			--end
+			--if E.db.ElvUI_EltreumUI.glow.blizzard then
+			--	LCG.ButtonGlow_Start(button, skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
+			--end
+		else
+			AutoCastShine_AutoCastStop(button.AutoCastShine)
+			--if E.db.ElvUI_EltreumUI.glow.pixel then
+				LCG.PixelGlow_Stop(button)
+			--end
+			--if E.db.ElvUI_EltreumUI.glow.autocast then
+				--LCG.AutoCastGlow_Stop(button)
+			--end
+			--if E.db.ElvUI_EltreumUI.glow.blizzard then
+			--	LCG.ButtonGlow_Stop(button)
+			--end
+		end
+	end
+end
+hooksecurefunc(AB, "UpdatePet", ElvUI_EltreumUI.UpdatePet)
