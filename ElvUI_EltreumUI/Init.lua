1b7a9972cbbada0d391cf6f9934e8adc09b596f8
Eltreum <eltreum2048@gmail.com>
2022-01-22 18:05:05 -0300

    Update Init.lua

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -59,7 +59,6 @@ function ElvUI_EltreumUI:PLAYER_ENTERING_WORLD()
 	ElvUI_EltreumUI:DynamicBuffs() --shows enemy player buffs on nameplates/unitframes if in arena/bgs, hides otherwise
 	ElvUI_EltreumUI:EnteringWorldCVars() --set cvars at the start
 	ElvUI_EltreumUI:GroupRoster() -- attempt at prevent CLEU tracking
-
 	if ElvUI_EltreumUI.Retail then
 		ElvUI_EltreumUI:WaypointTimeToArrive() --adds an ETA below waypoints
 		ElvUI_EltreumUI:SkillGlow() --makes skill glow using libcustomglow
