2d0757d44b65742aa412a74c7b42cae01176fd64
Eltreum <eltreum2048@gmail.com>
2022-01-22 06:20:15 -0300

    oops, forgot to remove print

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -85,7 +85,7 @@ function ElvUI_EltreumUI:PLAYER_ENTERING_WORLD()
 		ElvUI_EltreumUI:DevTools()
 	end
 	--a warning in case people are using the dev version
-	ElvUI_EltreumUI:Print("|cFFFF0000WARNING:|r You are using Eltruism DEV. This is a version not ready for release which can have incomplete features or issues. |cFFFF0000Please report them on discord if you encounter any.|r")
+	--ElvUI_EltreumUI:Print("|cFFFF0000WARNING:|r You are using Eltruism DEV. This is a version not ready for release which can have incomplete features or issues. |cFFFF0000Please report them on discord if you encounter any.|r")
 end
 
 function ElvUI_EltreumUI:HidePopups()
