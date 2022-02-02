87db06ea57f80c13fe020987d124c9947b70ae95
Eltreum <eltreum2048@gmail.com>
2022-02-02 19:32:29 -0300

    turns out ZONE_CHANGED_NEW_AREA returns wrong results for IsInInstance() if its run too fast

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -250,7 +250,7 @@ function ElvUI_EltreumUI:ZONE_CHANGED_INDOORS()
 end
 
 function ElvUI_EltreumUI:ZONE_CHANGED_NEW_AREA()
-	ElvUI_EltreumUI:FriendlyNameplates()
+	C_Timer.After(5, function() ElvUI_EltreumUI:FriendlyNameplates() end)
 	ElvUI_EltreumUI:QuestItem()
 end
 
