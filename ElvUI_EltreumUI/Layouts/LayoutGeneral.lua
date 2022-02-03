cd10177c930e3aeca14ce637245e83ea2d31843b
Eltreum <eltreum2048@gmail.com>
2022-02-03 09:04:21 -0300

    split the eltruism settings from layout into its own function/file

--- ElvUI_EltreumUI/Layouts/LayoutGeneral.lua
+++ ElvUI_EltreumUI/Layouts/LayoutGeneral.lua
@@ -19,6 +19,12 @@ function ElvUI_EltreumUI:SetupGeneralLayout()
 		--Setup CVars
 		ElvUI_EltreumUI:SetupCVars()
 
+		--Set the Profile and Private Settings
+		ElvUI_EltreumUI:UpdateSettings()
+
+		--mark as dark mode
+		--E.db.ElvUI_EltreumUI.lightmode = false
+
 		--SetupShadowAndLight
 		if IsAddOnLoaded("ElvUI_SLE") then
 			ElvUI_EltreumUI:GetSLEProfile()
@@ -37,86 +43,8 @@ function ElvUI_EltreumUI:SetupGeneralLayout()
 			ElvUI_EltreumUI:GetWindToolsProfile()
 		end
 
-		--set settings that would be true
-		E.db.ElvUI_EltreumUI.nameplatepower.autoadjustposition = true
-		E.db.ElvUI_EltreumUI.chat.enable = true
-		E.db.ElvUI_EltreumUI.chat.leftcombathide = true
-		E.db.ElvUI_EltreumUI.chat.colorsysmsg = true
-		E.db.ElvUI_EltreumUI.chat.looticons = true
-		E.db.ElvUI_EltreumUI.cvars.autohidenpcname = true
-		E.db.ElvUI_EltreumUI.skins.enable = true
-		E.db.ElvUI_EltreumUI.skins.widertransmog = true
-		E.db.ElvUI_EltreumUI.skins.expandedstable = true
-		E.db.ElvUI_EltreumUI.skins.classiconsoncharacterpanel = true
-		E.db.ElvUI_EltreumUI.skins.classiconsreleaf = true
-		E.db.ElvUI_EltreumUI.skins.classicarmory = true
-		E.db.ElvUI_EltreumUI.skins.classicarmoryautostats = true
-		E.db.ElvUI_EltreumUI.skins.ilvls = true
-		E.db.ElvUI_EltreumUI.skins.sockets = true
-		E.db.ElvUI_EltreumUI.skins.expandarmorybg = true
-		E.db.ElvUI_EltreumUI.skins.bossemote = true
-		E.db.ElvUI_EltreumUI.skins.quests = true
-		E.db.ElvUI_EltreumUI.skins.zones = true
-		E.db.ElvUI_EltreumUI.skins.shadows = true
-		E.db.ElvUI_EltreumUI.shadows.aura = true
-		E.db.ElvUI_EltreumUI.shadows.ufaura = true
-		E.db.ElvUI_EltreumUI.shadows.actionbars = true
-		E.db.ElvUI_EltreumUI.tbctalents.enable = true
-		E.db.ElvUI_EltreumUI.dynamicdatatext.enable = true
-		E.db.ElvUI_EltreumUI.nameplatelevel.enable = true
-		E.db.ElvUI_EltreumUI.cursor.enable = true
-		E.db.ElvUI_EltreumUI.cursor.cooldown = true
-		E.db.ElvUI_EltreumUI.cursor.combat = true
-		E.db.ElvUI_EltreumUI.nameplateOptions.ClassColorGlow = true
-		E.db.ElvUI_EltreumUI.nameplateOptions.ClassBorderNameplate = true
-		E.db.ElvUI_EltreumUI.nameplateOptions.nameplatetexture = true
-		E.db.ElvUI_EltreumUI.nameplateOptions.classbarautohide = true
-		E.db.ElvUI_EltreumUI.stealthOptions.stealtheffect = true
-		E.db.ElvUI_EltreumUI.glow.enable = true
-		E.db.ElvUI_EltreumUI.glow.pixel = true
-		E.db.ElvUI_EltreumUI.glow.enablepet = true
-		E.db.ElvUI_EltreumUI.partyraiddeath.enable = true
-		E.db.ElvUI_EltreumUI.friendlynameplatetoggle.friendlynames = true
-		E.db.ElvUI_EltreumUI.afkmusic.enable = true
-		E.db.ElvUI_EltreumUI.afkmusic.racial = true
-		E.db.ElvUI_EltreumUI.waypointetasetting.enable = true
-		E.db.ElvUI_EltreumUI.waypointetasetting.autopin = true
-		E.db.ElvUI_EltreumUI.waytext.enable = true
-		E.db.ElvUI_EltreumUI.widenameplate.enable = true
-		E.db.ElvUI_EltreumUI.widenameplate.npglow = true
-		if ElvUI_EltreumUI.Retail then
-			E.db.ElvUI_EltreumUI.questsettings.enable = true
-			E.db.ElvUI_EltreumUI.questsettings.arena = true
-		elseif ElvUI_EltreumUI.TBC or ElvUI_EltreumUI.Classic then
-			E.db.ElvUI_EltreumUI.questsettings.enable = false
-			E.db.ElvUI_EltreumUI.questsettings.arena = false
-		end
-		E.db.ElvUI_EltreumUI.questsettings.rogueopen = true
-		E.db.ElvUI_EltreumUI.questsettings.questitemsbar1 = true
-		E.db.ElvUI_EltreumUI.loottext.enable = true
-		E.db.ElvUI_EltreumUI.loottext.fontsetting = true
-		E.db.ElvUI_EltreumUI.loottext.combatindicator = true
-		E.db.ElvUI_EltreumUI.modetexture = true
-		E.db.ElvUI_EltreumUI.sparkcustomcolor.enable = true
-		E.db.ElvUI_EltreumUI.otherstuff.fastloot = true
-		E.db.ElvUI_EltreumUI.otherstuff.screenshot = true
-		E.db.ElvUI_EltreumUI.otherstuff.hidetalkinghead = true
-		E.db.ElvUI_EltreumUI.otherstuff.blizzcombattext = true
-		E.db.ElvUI_EltreumUI.otherstuff.dynamicxpbar = true
-		--private
-		E.private.ElvUI_EltreumUI.nameplatepower.enable = true
-		E.private.ElvUI_EltreumUI.nameplatepower.adjust = true
-		E.private.ElvUI_EltreumUI.combatmusic.enable = true
-		E.private.ElvUI_EltreumUI.combatmusic.bossmusic = true
-		E.private.ElvUI_EltreumUI.combatmusic.disableinstance = true
-
-
-
 		-- AB Conversion
 		E.db["convertPages"] = true
-
-		--mark as dark mode
-		E.db.ElvUI_EltreumUI.lightmode = false
 
 		-- General
 		E.db["general"]["altPowerBar"]["font"] = "Kimberley"
---
+++ ElvUI_EltreumUI/Layouts/UpdateSettings.lua
@@ -0,0 +1,82 @@
+local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
+local _G = _G
+local IsAddOnLoaded = _G.IsAddOnLoaded
+local valuecolors = E:ClassColor(E.myclass, true)
+local myclass = E.myclass
+
+--Old settings ettings that would be true but became false per request, now a function that can be used
+function ElvUI_EltreumUI:UpdateSettings()
+	E.db.ElvUI_EltreumUI.nameplatepower.autoadjustposition = true
+	E.db.ElvUI_EltreumUI.chat.enable = true
+	E.db.ElvUI_EltreumUI.chat.leftcombathide = true
+	E.db.ElvUI_EltreumUI.chat.colorsysmsg = true
+	E.db.ElvUI_EltreumUI.chat.looticons = true
+	E.db.ElvUI_EltreumUI.cvars.autohidenpcname = true
+	E.db.ElvUI_EltreumUI.skins.enable = true
+	E.db.ElvUI_EltreumUI.skins.widertransmog = true
+	E.db.ElvUI_EltreumUI.skins.expandedstable = true
+	E.db.ElvUI_EltreumUI.skins.classiconsoncharacterpanel = true
+	E.db.ElvUI_EltreumUI.skins.classiconsreleaf = true
+	E.db.ElvUI_EltreumUI.skins.classicarmory = true
+	E.db.ElvUI_EltreumUI.skins.classicarmoryautostats = true
+	E.db.ElvUI_EltreumUI.skins.ilvls = true
+	E.db.ElvUI_EltreumUI.skins.sockets = true
+	E.db.ElvUI_EltreumUI.skins.expandarmorybg = true
+	E.db.ElvUI_EltreumUI.skins.bossemote = true
+	E.db.ElvUI_EltreumUI.skins.quests = true
+	E.db.ElvUI_EltreumUI.skins.zones = true
+	E.db.ElvUI_EltreumUI.skins.shadows = true
+	E.db.ElvUI_EltreumUI.shadows.aura = true
+	E.db.ElvUI_EltreumUI.shadows.ufaura = true
+	E.db.ElvUI_EltreumUI.shadows.actionbars = true
+	E.db.ElvUI_EltreumUI.tbctalents.enable = true
+	E.db.ElvUI_EltreumUI.dynamicdatatext.enable = true
+	E.db.ElvUI_EltreumUI.nameplatelevel.enable = true
+	E.db.ElvUI_EltreumUI.cursor.enable = true
+	E.db.ElvUI_EltreumUI.cursor.cooldown = true
+	E.db.ElvUI_EltreumUI.cursor.combat = true
+	E.db.ElvUI_EltreumUI.nameplateOptions.ClassColorGlow = true
+	E.db.ElvUI_EltreumUI.nameplateOptions.ClassBorderNameplate = true
+	E.db.ElvUI_EltreumUI.nameplateOptions.nameplatetexture = true
+	E.db.ElvUI_EltreumUI.nameplateOptions.classbarautohide = true
+	E.db.ElvUI_EltreumUI.stealthOptions.stealtheffect = true
+	E.db.ElvUI_EltreumUI.glow.enable = true
+	E.db.ElvUI_EltreumUI.glow.pixel = true
+	E.db.ElvUI_EltreumUI.glow.enablepet = true
+	E.db.ElvUI_EltreumUI.partyraiddeath.enable = true
+	E.db.ElvUI_EltreumUI.friendlynameplatetoggle.friendlynames = true
+	E.db.ElvUI_EltreumUI.afkmusic.enable = true
+	E.db.ElvUI_EltreumUI.afkmusic.racial = true
+	E.db.ElvUI_EltreumUI.waypointetasetting.enable = true
+	E.db.ElvUI_EltreumUI.waypointetasetting.autopin = true
+	E.db.ElvUI_EltreumUI.waytext.enable = true
+	E.db.ElvUI_EltreumUI.widenameplate.enable = true
+	E.db.ElvUI_EltreumUI.widenameplate.npglow = true
+	if ElvUI_EltreumUI.Retail then
+		E.db.ElvUI_EltreumUI.questsettings.enable = true
+		E.db.ElvUI_EltreumUI.questsettings.arena = true
+	elseif ElvUI_EltreumUI.TBC or ElvUI_EltreumUI.Classic then
+		E.db.ElvUI_EltreumUI.questsettings.enable = false
+		E.db.ElvUI_EltreumUI.questsettings.arena = false
+	end
+	E.db.ElvUI_EltreumUI.questsettings.rogueopen = true
+	E.db.ElvUI_EltreumUI.questsettings.questitemsbar1 = true
+	E.db.ElvUI_EltreumUI.loottext.enable = true
+	E.db.ElvUI_EltreumUI.loottext.fontsetting = true
+	E.db.ElvUI_EltreumUI.loottext.combatindicator = true
+	E.db.ElvUI_EltreumUI.modetexture = true
+	E.db.ElvUI_EltreumUI.sparkcustomcolor.enable = true
+	E.db.ElvUI_EltreumUI.otherstuff.fastloot = true
+	E.db.ElvUI_EltreumUI.otherstuff.screenshot = true
+	E.db.ElvUI_EltreumUI.otherstuff.hidetalkinghead = true
+	E.db.ElvUI_EltreumUI.otherstuff.blizzcombattext = true
+	E.db.ElvUI_EltreumUI.otherstuff.dynamicxpbar = true
+	--private
+	E.private.ElvUI_EltreumUI.nameplatepower.enable = true
+	E.private.ElvUI_EltreumUI.nameplatepower.adjust = true
+	E.private.ElvUI_EltreumUI.combatmusic.enable = true
+	E.private.ElvUI_EltreumUI.combatmusic.bossmusic = true
+	E.private.ElvUI_EltreumUI.combatmusic.disableinstance = true
+
+	ElvUI_EltreumUI:Print(L["Eltruism settings were applied."])
+end
