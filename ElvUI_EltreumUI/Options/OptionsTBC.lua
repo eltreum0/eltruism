508b2fdf4cb3b0c8de49328a2f99c76dbbac5f28
Eltreum <eltreum2048@gmail.com>
2022-02-05 12:20:38 -0300

    reorder

--- ElvUI_EltreumUI/Options/OptionsTBC.lua
+++ ElvUI_EltreumUI/Options/OptionsTBC.lua
@@ -3303,42 +3303,6 @@ if ElvUI_EltreumUI.TBC then
 					name = L["CVars"],
 					icon = 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Icons\\cvar',
 					args = {
-						header1 = {
-							order = 1,
-							type = "description",
-							name = "",
-							width = 'full',
-							image = function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end,
-						},
-						setup = {
-							order = 2,
-							type = 'group',
-							inline = true,
-							name = L["Setup CVars"],
-							args = {
-								generalVars = {
-									order = 1,
-									type = 'execute',
-									name = L["General CVars"],
-									func = function() ElvUI_EltreumUI:SetupCVars() ElvUI_EltreumUI:NameplateCVars() end,
-									confirm = true,
-								},
-							},
-						},
-						generalDesc = {
-							order = 3,
-							type = 'group',
-							inline = true,
-							name = L["It will set these CVars:"],
-							args = {
-								cvarslist = {
-									order = 1,
-									type = 'description',
-									fontSize = 'medium',
-									name = '- autoLootDefault 1\n- ShowClassColorInFriendlyNameplate 1\n- removeChatDelay 1\n- autoQuestWatch 1\n- cameraDistanceMaxZoomFactor 2.6\n- nameplateLargerScale 1.2\n- nameplateMaxDistance 60\n- nameplateMinAlpha 1\n- nameplateMinScale 1\n- nameplateMotion 1\n- nameplateOccludedAlphaMult 0\n- nameplateOverlapH 0.8\n- nameplateOverlapV 1.1\n- nameplateSelectedScale 1\n- nameplateSelfAlpha 1\n- nameplateShowFriendlyMinions 0\n- nameplateOtherBottomInset 0.02\n- nameplateOtherTopInset 0.1\n- nameplateTargetRadialPosition 1\n- nameplateTargetBehindMaxDistance 40\n- UnitNameEnemyGuardianName 0\n- UnitNameEnemyMinionName 0\n- UnitNameEnemyPetName 0\n- UnitNameEnemyPlayerName 1\n- UnitNameEnemyTotemName 1\n- UnitNameNPC 1',
-								},
-							},
-						},
 						header2 = {
 							order = 4,
 							type = "description",
@@ -3395,6 +3359,42 @@ if ElvUI_EltreumUI.TBC then
 							width = "full",
 							get = function() return E.db.ElvUI_EltreumUI.cvars.cameraDistanceMaxZoomFactor end,
 							set = function(_, value) E.db.ElvUI_EltreumUI.cvars.cameraDistanceMaxZoomFactor = value SetCVar('cameraDistanceMaxZoomFactor', value) end,
+						},
+						header1 = {
+							order = 6,
+							type = "description",
+							name = "",
+							width = 'full',
+							image = function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end,
+						},
+						setup = {
+							order = 7,
+							type = 'group',
+							inline = true,
+							name = L["Setup CVars"],
+							args = {
+								generalVars = {
+									order = 1,
+									type = 'execute',
+									name = L["General CVars"],
+									func = function() ElvUI_EltreumUI:SetupCVars() ElvUI_EltreumUI:NameplateCVars() end,
+									confirm = true,
+								},
+							},
+						},
+						generalDesc = {
+							order = 8,
+							type = 'group',
+							inline = true,
+							name = L["It will set these CVars:"],
+							args = {
+								cvarslist = {
+									order = 1,
+									type = 'description',
+									fontSize = 'medium',
+									name = '- autoLootDefault 1\n- ShowClassColorInFriendlyNameplate 1\n- removeChatDelay 1\n- autoQuestWatch 1\n- cameraDistanceMaxZoomFactor 2.6\n- nameplateLargerScale 1.2\n- nameplateMaxDistance 60\n- nameplateMinAlpha 1\n- nameplateMinScale 1\n- nameplateMotion 1\n- nameplateOccludedAlphaMult 0\n- nameplateOverlapH 0.8\n- nameplateOverlapV 1.1\n- nameplateSelectedScale 1\n- nameplateSelfAlpha 1\n- nameplateShowFriendlyMinions 0\n- nameplateOtherBottomInset 0.02\n- nameplateOtherTopInset 0.1\n- nameplateTargetRadialPosition 1\n- nameplateTargetBehindMaxDistance 40\n- UnitNameEnemyGuardianName 0\n- UnitNameEnemyMinionName 0\n- UnitNameEnemyPetName 0\n- UnitNameEnemyPlayerName 1\n- UnitNameEnemyTotemName 1\n- UnitNameNPC 1',
+								},
+							},
 						},
 					},
 				},
