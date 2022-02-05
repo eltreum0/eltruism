ade806288c77347853fe45a175b5ffda12343ac4
Eltreum <eltreum2048@gmail.com>
2022-02-05 13:27:15 -0300

    Update DynamicCam.lua

--- ElvUI_EltreumUI/Profiles/DynamicCam.lua
+++ ElvUI_EltreumUI/Profiles/DynamicCam.lua
@@ -382,6 +382,9 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 						["situations"] = {
 							["303"] = {
 							},
+							["103"] = {
+								["enabled"] = false,
+							},
 							["033"] = {
 								["enabled"] = false,
 							},
@@ -394,19 +397,24 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 							["302"] = {
 								["enabled"] = false,
 							},
-							["060"] = {
+							["005"] = {
+								["cameraActions"] = {
+									["zoomSetting"] = "in",
+									["timeIsMax"] = false,
+									["transitionTime"] = 1.5,
+									["zoomValue"] = 14,
+								},
 								["cameraCVars"] = {
-									["test_cameraDynamicPitch"] = 0,
 									["test_cameraOverShoulder"] = 0,
 								},
 							},
-							["031"] = {
+							["034"] = {
 								["enabled"] = false,
 							},
 							["002"] = {
 								["cameraActions"] = {
-									["timeIsMax"] = false,
 									["zoomSetting"] = "in",
+									["timeIsMax"] = false,
 									["transitionTime"] = 1.05,
 									["zoomValue"] = 15,
 								},
@@ -426,13 +434,13 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 								["executeOnInit"] = "this.spells = {\n  556,     -- Astral Recall\n  3561,    -- Teleport: Stormwind\n  3562,    -- Teleport: Ironforge\n  3563,    -- Teleport: Undercity\n  3565,    -- Teleport: Darnassus\n  3566,    -- Teleport: Thunder Bluff\n  3567,    -- Teleport: Orgrimmar\n  8690,    -- Hearthstone\n  32271,   -- Teleport: Exodar\n  32272,   -- Teleport: Silvermoon\n  33690,   -- Teleport: Shattrath\n  35715,   -- Teleport: Shattrath\n  49358,   -- Teleport: Stonard\n  49359,   -- Teleport: Theramore\n  49844,   -- Using Direbrew's Remote\n  50977,   -- Death Gate\n  53140,   -- Teleport: Dalaran - Northrend\n  54406,   -- Teleport: Dalaran\n  75136,   -- Ethereal Portal\n  88342,   -- Teleport: Tol Barad\n  88344,   -- Teleport: Tol Barad\n  94719,   -- The Innkeeper's Daughter\n  120145,  -- Ancient Teleport: Dalaran\n  132621,  -- Teleport: Vale of Eternal Blossoms\n  132627,  -- Teleport: Vale of Eternal Blossoms\n  136508,  -- Dark Portal\n  140295,  -- Kirin Tor Beacon\n  168487,  -- Home Away from Home\n  168499,  -- Home Away from Home\n  171253,  -- Garrison Hearthstone\n  176242,  -- Teleport: Warspear\n  176248,  -- Teleport: Stormshield\n  189838,  -- Teleport to Shipyard\n  192084,  -- Jump to Skyhold\n  192085,  -- Jump to Skyhold\n  193669,  -- Basic Dimensional Rifting\n  193753,  -- Dreamwalk\n  193759,  -- Teleport: Hall of the Guardian\n  196079,  -- Recall (to the sanctuary of Frostwolf Keep)\n  196080,  -- Recall (to the sanctuary of Dun Baldar)\n  216016,  -- Jump to Skyhold\n  222695,  -- Dalaran Hearthstone\n  223805,  -- Advanced Dimensional Rifting\n  224869,  -- Teleport: Dalaran - Broken Isles\n  225428,  -- Town Portal: Shala'nir\n  225434,  -- Town Portal: Sashj'tar\n  225435,  -- Town Portal: Kal'delar\n  225440,  -- Town Portal: Lian'tril\n  225436,  -- Town Portal: Faronaar\n  227334,  -- Flight Master's Whistle\n  231504,  -- [Tome of] Town Portal\n  231505,  -- [Scroll of] Town Portal\n  248906,  -- Vindicaar Teleport Beacon\n  262100,  -- Recall (to your Great Hall)\n  278244,  -- Greatfather Winter's Hearthstone\n  278559,  -- Headless Horseman's Hearthstone\n  281403,  -- Teleport: Boralus\n  281404,  -- Teleport: Dazar'alor\n  285362,  -- Lunar Elder's Hearthstone\n  285424,  -- Peddlefeet's Lovely Hearthstone\n  286031,  -- Noble Gardener's Hearthstone\n  286331,  -- Fire Eater's Hearthstone\n  286353,  -- Brewfest Reveler's Hearthstone\n  298068,  -- Holographic Digitalization Hearthstone\n  308742,  -- Eternal Traveler's Hearthstone\n  312372,  -- Return to Camp\n  326064,  -- Night Fae Hearthstone\n  335671,  -- Scroll of Teleport: Theater of Pain\n  340200,  -- Necrolord Hearthstone\n  126892,  -- Zen Pilgrimage\n  340767,  -- Chromie's Teleportation Scroll  \n  344587,  -- Teleport: Oribos\n  \n}",
 								["cameraActions"] = {
 									["pitchDegrees"] = 15,
-									["zoomSetting"] = "set",
-									["transitionTime"] = 1,
 									["zoomValue"] = 4,
-									["yawDegrees"] = 270,
-									["rotate"] = true,
-									["rotateSetting"] = "degrees",
+									["transitionTime"] = 1,
 									["timeIsMax"] = false,
+									["zoomSetting"] = "set",
+									["yawDegrees"] = 270,
+									["rotateSetting"] = "degrees",
+									["rotate"] = true,
 								},
 								["cameraCVars"] = {
 									["test_cameraOverShoulder"] = 0,
@@ -466,24 +474,14 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 									["test_cameraTargetFocusInteractStrengthYaw"] = 1,
 								},
 							},
-							["005"] = {
-								["cameraActions"] = {
-									["timeIsMax"] = false,
-									["zoomSetting"] = "in",
-									["transitionTime"] = 1.5,
-									["zoomValue"] = 14,
-								},
+							["060"] = {
 								["cameraCVars"] = {
+									["test_cameraDynamicPitch"] = 0,
 									["test_cameraOverShoulder"] = 0,
 								},
 							},
-							["101"] = {
-								["cameraActions"] = {
-									["transitionTime"] = 0.5,
-									["rotateSpeed"] = 10,
-									["rotate"] = true,
-									["timeIsMax"] = false,
-								},
+							["031"] = {
+								["enabled"] = false,
 							},
 							["201"] = {
 								["enabled"] = false,
@@ -493,8 +491,8 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 							},
 							["001"] = {
 								["cameraActions"] = {
-									["timeIsMax"] = false,
 									["zoomSetting"] = "out",
+									["timeIsMax"] = false,
 									["transitionTime"] = 1.5,
 									["zoomValue"] = 20,
 								},
@@ -510,8 +508,8 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 							},
 							["004"] = {
 								["cameraActions"] = {
-									["timeIsMax"] = false,
 									["zoomSetting"] = "set",
+									["timeIsMax"] = false,
 									["transitionTime"] = 1.5,
 									["zoomValue"] = 15,
 								},
@@ -547,8 +545,13 @@ function ElvUI_EltreumUI:GetDynamicCamProfile()
 									["test_cameraOverShoulder"] = 0,
 								},
 							},
-							["034"] = {
-								["enabled"] = false,
+							["101"] = {
+								["cameraActions"] = {
+									["transitionTime"] = 0.5,
+									["rotateSpeed"] = 10,
+									["rotate"] = true,
+									["timeIsMax"] = false,
+								},
 							},
 							["custom1"] = {
 								["enabled"] = false,
