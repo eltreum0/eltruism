55ad2bf9f1bb986b52ac96be698873c661cf604b
Eltreum <eltreum2048@gmail.com>
2022-02-09 05:06:14 -0300

    add a check for bags being full

--- ElvUI_EltreumUI/Modules/InstantLoot.lua
+++ ElvUI_EltreumUI/Modules/InstantLoot.lua
@@ -4,10 +4,14 @@ local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 local EltruismInstantLoot = CreateFrame("Frame", "EltruismInstantLoot")
 EltruismInstantLoot:RegisterEvent("LOOT_READY")
 local function InstantLoot()
+	local openslots = 0
+	for i=0, 4 do
+		openslots = openslots + GetContainerNumFreeSlots(i)
+	end
 	if _G["TSMDestroyBtn"] and _G["TSMDestroyBtn"]:IsShown() then
-			return
+		return
 	elseif E.db.ElvUI_EltreumUI.otherstuff.fastloot then
-		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") and openslots > 0 then
 			for i = GetNumLootItems(), 1, -1 do
 				LootSlot(i)
 			end
@@ -19,29 +23,22 @@ EltruismInstantLoot:SetScript("OnEvent", InstantLoot)
 --attempt at checking for stacks and not looting if not enough slots, but was slower
 --[[local function InstantLoot()
 	if E.db.ElvUI_EltreumUI.otherstuff.fastloot then
-		local backpack = GetContainerNumFreeSlots(0)
-		local bag1 = GetContainerNumFreeSlots(1)
-		local bag2 = GetContainerNumFreeSlots(2)
-		local bag3 = GetContainerNumFreeSlots(3)
-		local bag4 = GetContainerNumFreeSlots(4)
-		local openslots = backpack+bag1+bag2+bag3+bag4
+		local openslots = 0
+		for i=0, 4 do
+			openslots = openslots + GetContainerNumFreeSlots(i)
+		end
 		if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
 			for i = GetNumLootItems(), 1, -1 do
 				local itemLink = GetLootSlotLink(i)
 				local _, _, _, _, _, _, _, itemStackCount = GetItemInfo(itemLink)
 				local itemCount = GetItemCount(itemLink)
 				print(itemStackCount,itemCount,openslots)
-				if openslots > 0 then
-					if itemCount > 0 then
-						if itemStackCount > 0 then
-							local openstackslots = itemStackCount - itemCount
-							if openstackslots > 0 then
-								LootSlot(i)
-							end
-						else
-							LootSlot(i)
-						end
+				if openslots > 0 and itemCount > 0 and itemStackCount > 0 then
+					if (itemStackCount - itemCount) > 0 then
+						LootSlot(i)
 					end
+				else
+					LootSlot(i)
 				end
 			end
 		end
