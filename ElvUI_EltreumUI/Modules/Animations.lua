7e4925182b7a6b8aff7581e65e44eefb3498f934
Eltreum <eltreum2048@gmail.com>
2022-02-15 04:37:44 -0300

    Update Animations.lua

--- ElvUI_EltreumUI/Modules/Animations.lua
+++ ElvUI_EltreumUI/Modules/Animations.lua
@@ -95,8 +95,8 @@ function ElvUI_EltreumUI.PlayerDeathAnimation()
 	end)
 
 	--run
-	--gta:RegisterEvent("PLAYER_DEAD")
-	gta:RegisterEvent("PLAYER_STARTED_MOVING")
+	gta:RegisterEvent("PLAYER_DEAD")
+	--gta:RegisterEvent("PLAYER_STARTED_MOVING")
 	gta:SetScript("OnEvent", function()
 		local x, y = UIParent:GetSize()
 		gta:SetSize(x,y)
