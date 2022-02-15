8a41d656b1b62b6855ec43fe37f40f708c6dfcfb
Eltreum <eltreum2048@gmail.com>
2022-02-15 04:35:08 -0300

    Update Init.lua

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -72,6 +72,7 @@ function ElvUI_EltreumUI:PLAYER_ENTERING_WORLD()
 	ElvUI_EltreumUI:SkinQuests() --skins quest objective frame to be class colored
 	ElvUI_EltreumUI:ExpandedCharacterStats() --attempt at improving the character panel
 	ElvUI_EltreumUI:DeathSound() -- set the party/raid death sound
+	ElvUI_EltreumUI.PlayerDeathAnimation() -- death animation
 	ElvUI_EltreumUI:ChangePlayerTexture() -- load the player texture
 	ElvUI_EltreumUI:DeleteItem() -- automatically type delete
 	ElvUI_EltreumUI:SkillGlow() --makes skill glow using libcustomglow
