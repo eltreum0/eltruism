3e0b8f2f04304fcdd017f0faf1ff024166f8f273
Eltreum <eltreum2048@gmail.com>
2022-02-20 01:09:45 -0300

    add pepsi-cyr-lat

--- ElvUI_EltreumUI/Media/SharedMedia.lua
+++ ElvUI_EltreumUI/Media/SharedMedia.lua
@@ -13,6 +13,7 @@ LSM:Register("font", "Exo2 Extra Bold", [[Interface\Addons\ElvUI_EltreumUI\Media
 LSM:Register("font", "GothamNarrow Black", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\GothamNarrowBlack.ttf]])
 LSM:Register('font', "Kimberley", [[Interface\AddOns\ElvUI_EltreumUI\Media\Fonts\Kimberley.ttf]])
 LSM:Register("font", "Roboto", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\Roboto-Bold.ttf]])
+LSM:Register("font", "Pepsi-Cyr-Lat", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\Pepsi-Cyr-Lat.ttf]])
 
 --extra media that has to be loaded anyway
 function ElvUI_EltreumUI:ExtraMedia()
@@ -66,6 +67,7 @@ function ElvUI_EltreumUI:ExtraMediaFonts()
 	LSM:Register("font", "Neuropol", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\neuropolxrg.ttf]])
 	LSM:Register("font", "OptimusPrinceps", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\OptimusPrinceps.ttf]])
 	LSM:Register("font", "Pricedown", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\pricedown.ttf]])
+	--LSM:Register("font", "Pepsi-Cyr-Lat", [[Interface\Addons\ElvUI_EltreumUI\Media\Fonts\Pepsi-Cyr-Lat.ttf]])
 end
 
 function ElvUI_EltreumUI:ExtraMediaSounds()
