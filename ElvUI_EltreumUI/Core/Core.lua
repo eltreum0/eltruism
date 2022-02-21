0e94c90ed19a241c3295e95c95ea0fab7fcf27cf
Eltreum <eltreum2048@gmail.com>
2022-02-21 16:14:54 -0300

    Update Core.lua

--- ElvUI_EltreumUI/Core/Core.lua
+++ ElvUI_EltreumUI/Core/Core.lua
@@ -77,13 +77,15 @@ function ElvUI_EltreumUI:Anchors()
 		E:CreateMover(_G.UIErrorsFrame, "MoverUIERRORS", "UI Error Frame", nil, nil, nil, "ALL,SOLO")
 		if E.db.ElvUI_EltreumUI.blizzframes.errorframe then
 			_G.UIErrorsFrame:SetFont(E.LSM:Fetch("font", E.db.general.font), E.db.ElvUI_EltreumUI.blizzframes.errorframefontsize, E.db.general.fontStyle)
+			print(E.db.general.fontStyle)
 		end
 	end
 
 	if E.Retail then
 		E:CreateMover(_G.RaidBossEmoteFrame, "MoverRaidBossEmoteFrame", "Raid/Boss Emote Frame", nil, nil, nil, "ALL,SOLO")
 		if E.db.ElvUI_EltreumUI.blizzframes.raidbossframe then
-			_G.RaidBossEmoteFrame:SetFont(E.LSM:Fetch("font", E.db.general.font), E.db.ElvUI_EltreumUI.blizzframes.raidbossframefontsize, E.db.general.fontStyle)
+			_G.RaidWarningFrameSlot1:SetFont(E.LSM:Fetch("font", E.db.general.font), E.db.ElvUI_EltreumUI.blizzframes.raidbossframefontsize, E.db.general.fontStyle)
+			--_G.RaidBossEmoteFrame:SetFont(E.LSM:Fetch("font", E.db.general.font), E.db.ElvUI_EltreumUI.blizzframes.raidbossframefontsize, E.db.general.fontStyle)
 		end
 	end
 end
