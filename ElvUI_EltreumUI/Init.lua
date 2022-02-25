dd0e4258130be2c1ef5c18b0a8705617e627a462
Eltreum <eltreum2048@gmail.com>
2022-02-25 02:49:55 -0300

    Update Init.lua

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -193,9 +193,7 @@ end
 function ElvUI_EltreumUI:GROUP_ROSTER_UPDATE()
 	ElvUI_EltreumUI:RaidDeathGroupCheck()
 	if E.db.ElvUI_EltreumUI.lightmode and E.db.ElvUI_EltreumUI.modetexture then
-		ElvUI_EltreumUI:ChangePartyTexture()
-		ElvUI_EltreumUI:ChangeRaid40Texture()
-		ElvUI_EltreumUI:ChangeRaidTexture()
+		ElvUI_EltreumUI:ChangeGroupUnitframe()
 	end
 end
 
