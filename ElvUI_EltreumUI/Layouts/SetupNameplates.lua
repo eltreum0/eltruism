59f16aa9ad47875f687e1971b2426307e353abd3
Eltreum <eltreum2048@gmail.com>
2022-02-28 01:51:33 -0300

    Update SetupNameplates.lua

--- ElvUI_EltreumUI/Layouts/SetupNameplates.lua
+++ ElvUI_EltreumUI/Layouts/SetupNameplates.lua
@@ -614,8 +614,6 @@ function ElvUI_EltreumUI:SetupStyleFilters()
 			E.global["nameplates"]["filters"]["EltreumExecute"]["triggers"]["class"]["WARRIOR"]["specs"][71] = true
 			E.global["nameplates"]["filters"]["EltreumExecute"]["triggers"]["class"]["WARRIOR"]["specs"][72] = true
 			E.global["nameplates"]["filters"]["EltreumExecute"]["triggers"]["class"]["WARRIOR"]["specs"][73] = true
-		elseif E.TBC or E.Classic then
-			E.global["nameplates"]["filters"]["EltreumExecute"]["triggers"]["class"]["WARRIOR"]["enabled"] = true
 		end
 
 		--fancy rares
