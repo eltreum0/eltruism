12ebc94d95777a3807fb1509b99bef0a33b78f7e
Eltreum <eltreum2048@gmail.com>
2022-03-04 22:40:23 -0300

    update profiles

--- ElvUI_EltreumUI/Profiles/NameplateSCT.lua
+++ ElvUI_EltreumUI/Profiles/NameplateSCT.lua
@@ -2,59 +2,30 @@ local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 
 -- NameplateSCT profile setup
 function ElvUI_EltreumUI:GetNameplateSCTProfile()
-	if E.Retail or E.Classic then
-		NameplateSCTDB = {
-			["global"] = {
-				["iconScale"] = 0.8,
-				["sizing"] = {
-					["critsScale"] = 3,
-					["smallHitsScale"] = 0.5,
-				},
-				["personal"] = false,
-				["yOffset"] = 75,
-				["enableMSQ"] = false,
-				["truncate"] = false,
-				["modOffTargetStrata"] = true,
-				["fontFlag"] = "THICKOUTLINE",
-				["shouldDisplayOverkill"] = true,
-				["font"] = "Kimberley",
-				["strata"] = {
-					["target"] = "BACKGROUND",
-					["offTarget"] = "BACKGROUND",
-				},
-				["yOffsetIcon"] = 2,
-				["animations"] = {
-					["miss"] = "rainfall",
-				},
-				["textShadow"] = false,
+	NameplateSCTDB = {
+		["global"] = {
+			["iconScale"] = 0.8,
+			["sizing"] = {
+				["critsScale"] = 3,
+				["smallHitsScale"] = 0.5,
 			},
-		}
-	elseif E.TBC then
-		NameplateSCTDB = {
-			["global"] = {
-				["iconScale"] = 0.8,
-				["sizing"] = {
-					["critsScale"] = 3,
-					["smallHitsScale"] = 0.5,
-				},
-				["personal"] = false,
-				["shouldDisplayOverkill"] = true,
-				["yOffset"] = 75,
-				["enableMSQ"] = false,
-				["truncate"] = false,
-				["modOffTargetStrata"] = true,
-				["fontFlag"] = "THICKOUTLINE",
-				["font"] = "Kimberley",
-				["strata"] = {
-					["target"] = "BACKGROUND",
-					["offTarget"] = "BACKGROUND",
-				},
-				["yOffsetIcon"] = 2,
-				["animations"] = {
-					["miss"] = "rainfall",
-				},
-				["textShadow"] = false,
+			["personal"] = false,
+			["shouldDisplayOverkill"] = true,
+			["yOffset"] = 75,
+			["enableMSQ"] = false,
+			["truncate"] = false,
+			["modOffTargetStrata"] = true,
+			["fontFlag"] = "THICKOUTLINE",
+			["font"] = "Kimberley",
+			["strata"] = {
+				["target"] = "BACKGROUND",
+				["offTarget"] = "BACKGROUND",
 			},
-		}
-	end
+			["yOffsetIcon"] = 2,
+			["animations"] = {
+				["miss"] = "rainfall",
+			},
+			["textShadow"] = false,
+		},
+	}
 end
--- ElvUI_EltreumUI/Profiles/ProjectAzilroka.lua
+++ ElvUI_EltreumUI/Profiles/ProjectAzilroka.lua
@@ -1,6 +1,5 @@
 local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 local _G = _G
-local pacheck = GetAddOnMetadata('ProjectAzilroka', 'Version')
 
 -- ProjectAzilroka profile setup
 function ElvUI_EltreumUI:GetPAProfile()
@@ -13,11 +12,7 @@ function ElvUI_EltreumUI:GetPAProfile()
 	PA.db["QuestSounds"]["Enable"] = false
 	PA.db["FasterLoot"]["Enable"] = false
 	if E.Retail then
-		if pacheck >= '1.77' then
-			PA.db["SunsongRanchFarmer"]["Enable"] = false
-		elseif pacheck < '1.77' then
-			PA.db["BigButtons"]["Enable"] = false
-		end
+		PA.db["SunsongRanchFarmer"]["Enable"] = false
 		PA.db["QuestSounds"]["Enable"] = true
 		PA.db["QuestSounds"]["ObjectiveCompleteID"] = "None"
 		PA.db["QuestSounds"]["ObjectiveProgressID"] = "None"
@@ -42,98 +37,6 @@ function ElvUI_EltreumUI:GetPAProfile()
 	PA.db["OzCooldowns"]["Enable"] = false
 	if E.Classic or E.TBC then
 		PA.db["MovableFrames"]["Enable"] = true
-		--[[
-		PA.db["MovableFrames"]["GhostFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["WorldStateScoreFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GameMenuFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ItemTextFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CovenantSanctumFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["VoidStorageFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GuildInviteFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["StaticPopup3"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonBuildingFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonLandingPage"]["Permanent"] = false
-		PA.db["MovableFrames"]["PetitionFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["StaticPopup1"]["Permanent"] = false
-		PA.db["MovableFrames"]["ItemSocketingFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["QuestFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TradeFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CalendarFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["InterfaceOptionsFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["AuctionFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GuildRegistrarFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["SplashFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TaxiFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GossipFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonRecruiterFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["DressUpFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["WorldMapFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["LFGDungeonReadyDialog"]["Permanent"] = false
-		PA.db["MovableFrames"]["PVEFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["QuestLogFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TradeSkillFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["RaidParentFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ClassTrainerFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["VideoOptionsFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CovenantPreviewFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["MailFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ScrollOfResurrectionSelectionFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["MacroFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["BarberShopFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CollectionsJournal"]["Permanent"] = false
-		PA.db["MovableFrames"]["AuctionHouseFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["StaticPopup4"]["Permanent"] = false
-		PA.db["MovableFrames"]["ChannelFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonCapacitiveDisplayFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TabardFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["BlackMarketFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonRecruitSelectFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["PlayerTalentFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GuildBankFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["SpellBookFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonShipyardFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["AchievementFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["StaticPopup2"]["Permanent"] = false
-		PA.db["MovableFrames"]["FriendsFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TimeManagerFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ReadyCheckFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ItemUpgradeFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["LookingForGuildFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CalendarCreateEventFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["WeeklyRewardsFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["InspectFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CraftFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CommunitiesFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["BankFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["RaidBrowserFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CharacterFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["QuestChoiceFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TalentFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ScrappingMachineFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["ArchaeologyFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["AnimaDiversionFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CalendarViewEventFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["LossOfControlFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GarrisonMissionFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["MerchantFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["AddonList"]["Permanent"] = false
-		PA.db["MovableFrames"]["ChallengesKeystoneFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["FriendsFriendsFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["OrderHallTalentFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["PetStableFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["QuestLogPopupDetailFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["GuildControlUI"]["Permanent"] = false
-		PA.db["MovableFrames"]["LootFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["CalendarViewHolidayFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["TalkingHeadFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["SoulbindViewer"]["Permanent"] = false
-		PA.db["MovableFrames"]["EncounterJournal"]["Permanent"] = false
-		PA.db["MovableFrames"]["KeyBindingFrame"]["Permanent"] = false
-		PA.db["MovableFrames"]["HelpFrame"]["Permanent"] = false
-		]]
-		--PA.db["EnhancedFriendsList"] = {}
-		--PA.db["EnhancedFriendsList"] = true
-		--PA.db["EnhancedFriendsList"]["StatusIconPack"] = "Square"
 	end
 	PA.db["TargetSounds"]["Enable"] = true
 	PA.db["stAddonManager"]["Enable"] = true
@@ -157,7 +60,6 @@ function ElvUI_EltreumUI:GetPAProfile()
 	PA.db["SquareMinimapButtons"]["Visibility"] = "show" --"[petbattle][combat]hide; show"
 	PA.db["SquareMinimapButtons"]["MoveTracker"] = false
 	PA.db["SquareMinimapButtons"]["BarMouseOver"] = true
-
 
 	E.private.ElvUI_EltreumUI.isInstalled.projectazilroka = true
 end
