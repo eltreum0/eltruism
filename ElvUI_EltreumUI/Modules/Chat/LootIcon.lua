e0781d92f9b12bd2ab1b9ae685768ab592308000
Eltreum <eltreum2048@gmail.com>
2022-03-09 04:26:52 -0300

    Update LootIcon.lua

--- ElvUI_EltreumUI/Modules/Chat/LootIcon.lua
+++ ElvUI_EltreumUI/Modules/Chat/LootIcon.lua
@@ -6,23 +6,40 @@ local function AddLootIcons(_, _, message, ...)
 		return
 	elseif E.db.ElvUI_EltreumUI.chat.looticons then
 		local function Icon(link)
-			if link then
-				local itemQuality, itemLevel, itemType, texture
-				local item = Item:CreateFromItemLink(link)
-				item:ContinueOnItemLoad(function()
-					--itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expacID, setID, isCraftingReagent = GetItemInfo(item)
-					itemQuality = select(3, GetItemInfo(link))
-					itemLevel = select(4, GetItemInfo(link))
-					itemType = select(6, GetItemInfo(link))
-					texture = select(10, GetItemInfo(link))
-					--_, _, itemQuality, itemLevel, _, itemType, _, _, _, texture = GetItemInfo(link)
-				end)
-				if itemLevel ~= nil and itemLevel > 1 and E.db.ElvUI_EltreumUI.chat.itemlevels and itemQuality and itemType ~= "Consumable" then
-					local _, _, _, hex = GetItemQualityColor(itemQuality)
-					return "|T"..texture..":12:12:0:0:64:64:5:59:5:59|t|c"..hex.."["..itemLevel.."]|r"..link
-				else
-					return "|T"..texture..":12:12:0:0:64:64:5:59:5:59|t"..link
+
+			local ilvlpattern = _G.ITEM_LEVEL:gsub('%%d', '(%%d+)')
+			local texture = GetItemIcon(link)
+			local itemLevel
+
+			--from elvui
+			local tt = E.ScanTooltip
+			tt:SetOwner(_G.UIParent, 'ANCHOR_NONE')
+			tt:SetHyperlink(link)
+
+			for x = 1, tt:NumLines() do
+				local line = _G['ElvUI_ScanTooltipTextLeft'..x]
+				if line then
+					local lineText = line:GetText()
+
+					if lineText ~= nil then
+						itemLevel = tonumber(lineText:match(ilvlpattern))
+					end
+					if itemLevel ~= nil then
+						break
+					end
 				end
+			end
+			tt:Hide()
+
+			local _, _, itemQuality, _, _, itemType = GetItemInfo(link)
+			--itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expacID, setID, isCraftingReagent = GetItemInfo(item)
+			if itemLevel ~= nil and itemLevel > 1 and E.db.ElvUI_EltreumUI.chat.itemlevels and itemQuality and itemType ~= "Consumable" then
+				local _, _, _, hex = GetItemQualityColor(itemQuality)
+				--return "|T"..texture..":".. 12 .."|t|c"..hex.."["..itemLevel.."]|r"..link
+				return "|T"..texture..":12:12:0:0:64:64:5:59:5:59|t|c"..hex.."["..itemLevel.."]|r"..link
+			else
+				--return "|T"..texture..":".. 12 .."|t"..link
+				return "|T"..texture..":12:12:0:0:64:64:5:59:5:59|t"..link
 			end
 		end
 		message = message:gsub("(|c%x+|Hitem:.-|h|r)", Icon)
