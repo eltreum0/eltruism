a6023ea9de00d4996e7a233dc61500c13b59997b
Eltreum <eltreum2048@gmail.com>
2022-07-11 19:08:54 -0300

    Revert "test merging player grad into unit function"
    This reverts commit 9dcc4ba80adca4958020fc7826051a83de0966f7.

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -73,7 +73,7 @@ function ElvUI_EltreumUI:PLAYER_ENTERING_WORLD()
 	ElvUI_EltreumUI:ExpandedCharacterStats() --attempt at improving the character panel
 	ElvUI_EltreumUI:DeathSound() -- set the party/raid death sound
 	ElvUI_EltreumUI.PlayerDeathAnimation() -- death animation
-	--ElvUI_EltreumUI:ChangePlayerTexture() -- load the player texture
+	ElvUI_EltreumUI:ChangePlayerTexture() -- load the player texture
 	ElvUI_EltreumUI:DeleteItem() -- automatically type delete
 	ElvUI_EltreumUI:SkillGlow() --makes skill glow using libcustomglow
 	ElvUI_EltreumUI:BattleRes() --bres count/cd
