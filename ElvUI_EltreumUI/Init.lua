16632608d433c02569980d248dc0fe83a3164269
Eltreum <eltreum2048@gmail.com>
2022-07-13 00:23:49 -0300

    prompt a reload if borders enabled

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -265,6 +265,9 @@ function ElvUI_EltreumUI:PLAYER_SPECIALIZATION_CHANGED()
 		if E.private.nameplates.enable then
 			ElvUI_EltreumUI:UpdateNPwithoutBar()
 		end
+		if E.db.ElvUI_EltreumUI.borders.borders then
+			E:StaticPopup_Show('CONFIG_RL')
+		end
 	end
 end
 
