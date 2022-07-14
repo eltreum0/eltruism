94361aac73b8f96db260abe6604f19bfebadf92b
Eltreum <eltreum2048@gmail.com>
2022-07-13 21:17:26 -0300

    fix disabled when custom enabled

--- ElvUI_EltreumUI/Options/OptionsRetail.lua
+++ ElvUI_EltreumUI/Options/OptionsRetail.lua
@@ -4580,7 +4580,7 @@ function ElvUI_EltreumUI:Configtable()
 									["V2"] = L["Version 2"],
 								},
 								style = 'radio',
-								disabled = function() return not E.db.ElvUI_EltreumUI.UFmodifications end,
+								disabled = function() return E.db.ElvUI_EltreumUI.ufcustomtexture.enable or not E.db.ElvUI_EltreumUI.UFmodifications end,
 								get = function() return E.db.ElvUI_EltreumUI.uftextureversion end,
 								set = function(_, value) E.db.ElvUI_EltreumUI.uftextureversion = value E:StaticPopup_Show('CONFIG_RL') end,
 							},
