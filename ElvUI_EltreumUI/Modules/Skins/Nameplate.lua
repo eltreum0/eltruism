d1932806a2647087338ecf90686f8a5bc799313f
Eltreum <eltreum2048@gmail.com>
2022-08-10 18:19:35 -0300

    temp test

--- ElvUI_EltreumUI/Modules/Skins/Nameplate.lua
+++ ElvUI_EltreumUI/Modules/Skins/Nameplate.lua
@@ -4,7 +4,11 @@ local _G = _G
 local hooksecurefunc = _G.hooksecurefunc
 
 --gradient nameplates
-local function GradientNameplates(unit)
+local function GradientNameplates(unit, status)
+	print(unit.colorTapping)
+	if type(status) == "number" then
+		print(unit.isTank,unit.offTank,status)
+	end
 	if E.db.ElvUI_EltreumUI.gradientmode.npenable then
 		if unit and unit.Health then
 			local _, className = UnitClass(unit.unit)
@@ -28,7 +32,9 @@ local function GradientNameplates(unit)
 					unit.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColors(className))
 				end
 			elseif reaction ~= nil then
-				if E.db.ElvUI_EltreumUI.gradientmode.npcustomcolor then
+				if UnitIsTapDenied(unit.unit) then
+					return
+				elseif E.db.ElvUI_EltreumUI.gradientmode.npcustomcolor then
 					unit.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColorsCustom(targettype, false, false))
 				else
 					unit.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColors(targettype, false, false))
@@ -38,3 +44,26 @@ local function GradientNameplates(unit)
 	end
 end
 hooksecurefunc(NP, "Health_UpdateColor", GradientNameplates)
+
+
+local function Threat(unit, _, status) --(unit,nameplate,status)
+	if status ~= nil and type(status) == "number" then
+		GradientNameplates(unit, status)
+	end
+	if E.db.ElvUI_EltreumUI.gradientmode.npenable then
+		if unit and unit.Health then
+			print("---------------------")
+			--print(unit.isTank,unit.offTank,status, unit.colors.badTransition, status,status2,status3,status4)
+			if status ~= nil and status == 3 then
+				print("3")
+			elseif status ~= nil and status == 2 then
+				print("2")
+			elseif status ~= nil and status == 1 then
+				print("1")
+			elseif status ~= nil and status == 0 then
+				print("0")
+			end
+		end
+	end
+end
+hooksecurefunc(NP, "ThreatIndicator_PostUpdate", Threat)
