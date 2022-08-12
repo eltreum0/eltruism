70a1caee41dd120bf3a1b08585210715f53971cc
Eltreum <eltreum2048@gmail.com>
2022-08-12 15:55:37 -0300

    test

--- ElvUI_EltreumUI/Modules/Skins/Nameplate.lua
+++ ElvUI_EltreumUI/Modules/Skins/Nameplate.lua
@@ -191,7 +191,6 @@ end
 hooksecurefunc(NP, "Health_UpdateColor", GradientNameplates)
 
 --currently not working
---[[
 function NP:StyleFilterClearChanges(frame, HealthColor, PowerColor, Borders, HealthFlash, HealthTexture, Scale, Alpha, NameTag, PowerTag, HealthTag, TitleTag, LevelTag, Portrait, NameOnly, Visibility)
 	local db = NP:PlateDB(frame)
 
@@ -213,25 +212,7 @@ function NP:StyleFilterClearChanges(frame, HealthColor, PowerColor, Borders, Hea
 			h:SetStatusBarColor(h.r, h.g, h.b, h.r-0.4, h.g-0.4, h.b-0.4)
 			--h:SetStatusBarColor(gradR1,gradG1,gradB1,gradR2,grabG2,gradB2)
 			if E.db.ElvUI_EltreumUI.gradientmode.npenable then
-				if className and player then
-					if E.db.ElvUI_EltreumUI.gradientmode.npcustomcolor then
-						h:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColorsCustom(className))
-						gradR1,gradG1,gradB1,gradR2,grabG2,gradB2 = ElvUI_EltreumUI:GradientColorsCustom(className)
-					else
-						h:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColors(className))
-						gradR1,gradG1,gradB1,gradR2,grabG2,gradB2 = ElvUI_EltreumUI:GradientColors(className)
-					end
-				elseif reaction ~= nil then
-					if E.db.ElvUI_EltreumUI.gradientmode.npcustomcolor then
-						h:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColorsCustom(targettype, false, false))
-						gradR1,gradG1,gradB1,gradR2,grabG2,gradB2 = ElvUI_EltreumUI:GradientColorsCustom(targettype, false, false)
-					else
-						h:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation, ElvUI_EltreumUI:GradientColors(targettype, false, false))
-						gradR1,gradG1,gradB1,gradR2,grabG2,gradB2 = ElvUI_EltreumUI:GradientColors(targettype, false, false)
-					end
-				end
-
-
+				GradientNameplates(frame)
 				--h:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.gradientmode.nporientation,gradR1,gradG1,gradB1,gradR2,grabG2,gradB2)
 			end
 			frame.Cutaway.Health:SetVertexColor(h.r * 1.5, h.g * 1.5, h.b * 1.5, 1)
@@ -277,4 +258,4 @@ function NP:StyleFilterClearChanges(frame, HealthColor, PowerColor, Borders, Hea
 		if LevelTag then frame:Tag(frame.Level, db.level.format) frame.Level:UpdateTag() end
 	end
 end
-]]
+
