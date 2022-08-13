8a9f126dc2b81d0ef6bb1f3b73b0d116ca81c5d0
Eltreum <eltreum2048@gmail.com>
2022-08-13 14:22:59 -0300

    this works

--- ElvUI_EltreumUI/Modules/Skins/Unitframes.lua
+++ ElvUI_EltreumUI/Modules/Skins/Unitframes.lua
@@ -44,7 +44,8 @@ function ElvUI_EltreumUI:ApplyUnitGradientTexture(unit,name,uf)
 			end
 		end
 	end
-	if UF["units"][uf] and UnitExists(unit) then
+	--if UF["units"][uf] and UnitExists(unit) then
+	if E.db["unitframe"]["units"][uf] and UnitExists(unit) then
 		local unitframe = _G["ElvUF_"..name]
 		if unitframe and unitframe.Health then
 			unitframe.Health:SetOrientation(E.db.ElvUI_EltreumUI.UForientation)
