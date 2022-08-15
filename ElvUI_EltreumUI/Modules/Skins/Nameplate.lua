58a6aa967d842711eddadb4bbf2733880e7e0428
Eltreum <eltreum2048@gmail.com>
2022-08-15 18:05:16 -0300

    turns out its the style filter scale
    gotta play around to figure out a delay or something

--- ElvUI_EltreumUI/Modules/Skins/Nameplate.lua
+++ ElvUI_EltreumUI/Modules/Skins/Nameplate.lua
@@ -222,45 +222,52 @@ local target3d = CreateFrame('PlayerModel', "EltruismNameplateModel")
 function ElvUI_EltreumUI:NameplateModel(nameplate)
 	if E.db.ElvUI_EltreumUI.nameplateOptions.targetmodel then
 		if UnitExists("target") then
-
-
 			if nameplate and nameplate.unit then
 				ElvUI_EltreumUI:NameplateCustomOptions(nameplate) --testing sending unit to other function
-
-
-				target3d:SetSize(E.db.nameplates.plateSize.enemyWidth or P.nameplates.plateSize.enemyWidth, E.db.ElvUI_EltreumUI.nameplateOptions.incombatHeight)
-				target3d:ClearModel()
-				target3d:SetUnit(nameplate.unit)
-				target3d:SetDesaturation(E.db.ElvUI_EltreumUI.nameplateOptions.desaturation)
-				target3d:SetPaused(E.db.ElvUI_EltreumUI.nameplateOptions.paused)
-				target3d:SetPortraitZoom(1) --allows the same cam as elvui UF
-				target3d:SetCamDistanceScale(E.db.ElvUI_EltreumUI.nameplateOptions.CamDistanceScale)
-				target3d:SetViewTranslation(E.db.ElvUI_EltreumUI.nameplateOptions.ViewTranslationx*100,E.db.ElvUI_EltreumUI.nameplateOptions.ViewTranslationy*100)
-				target3d:SetRotation(rad(E.db.ElvUI_EltreumUI.nameplateOptions.Rotation))
-				target3d:SetAlpha(E.db.ElvUI_EltreumUI.nameplateOptions.modelalpha)
-
-				--print(nameplate.Health:GetFrameLevel())
-
-
-				if nameplate.Health then
+				if nameplate.Health and nameplate.Health:IsVisible() then
+					target3d:ClearModel()
+					target3d:SetUnit(nameplate.unit)
+					target3d:SetDesaturation(E.db.ElvUI_EltreumUI.nameplateOptions.desaturation)
+					target3d:SetPaused(E.db.ElvUI_EltreumUI.nameplateOptions.paused)
+					target3d:SetPortraitZoom(1) --allows the same cam as elvui UF
+					target3d:SetCamDistanceScale(E.db.ElvUI_EltreumUI.nameplateOptions.CamDistanceScale)
+					target3d:SetViewTranslation(E.db.ElvUI_EltreumUI.nameplateOptions.ViewTranslationx*100,E.db.ElvUI_EltreumUI.nameplateOptions.ViewTranslationy*100)
+					target3d:SetRotation(rad(E.db.ElvUI_EltreumUI.nameplateOptions.Rotation))
+					target3d:SetAlpha(E.db.ElvUI_EltreumUI.nameplateOptions.modelalpha)
+					target3d:SetParent(nameplate.Health)
+					target3d:SetFrameLevel(nameplate.Health:GetFrameLevel())
+					target3d:SetSize(E.db.nameplates.plateSize.enemyWidth or P.nameplates.plateSize.enemyWidth, E.db.ElvUI_EltreumUI.nameplateOptions.incombatHeight)
 					target3d:ClearAllPoints()
 					target3d:SetPoint("CENTER", nameplate.Health, "CENTER")
 					target3d:SetInside(nameplate.Health, 0, 0) --(obj, anchor, xOffset, yOffset, anchor2, noScale)
-					target3d:Show()
+					target3d:SetAlpha(1)
+
+					E:Delay(0.15, function()
+						target3d:ClearModel()
+						target3d:SetUnit(nameplate.unit)
+						target3d:SetDesaturation(E.db.ElvUI_EltreumUI.nameplateOptions.desaturation)
+						target3d:SetPaused(E.db.ElvUI_EltreumUI.nameplateOptions.paused)
+						target3d:SetPortraitZoom(1) --allows the same cam as elvui UF
+						target3d:SetCamDistanceScale(E.db.ElvUI_EltreumUI.nameplateOptions.CamDistanceScale)
+						target3d:SetViewTranslation(E.db.ElvUI_EltreumUI.nameplateOptions.ViewTranslationx*100,E.db.ElvUI_EltreumUI.nameplateOptions.ViewTranslationy*100)
+						target3d:SetRotation(rad(E.db.ElvUI_EltreumUI.nameplateOptions.Rotation))
+						target3d:SetAlpha(E.db.ElvUI_EltreumUI.nameplateOptions.modelalpha)
+						target3d:SetParent(nameplate.Health)
+						target3d:SetFrameLevel(nameplate.Health:GetFrameLevel())
+						target3d:SetSize(E.db.nameplates.plateSize.enemyWidth or P.nameplates.plateSize.enemyWidth, E.db.ElvUI_EltreumUI.nameplateOptions.incombatHeight)
+						target3d:ClearAllPoints()
+						target3d:SetPoint("CENTER", nameplate.Health, "CENTER")
+						target3d:SetInside(nameplate.Health, 0, 0) --(obj, anchor, xOffset, yOffset, anchor2, noScale)
+						target3d:SetAlpha(1)
+					end)
 				else
 					target3d:ClearAllPoints()
-					target3d:Hide()
+					target3d:SetAlpha(0)
 				end
-			end
-
-			if nameplate and nameplate.Health then
-				target3d:SetFrameLevel(nameplate.Health:GetFrameLevel())
-				target3d:SetParent(nameplate.Health)
 			end
 		else
 			target3d:ClearAllPoints()
-			target3d:ClearModel()
-			target3d:Hide()
+			target3d:SetAlpha(0)
 		end
 	end
 end
