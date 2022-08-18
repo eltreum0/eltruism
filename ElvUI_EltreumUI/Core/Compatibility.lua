3ed9e7a918bd22c4db1ab5357012fb11ef76eb76
Eltreum <eltreum2048@gmail.com>
2022-08-17 21:15:40 -0300

    better

--- ElvUI_EltreumUI/Core/Compatibility.lua
+++ ElvUI_EltreumUI/Core/Compatibility.lua
@@ -1,10 +1,15 @@
 local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 local _G = _G
+local compatibilityran = false
+local addonname
+
 
 --check for stuff that would cause problems
+
+
 function ElvUI_EltreumUI:CheckCompatibility()
 	E.PopupDialogs["ELTRUISMCOMPATIBILITYFIX"] = {
-		text = L["MerathilisUI was detected, due to MerathilisUI and Eltruism doing some things that are similar, such as armory and gradient nameplates, those settings were disabled in order to not cause issues"],
+		text = addonname..L[" was detected, due to "..addonname.." and Eltruism doing some things that are similar, settings that are similar were disabled"],
 		button1 = OKAY,
 		timeout = 0,
 		whileDead = 1,
@@ -17,7 +22,12 @@ function ElvUI_EltreumUI:CheckCompatibility()
 
  		if E.db.ElvUI_EltreumUI.skins.classiconsoncharacterpanel == true and E.db["mui"]["armory"]["character"]["classIcon"] == true then
  			E.db["mui"]["armory"]["character"]["classIcon"] = false
+ 			compatibilityran = true
+ 			addonname = "MerathilisUI"
  		end
+
+
+
 
  	end
 end
