b4bf0783cecd3540b5e71fff767480a8b3ddec67
Eltreum <eltreum2048@gmail.com>
2022-08-20 09:49:10 -0300

    these can stay

--- ElvUI_EltreumUI/Modules/Skins/CharacterPanelIcons.lua
+++ ElvUI_EltreumUI/Modules/Skins/CharacterPanelIcons.lua
@@ -2,7 +2,6 @@ local ElvUI_EltreumUI, E, L, V, P, G = unpack(select(2, ...))
 local _G = _G
 local classcolor = E:ClassColor(E.myclass, true)
 local CreateFrame = _G.CreateFrame
-local UIParent = _G.UIParent
 local IsAddOnLoaded = _G.IsAddOnLoaded
 local hooksecurefunc = _G.hooksecurefunc
 local CharacterLevelText = _G.CharacterLevelText
