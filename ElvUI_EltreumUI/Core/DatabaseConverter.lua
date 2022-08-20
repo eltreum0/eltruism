0d1fa94356ba66c1b9fb68ede7b7500604eb67f3
Eltreum <eltreum2048@gmail.com>
2022-08-20 05:16:09 -0300

    just making sure it fires only once

--- ElvUI_EltreumUI/Core/DatabaseConverter.lua
+++ ElvUI_EltreumUI/Core/DatabaseConverter.lua
@@ -12,7 +12,7 @@ function ElvUI_EltreumUI:DatabaseConversions(forced)
 			hideOnEscape = false,
 		}
 
-		if E.private.ElvUI_EltreumUI.install_version < "3.2.0" or forced then
+		if E.private.ElvUI_EltreumUI.install_version < "3.2.5" or forced then
 			--Profile options conversion
 			local ProfileNames = "NONE"
 			local CharacterNames = "NONE"
