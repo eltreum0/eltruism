42be8b6942292954b0d75cfebbb70af0bffdf3bf
Eltreum <eltreum2048@gmail.com>
2022-08-20 01:10:29 -0300

    dont declare local inside function

--- ElvUI_EltreumUI/Modules/Skins/Actionbars.lua
+++ ElvUI_EltreumUI/Modules/Skins/Actionbars.lua
@@ -46,6 +46,7 @@ local skillglowcolorpet
 local _, autoCastEnabled
 local buttonNamepet
 local slots
+local buttonclassic
 
 --classic glows
 local SPELL_ID = {
@@ -330,14 +331,13 @@ function ElvUI_EltreumUI:SkillGlow()
 			function ElvUI_EltreumUI:ClassicGlow(barName)
 				bar = AB["handledBars"][barName]
 				if not bar then return end
-				local button
 				procFrame:RegisterEvent('ACTIONBAR_UPDATE_USABLE')
 				procFrame:RegisterEvent('SPELL_UPDATE_USABLE')
 				procFrame:RegisterEvent('PLAYER_TARGET_CHANGED')
 				procFrame:SetScript('OnEvent', function()
 					for i=1, NUM_ACTIONBAR_BUTTONS do
-						button = bar.buttons[i]
-						buttonname = button:GetName()
+						buttonclassic = bar.buttons[i]
+						buttonname = buttonclassic:GetName()
 						if _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() then
 							proc = _G[buttonname]:GetSpellId()
 						end
