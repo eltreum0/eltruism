16c58bae332432e2b8409b00d80724d087315a7b
Eltreum <eltreum2048@gmail.com>
2022-08-20 22:47:42 -0300

    wrath needs another event too

--- ElvUI_EltreumUI/Modules/Skins/Actionbars.lua
+++ ElvUI_EltreumUI/Modules/Skins/Actionbars.lua
@@ -320,6 +320,7 @@ function ElvUI_EltreumUI:SkillGlow()
 				local button
 				local procFrame = CreateFrame('frame')
 				procFrame:RegisterEvent('ACTIONBAR_UPDATE_USABLE')
+				procFrame:RegisterEvent('ACTIONBAR_UPDATE_STATE')
 				procFrame:RegisterEvent('SPELL_UPDATE_USABLE')
 				procFrame:RegisterEvent('PLAYER_TARGET_CHANGED')
 				procFrame:SetScript('OnEvent', function()
@@ -329,7 +330,7 @@ function ElvUI_EltreumUI:SkillGlow()
 						if _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() then
 							proc = _G[buttonname]:GetSpellId()
 						end
-						if SPELL_ID[proc] then
+						if SPELL_ID[proc] and not (E.Wrath and proc == 34026)then
 							local USABLE, NO_MANA = IsUsableSpell(proc)
 							local cd = GetSpellCooldown(proc)
 							if (UnitExists("target") and UnitCanAttack("player", "target") and cd == 0 and USABLE and not NO_MANA) and _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() == proc then
