e802c11ce9234aeb4c7df7a62656c50aee23ca26
Eltreum <eltreum2048@gmail.com>
2022-08-24 23:03:15 -0300

    figured out how to get class

--- ElvUI_EltreumUI/Modules/Skins/Unitframes.lua
+++ ElvUI_EltreumUI/Modules/Skins/Unitframes.lua
@@ -10,10 +10,6 @@ local tostring = _G.tostring
 local select = _G.select
 local UnitIsTapDenied = _G.UnitIsTapDenied
 local UnitPlayerControlled = _G.UnitPlayerControlled
-local truer = 0
-local trueg = 0
-local trueb = 0
-local trueclass
 local _, unit1class
 local r,g,b
 local unitframe
@@ -408,289 +404,38 @@ function ElvUI_EltreumUI:GradientCustomTexture(unit)
 				return
 			end
 
-			if E.db.ElvUI_EltreumUI.unitframes.lightmode then
-				if E. Retail then
-					if unit1class == 'WARRIOR' then
-						truer = 0.77646887302399
-						trueg = 0.60784178972244
-						trueb = 0.4274500310421
-						trueclass = "WARRIOR"
-					elseif unit1class == 'PALADIN' then
-						truer = 0.95686066150665
-						trueg = 0.54901838302612
-						trueb = 0.72941017150879
-						trueclass = "PALADIN"
-					elseif unit1class == 'HUNTER' then
-						truer = 0.66666519641876
-						trueg = 0.82744914293289
-						trueb = 0.44705784320831
-						trueclass = "HUNTER"
-					elseif unit1class == 'ROGUE' then
-						truer = 0.99999779462814
-						trueg = 0.95686066150665
-						trueb = 0.40784224867821
-						trueclass = "ROGUE"
-					elseif unit1class == 'PRIEST' and E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then --priest ruins everything gradient
-						trueclass = "PRIEST"
-						return
-					elseif unit1class == 'PRIEST' and not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then
-						truer = 0.99999779462814
-						trueg = 0.99999779462814
-						trueb = 0.99999779462814
-						trueclass = "PRIEST"
-					elseif unit1class == 'DEATHKNIGHT' then
-						truer = 0.76862573623657
-						trueg = 0.11764679849148
-						trueb = 0.2274504750967
-						trueclass = "DEATHKNIGHT"
-					elseif unit1class == 'SHAMAN' then
-						truer = 0
-						trueg = 0.4392147064209
-						trueb = 0.86666476726532
-						trueclass = "SHAMAN"
-					elseif unit1class == 'MAGE' then
-						truer = 0.24705828726292
-						trueg = 0.78039044141769
-						trueb = 0.92156660556793
-						trueclass = "MAGE"
-					elseif unit1class == 'WARLOCK' then
-						truer = 0.52941060066223
-						trueg = 0.53333216905594
-						trueb = 0.93333131074905
-						trueclass = "WARLOCK"
-					elseif unit1class == 'MONK' then
-						truer = 0
-						trueg = 0.99999779462814
-						trueb = 0.59607714414597
-						trueclass = "MONK"
-					elseif unit1class == 'DRUID' then
-						truer = 0.99999779462814
-						trueg = 0.48627343773842
-						trueb = 0.039215601980686
-						trueclass = "DRUID"
-					elseif unit1class == 'DEMONHUNTER' then
-						truer = 0.63921427726746
-						trueg = 0.1882348805666
-						trueb = 0.78823357820511
-						trueclass = "DEMONHUNTER"
-					end
-				elseif E.Wrath or E.TBC or E.Classic then
-					if unit1class == 'WARRIOR' then
-						truer = 0.78039044141769
-						trueg = 0.61176335811615
-						trueb = 0.43137159943581
-						trueclass = "WARRIOR"
-					elseif unit1class == 'PALADIN' then
-						truer = 0.96078222990036
-						trueg = 0.54901838302612
-						trueb = 0.72941017150879
-						trueclass = "PALADIN"
-					elseif unit1class == 'HUNTER' then
-						truer = 0.67058676481247
-						trueg = 0.8313707113266
-						trueb = 0.45097941160202
-						trueclass = "HUNTER"
-					elseif unit1class == 'ROGUE' then
-						truer = 0.99999779462814
-						trueg = 0.96078222990036
-						trueb = 0.41176378726959
-						trueclass = "ROGUE"
-					elseif unit1class == 'PRIEST' and E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then --priest ruins everything gradient
-						trueclass = "PRIEST"
-						return
-					elseif unit1class == 'PRIEST' and not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then
-						truer = 0.99999779462814
-						trueg = 0.99999779462814
-						trueb = 0.99999779462814
-						trueclass = "PRIEST"
-					elseif unit1class == 'DEATHKNIGHT' then
-						truer = 0.76862573623657
-						trueg = 0.1215683594346
-						trueb = 0.23137204349041
-						trueclass = "DEATHKNIGHT"
-					elseif unit1class == 'SHAMAN' and (E.Wrath or E.TBC) then
-						truer = 0
-						trueg = 0.4392147064209
-						trueb = 0.87058633565903
-						trueclass = "SHAMAN"
-					elseif unit1class == 'SHAMAN' and E.Classic then
-						truer = 0.96078222990036
-						trueg = 0.54901838302612
-						trueb = 0.72941017150879
-						trueclass = "SHAMAN"
-					elseif unit1class == 'MAGE' then
-						truer = 0.25097984075546
-						trueg = 0.78039044141769
-						trueb = 0.92156660556793
-						trueclass = "MAGE"
-					elseif unit1class == 'WARLOCK' then
-						truer = 0.52941060066223
-						trueg = 0.52941060066223
-						trueb = 0.92940974235535
-						trueclass = "WARLOCK"
-					elseif unit1class == 'DRUID' then
-						truer = 0.99999779462814
-						trueg = 0.49019500613213
-						trueb = 0.039215601980686
-						trueclass = "DRUID"
-					end
-				end
-			elseif E.db.ElvUI_EltreumUI.unitframes.darkmode then
-				if E. Retail then
-					if unit1class == 'WARRIOR' then
-						truer = 0.58039087057114
-						trueg = 0.45490095019341
-						trueb = 0.32156792283058
-						trueclass = "WARRIOR"
-					elseif unit1class == 'PALADIN' then
-						truer = 0.71764546632767
-						trueg = 0.41176378726959
-						trueb = 0.54509681463242
-						trueclass = "PALADIN"
-					elseif unit1class == 'HUNTER' then
-						truer = 0.49803811311722
-						trueg = 0.61960649490356
-						trueb = 0.33333259820938
-						trueclass = "HUNTER"
-					elseif unit1class == 'ROGUE' then
-						truer = 0.74901795387268
-						trueg = 0.71764546632767
-						trueb = 0.30588167905807
-						trueclass = "ROGUE"
-					elseif unit1class == 'PRIEST' then
-						truer = 0.74901795387268
-						trueg = 0.74901795387268
-						trueb = 0.74901795387268
-						trueclass = "PRIEST"
-					elseif unit1class == 'DEATHKNIGHT' then
-						truer = 0.57646930217743
-						trueg = 0.086274318397045
-						trueb = 0.16862708330154
-						trueclass = "DEATHKNIGHT"
-					elseif unit1class == 'SHAMAN' then
-						truer = 0
-						trueg = 0.32941102981567
-						trueb = 0.65097898244858
-						trueclass = "SHAMAN"
-					elseif unit1class == 'MAGE' then
-						truer = 0.18431332707405
-						trueg = 0.58431243896484
-						trueb = 0.69019454717636
-						trueclass = "MAGE"
-					elseif unit1class == 'WARLOCK' then
-						truer = 0.39607757329941
-						trueg = 0.39999911189079
-						trueb = 0.69803768396378
-						trueclass = "WARLOCK"
-					elseif unit1class == 'MONK' then
-						truer = 0
-						trueg = 0.74901795387268
-						trueb = 0.44705784320831
-						trueclass = "MONK"
-					elseif unit1class == 'DRUID' then
-						truer = 0.74901795387268
-						trueg = 0.36470508575439
-						trueb = 0.027450919151306
-						trueclass = "DRUID"
-					elseif unit1class == 'DEMONHUNTER' then
-						truer = 0.47843033075333
-						trueg = 0.14117616415024
-						trueb = 0.59215557575226
-						trueclass = "DEMONHUNTER"
-					end
-				elseif E.Wrath or E.TBC or E.Classic then
-					if unit1class == 'WARRIOR' then
-						truer = 0.58431243896484
-						trueg = 0.45882251858711
-						trueb = 0.32156792283058
-						trueclass = "WARRIOR"
-					elseif unit1class == 'PALADIN' then
-						truer = 0.72156703472137
-						trueg = 0.41176378726959
-						trueb = 0.54901838302612
-						trueclass = "PALADIN"
-					elseif unit1class == 'HUNTER' then
-						truer = 0.50195968151093
-						trueg = 0.62352806329727
-						trueb = 0.33725416660309
-						trueclass = "HUNTER"
-					elseif unit1class == 'ROGUE' then
-						truer = 0.74901795387268
-						trueg = 0.72156703472137
-						trueb = 0.30588167905807
-						trueclass = "ROGUE"
-					elseif unit1class == 'PRIEST' then
-						truer = 0.74901795387268
-						trueg = 0.74901795387268
-						trueb = 0.74901795387268
-						trueclass = "PRIEST"
-					elseif unit1class == 'DEATHKNIGHT' then
-						truer = 0.57646930217743
-						trueg = 0.090195879340172
-						trueb = 0.17254863679409
-						trueclass = "DEMONHUNTER"
-					elseif unit1class == 'SHAMAN' and (E.Wrath or E.TBC) then
-						truer = 0
-						trueg = 0.32941102981567
-						trueb = 0.65097898244858
-						trueclass = "SHAMAN"
-					elseif unit1class == 'SHAMAN' and E.Classic then
-						truer = 0.72156703472137
-						trueg = 0.41176378726959
-						trueb = 0.54901838302612
-						trueclass = "SHAMAN"
-					elseif unit1class == 'MAGE' then
-						truer = 0.1882348805666
-						trueg = 0.58431243896484
-						trueb = 0.69019454717636
-						trueclass = "MAGE"
-					elseif unit1class == 'WARLOCK' then
-						truer = 0.39607757329941
-						trueg = 0.39607757329941
-						trueb = 0.69803768396378
-						trueclass = "WARLOCK"
-					elseif unit1class == 'DRUID' then
-						truer = 0.74901795387268
-						trueg = 0.3686266541481
-						trueb = 0.031372480094433
-						trueclass = "DRUID"
-					end
-				end
-			end
-
-			local function ApplyGroupGradientTexture(r, g, b, button)
+			local function ApplyGroupGradientTexture(button)
 				button.Health:SetOrientation(E.db.ElvUI_EltreumUI.unitframes.UForientation)
 				if E.db.ElvUI_EltreumUI.unitframes.lightmode then ---TODO confirm this is fine for dark mode
 					button.Health.backdrop:SetBackdropColor(0,0,0,E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha)
 				end
-				if tostring(r) == tostring(truer) and tostring(g) == tostring(trueg) and tostring(b) == tostring(trueb) then
-					if E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable and E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablegroupunits then
-						if E.db.ElvUI_EltreumUI.unitframes.lightmode then
-							button.Health:SetStatusBarTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.gradientmode.texture))
-							if E.db.ElvUI_EltreumUI.unitframes.gradientmode.customcolor then
-								button.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColorsCustom(trueclass, false, false))
-							else
-								button.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColors(trueclass, false, false))
-							end
-						elseif E.db.ElvUI_EltreumUI.unitframes.darkmode and button.Health.backdropTex then
-							button.Health.backdropTex:SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.gradientmode.texture))
-							if E.db.ElvUI_EltreumUI.unitframes.gradientmode.customcolor then
-								button.Health.backdropTex:SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColorsCustom(trueclass, false, true))
-							else
-								button.Health.backdropTex:SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColors(trueclass, false, true))
-							end
+				local _, buttonclass = UnitClass(button.unit)
+				if E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable and E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablegroupunits then
+					if E.db.ElvUI_EltreumUI.unitframes.lightmode then
+						button.Health:SetStatusBarTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.gradientmode.texture))
+						if E.db.ElvUI_EltreumUI.unitframes.gradientmode.customcolor then
+							button.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColorsCustom(buttonclass, false, false))
+						else
+							button.Health:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColors(buttonclass, false, false))
 						end
-					elseif not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then
-						if E.db.ElvUI_EltreumUI.unitframes.lightmode then
-							if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable then
-								button.Health:SetStatusBarTexture(ElvUI_EltreumUI:UnitframeClassTextureCustom(trueclass))
-							else
-								button.Health:SetStatusBarTexture(ElvUI_EltreumUI:UnitframeClassTexture(trueclass))
-							end
-						elseif E.db.ElvUI_EltreumUI.unitframes.darkmode then
-							if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable and button.Health.backdropTex then
-								button.Health.backdropTex:SetTexture(ElvUI_EltreumUI:UnitframeClassTextureCustom(trueclass))
-							end
+					elseif E.db.ElvUI_EltreumUI.unitframes.darkmode and button.Health.backdropTex then
+						button.Health.backdropTex:SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.gradientmode.texture))
+						if E.db.ElvUI_EltreumUI.unitframes.gradientmode.customcolor then
+							button.Health.backdropTex:SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColorsCustom(buttonclass, false, true))
+						else
+							button.Health.backdropTex:SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColors(buttonclass, false, true))
+						end
+					end
+				elseif not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then
+					if E.db.ElvUI_EltreumUI.unitframes.lightmode then
+						if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable then
+							button.Health:SetStatusBarTexture(ElvUI_EltreumUI:UnitframeClassTextureCustom(buttonclass))
+						else
+							button.Health:SetStatusBarTexture(ElvUI_EltreumUI:UnitframeClassTexture(buttonclass))
+						end
+					elseif E.db.ElvUI_EltreumUI.unitframes.darkmode then
+						if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable and button.Health.backdropTex then
+							button.Health.backdropTex:SetTexture(ElvUI_EltreumUI:UnitframeClassTextureCustom(buttonclass))
 						end
 					end
 				end
@@ -702,12 +447,7 @@ function ElvUI_EltreumUI:GradientCustomTexture(unit)
 					for j = 1, group:GetNumChildren() do
 						groupbutton = select(j, group:GetChildren())
 						if groupbutton and groupbutton.Health then
-							if E.db.ElvUI_EltreumUI.unitframes.lightmode then
-								rgroup,ggroup,bgroup = groupbutton.Health:GetStatusBarColor()
-							elseif E.db.ElvUI_EltreumUI.unitframes.darkmode and groupbutton.Health.backdropTex then
-								rgroup,ggroup,bgroup = groupbutton.Health.backdropTex:GetVertexColor()
-							end
-							ApplyGroupGradientTexture(rgroup, ggroup, bgroup, groupbutton)
+							ApplyGroupGradientTexture(groupbutton)
 						end
 					end
 				end
@@ -717,12 +457,7 @@ function ElvUI_EltreumUI:GradientCustomTexture(unit)
 				for i = 1, headertank:GetNumChildren() do
 					tankbutton = select(i, headertank:GetChildren())
 					if tankbutton and tankbutton.Health then
-						if E.db.ElvUI_EltreumUI.unitframes.lightmode then
-							rtank,gtank,btank = tankbutton.Health:GetStatusBarColor()
-						elseif E.db.ElvUI_EltreumUI.unitframes.darkmode and tankbutton.Health.backdropTex then
-							rtank,gtank,btank = tankbutton.Health.backdropTex:GetVertexColor()
-						end
-						ApplyGroupGradientTexture(rtank, gtank, btank, tankbutton)
+						ApplyGroupGradientTexture(tankbutton)
 					end
 				end
 			end
@@ -731,12 +466,7 @@ function ElvUI_EltreumUI:GradientCustomTexture(unit)
 				for i = 1, headerassist:GetNumChildren() do
 					assistbutton = select(i, headerassist:GetChildren())
 					if assistbutton and assistbutton.Health then
-						if E.db.ElvUI_EltreumUI.unitframes.lightmode then
-							rassist,gassist,bassist = assistbutton.Health:GetStatusBarColor()
-						elseif E.db.ElvUI_EltreumUI.unitframes.darkmode and assistbutton.Health.backdropTex then
-							rassist,gassist,bassist = assistbutton.Health.backdropTex:GetVertexColor()
-						end
-						ApplyGroupGradientTexture(rassist, gassist, bassist, assistbutton)
+						ApplyGroupGradientTexture(assistbutton)
 					end
 				end
 			end
