72c53d89366de247f4f021aaffbb7b12eb7e81ef
Eltreum <eltreum2048@gmail.com>
2022-08-28 05:12:12 -0300

    do the same as health

--- ElvUI_EltreumUI/Modules/Skins/GradientPower.lua
+++ ElvUI_EltreumUI/Modules/Skins/GradientPower.lua
@@ -44,6 +44,25 @@ function ElvUI_EltreumUI:GradientPower()
 		ElvUI_EltreumUI:ApplyGradientPower("target", "Target")
 		ElvUI_EltreumUI:ApplyGradientPower("targettarget", "TargetTarget")
 		ElvUI_EltreumUI:ApplyGradientPower("pet", "Pet")
+		ElvUI_EltreumUI:ApplyGradientPower("targettargettarget", "TargetTargetTarget"," targetargettarget")
+		if E.Retail then
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss1", "Boss1", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss2", "Boss2", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss3", "Boss3", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss4", "Boss4", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss5", "Boss5", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss6", "Boss6", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss7", "Boss7", "boss")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("boss8", "Boss8", "boss")
+		end
+		if E.Retail or E.TBC or E.Wrath then
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("focus", "Focus", "focus")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("arena1", "Arena1", "arena")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("arena2", "Arena2", "arena")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("arena3", "Arena3", "arena")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("arena4", "Arena4", "arena")
+			ElvUI_EltreumUI:ApplyUnitGradientTexture("arena5", "Arena5", "arena")
+		end
 	end
 end
 hooksecurefunc(UF, "Construct_PowerBar", ElvUI_EltreumUI.GradientPower)
