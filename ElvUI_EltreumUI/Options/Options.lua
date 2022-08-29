8d53a2fb7ce8d4dc1faa422cb24cc9cb8212ced4
Eltreum <eltreum2048@gmail.com>
2022-08-29 15:11:55 -0300

    let power color change if using power bar

--- ElvUI_EltreumUI/Options/Options.lua
+++ ElvUI_EltreumUI/Options/Options.lua
@@ -5583,7 +5583,7 @@ function ElvUI_EltreumUI:Configtable()
 								name = L["Enable Custom Gradient Power"],
 								desc = L["Enable Custom Gradient Power Colors"],
 								width = 'full',
-								disabled = function() return not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablepower end,
+								disabled = function() return not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablepower and not E.db.ElvUI_EltreumUI.nameplates.nameplatepower.gradient end,
 								get = function() return E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablepowercustom end,
 								set = function(_, value) E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablepowercustom = value ElvUI_EltreumUI:GradientColorTableUpdate() end,
 							},
