9d2cc84018f209afc11bcf613f217f0f937b22cf
Eltreum <eltreum2048@gmail.com>
2022-08-30 11:31:28 -0300

    disable name only since its not fixed yet

--- ElvUI_EltreumUI/Layouts/SetupNameplates.lua
+++ ElvUI_EltreumUI/Layouts/SetupNameplates.lua
@@ -734,7 +734,7 @@ function ElvUI_EltreumUI:SetupStyleFilters()
 		E.global["nameplates"]["filters"]["EltreumSpellsteal"]["actions"]["flash"]["enable"] = true
 		E.global["nameplates"]["filters"]["EltreumSpellsteal"]["actions"]["scale"] = 1.25
 		--hide nameplates for unattackable npcs
-		E.global["nameplates"]["filters"]["EltreumHideNP"]["actions"]["nameOnly"] = true
+		E.global["nameplates"]["filters"]["EltreumHideNP"]["actions"]["nameOnly"] = false
 		E.global["nameplates"]["filters"]["EltreumHideNP"]["actions"]["tags"]["name"] = "[namecolor][name]"
 		E.global["nameplates"]["filters"]["EltreumHideNP"]["actions"]["tags"]["title"] = "[namecolor][npctitle:brackets]"
 		E.global["nameplates"]["filters"]["EltreumHideNP"]["triggers"]["nameplateType"]["enable"] = true
@@ -743,7 +743,7 @@ function ElvUI_EltreumUI:SetupStyleFilters()
 		E.global["nameplates"]["filters"]["EltreumHideNP"]["triggers"]["priority"] = 15
 		--non target full alpha when resting and not in combat, name only since cannot attack anyway
 		E.global["nameplates"]["filters"]["EltreumRestedNP"]["actions"]["alpha"] = 100
-		E.global["nameplates"]["filters"]["EltreumRestedNP"]["actions"]["nameOnly"] = true
+		E.global["nameplates"]["filters"]["EltreumRestedNP"]["actions"]["nameOnly"] = false
 		E.global["nameplates"]["filters"]["EltreumRestedNP"]["actions"]["tags"]["name"] = "[namecolor][name:title][realm:dash]"
 		E.global["nameplates"]["filters"]["EltreumRestedNP"]["actions"]["tags"]["title"] = "[namecolor][npctitle:brackets][guild:brackets]"
 		E.global["nameplates"]["filters"]["EltreumRestedNP"]["triggers"]["isResting"] = true
