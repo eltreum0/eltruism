ea1542700385e8c494fa9fd8664cbb2b71f35237
Eltreum <eltreum2048@gmail.com>
2022-09-06 12:24:01 -0300

    remove debug print

--- ElvUI_EltreumUI/Modules/Skins/ExpandedTalents.lua
+++ ElvUI_EltreumUI/Modules/Skins/ExpandedTalents.lua
@@ -40,7 +40,6 @@ function ElvUI_EltreumUI:ExpandedTalents()
 					--fix rank size
 					for i =1, MAX_NUM_TALENTS do
 						if _G["PlayerTalentFrameTalent"..i.."Rank"] then
-							print(_G["PlayerTalentFrameTalent"..i.."Rank"]:GetFont())
 							_G["PlayerTalentFrameTalent"..i.."Rank"]:SetFont(E.LSM:Fetch('font', E.db.general.font), E.db.general.fontSize+4, E.db.general.fontStyle)
 						end
 					end
