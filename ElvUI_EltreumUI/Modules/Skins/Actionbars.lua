8f97629c2d57f82af4a410a788bde80b3751cbb4
Eltreum <eltreum2048@gmail.com>
2022-09-15 00:00:47 -0300

    restrict to only classes that actually have procs

--- ElvUI_EltreumUI/Modules/Skins/Actionbars.lua
+++ ElvUI_EltreumUI/Modules/Skins/Actionbars.lua
@@ -229,117 +229,134 @@ function ElvUI_EltreumUI:SkillGlow()
 				end)
 			end
 
+			if E.myclass == 'PALADIN' or E.myclass == 'WARRIOR' or E.myclass == 'HUNTER' or E.myclass == 'ROGUE' or E.myclass == 'DEATHKNIGHT' then
+				--classic glow proc list
+				local SPELL_ID = {
 
-			--classic glow proc list
-			local SPELL_ID = {
+					--victory rush
+					[34428] = true,
+					--kill command
+					[34026] = true,
+					--riposte
+					[14251] = true,
 
-				--victory rush
-				[34428] = true,
-				--kill command
-				[34026] = true,
-				--riposte
-				[14251] = true,
+					--execute
+					[25236]	= true,
+					[25234]	= true,
+					[20662]	= true,
+					[20661]	= true,
+					[20660]	= true,
+					[20658]	= true,
+					[5308] = true,
 
-				--execute
-				[25236]	= true,
-				[25234]	= true,
-				[20662]	= true,
-				[20661]	= true,
-				[20660]	= true,
-				[20658]	= true,
-				[5308] = true,
+					--overpower
+					[11585] = true,
+					[11584] = true,
+					[7887] = true,
+					[7384] = true,
 
-				--overpower
-				[11585] = true,
-				[11584] = true,
-				[7887] = true,
-				[7384] = true,
+					--regenge
+					[30327] = true,
+					[25288] = true,
+					[25269] = true,
+					[11601] = true,
+					[11600] = true,
+					[7379] = true,
+					[6574] = true,
+					[6572] = true,
 
-				--regenge
-				[30327] = true,
-				[25288] = true,
-				[25269] = true,
-				[11601] = true,
-				[11600] = true,
-				[7379] = true,
-				[6574] = true,
-				[6572] = true,
+					--kill shot
+					[61006] = true,
 
-				--kill shot
-				[61006] = true,
+					--rune strike
+					[56815] = true,
 
-				--rune strike
-				[56815] = true,
+					--mongoose bite (always works on wotlk)
+					--[[[36916] = true,
+					[14271] = true,
+					[14270] = true,
+					[14269] = true,
+					[1495] = true,]]
 
-				--mongoose bite (always works on wotlk)
-				--[[[36916] = true,
-				[14271] = true,
-				[14270] = true,
-				[14269] = true,
-				[1495] = true,]]
+					--counterattack
+					[27067] = true,
+					[20910] = true,
+					[20909] = true,
+					[19306] = true,
 
-				--counterattack
-				[27067] = true,
-				[20910] = true,
-				[20909] = true,
-				[19306] = true,
+					--[[--exorcism  --different in wrath
+					[27138] = true,
+					[10314] = true,
+					[10313] = true,
+					[10312] = true,
+					[5615] = true,
+					[5614] = true,
+					[879] = true,]]
 
-				--[[--exorcism  --different in wrath
-				[27138] = true,
-				[10314] = true,
-				[10313] = true,
-				[10312] = true,
-				[5615] = true,
-				[5614] = true,
-				[879] = true,]]
+					--hammer of wrath
+					[27180] = true,
+					[24275] = true,
+					[24274] = true,
+					[24239] = true,
 
-				--hammer of wrath
-				[27180] = true,
-				[24275] = true,
-				[24274] = true,
-				[24239] = true,
+					--shadow bolt
+					--[[[27209] = true,
+					[25307] = true,
+					[11661] = true,
+					[11660] = true,
+					[11659] = true,
+					[7641] = true,
+					[1106] = true,
+					[1088] = true,
+					[705] = true,
+					[695] = true,
+					[686] = true,]]
+				}
 
-				--shadow bolt
-				--[[[27209] = true,
-				[25307] = true,
-				[11661] = true,
-				[11660] = true,
-				[11659] = true,
-				[7641] = true,
-				[1106] = true,
-				[1088] = true,
-				[705] = true,
-				[695] = true,
-				[686] = true,]]
-			}
-
-			local proc
-			--local auraid, _
-			function ElvUI_EltreumUI:ClassicGlow(barName)
-				local bar = AB["handledBars"][barName]
-				if not bar then return end
-				local button
-				local procFrame = CreateFrame('frame')
-				procFrame:RegisterEvent('ACTIONBAR_UPDATE_USABLE')
-				procFrame:RegisterEvent('ACTIONBAR_UPDATE_STATE')
-				procFrame:RegisterEvent('SPELL_UPDATE_USABLE')
-				procFrame:RegisterEvent('PLAYER_TARGET_CHANGED')
-				procFrame:SetScript('OnEvent', function()
-					for i=1, NUM_ACTIONBAR_BUTTONS do
-						button = bar.buttons[i]
-						local buttonname = button:GetName()
-						if _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() then
-							proc = _G[buttonname]:GetSpellId()
-						end
-						if SPELL_ID[proc] and not (E.Wrath and proc == 34026)then
-							local USABLE, NO_MANA = IsUsableSpell(proc)
-							local cd = GetSpellCooldown(proc)
-							if (UnitExists("target") and UnitCanAttack("player", "target") and cd == 0 and USABLE and not NO_MANA) and _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() == proc then
-								if E.myclass == 'PALADIN' then
-									if proc == 27138 or proc == 10314 or proc == 10313 or proc == 10312 or proc == 5615 or proc == 5614 or proc == 879 then
-										local unittype = UnitCreatureType("target")
-										--if unittype == "Demon" or unittype == "Dämon" or unittype == "Demonio" or unittype == "Démon" or unittype == "Demone" or unittype == "Demônio" or unittype == "Демон" or unittype == "악마" or unittype == "恶魔" or unittype == "惡魔" or unittype == "Undead" or unittype == "Untoter" or unittype == "No-muerto" or unittype == "Mort-vivant" or unittype == "Non Morto" or unittype == "Renegado" or unittype == "Нежить" or unittype == "언데드" or unittype == "亡灵" or unittype == "不死族" then
-										if unittype == _G.PET_TYPE_DEMON or unittype == _G.BATTLE_PET_DAMAGE_NAME_4 then
+				local proc
+				--local auraid, _
+				function ElvUI_EltreumUI:ClassicGlow(barName)
+					local bar = AB["handledBars"][barName]
+					if not bar then return end
+					local button
+					local procFrame = CreateFrame('frame')
+					procFrame:RegisterEvent('ACTIONBAR_UPDATE_USABLE')
+					procFrame:RegisterEvent('ACTIONBAR_UPDATE_STATE')
+					procFrame:RegisterEvent('SPELL_UPDATE_USABLE')
+					procFrame:RegisterEvent('PLAYER_TARGET_CHANGED')
+					procFrame:SetScript('OnEvent', function()
+						for i=1, NUM_ACTIONBAR_BUTTONS do
+							button = bar.buttons[i]
+							local buttonname = button:GetName()
+							if _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() then
+								proc = _G[buttonname]:GetSpellId()
+							end
+							if SPELL_ID[proc] and not (E.Wrath and proc == 34026)then
+								local USABLE, NO_MANA = IsUsableSpell(proc)
+								local cd = GetSpellCooldown(proc)
+								if (UnitExists("target") and UnitCanAttack("player", "target") and cd == 0 and USABLE and not NO_MANA) and _G[buttonname].GetSpellId and _G[buttonname]:GetSpellId() == proc then
+									if E.myclass == 'PALADIN' then
+										if proc == 27138 or proc == 10314 or proc == 10313 or proc == 10312 or proc == 5615 or proc == 5614 or proc == 879 then
+											local unittype = UnitCreatureType("target")
+											--if unittype == "Demon" or unittype == "Dämon" or unittype == "Demonio" or unittype == "Démon" or unittype == "Demone" or unittype == "Demônio" or unittype == "Демон" or unittype == "악마" or unittype == "恶魔" or unittype == "惡魔" or unittype == "Undead" or unittype == "Untoter" or unittype == "No-muerto" or unittype == "Mort-vivant" or unittype == "Non Morto" or unittype == "Renegado" or unittype == "Нежить" or unittype == "언데드" or unittype == "亡灵" or unittype == "不死族" then
+											if unittype == _G.PET_TYPE_DEMON or unittype == _G.BATTLE_PET_DAMAGE_NAME_4 then
+												if E.db.ElvUI_EltreumUI.glow.pixel then
+													LCG.PixelGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
+												elseif E.db.ElvUI_EltreumUI.glow.autocast then
+													LCG.AutoCastGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberauto, E.db.ElvUI_EltreumUI.glow.frequencyauto, E.db.ElvUI_EltreumUI.glow.autoscale, E.db.ElvUI_EltreumUI.glow.autoxOffset, E.db.ElvUI_EltreumUI.glow.autoyOffset)
+												elseif E.db.ElvUI_EltreumUI.glow.blizzard then
+													LCG.ButtonGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
+												end
+											else
+												if E.db.ElvUI_EltreumUI.glow.pixel then
+													LCG.PixelGlow_Stop(_G[buttonname])
+												elseif E.db.ElvUI_EltreumUI.glow.autocast then
+													LCG.AutoCastGlow_Stop(_G[buttonname])
+												elseif E.db.ElvUI_EltreumUI.glow.blizzard then
+													LCG.ButtonGlow_Stop(_G[buttonname])
+												end
+											end
+										else
 											if E.db.ElvUI_EltreumUI.glow.pixel then
 												LCG.PixelGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
 											elseif E.db.ElvUI_EltreumUI.glow.autocast then
@@ -347,15 +364,30 @@ function ElvUI_EltreumUI:SkillGlow()
 											elseif E.db.ElvUI_EltreumUI.glow.blizzard then
 												LCG.ButtonGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
 											end
-										else
-											if E.db.ElvUI_EltreumUI.glow.pixel then
-												LCG.PixelGlow_Stop(_G[buttonname])
-											elseif E.db.ElvUI_EltreumUI.glow.autocast then
-												LCG.AutoCastGlow_Stop(_G[buttonname])
-											elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-												LCG.ButtonGlow_Stop(_G[buttonname])
-											end
 										end
+									--[[elseif E.myclass == 'WARLOCK' then
+										procFrame:RegisterUnitEvent('UNIT_AURA', "player")
+										for i = 1, 30 do
+											--auraid = select(10, UnitAura("player", i))
+											--print(auraid)
+											if auraid == 34936 or auraid == 17941 then --backlash and shadow trance
+												if E.db.ElvUI_EltreumUI.glow.pixel and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
+													LCG.PixelGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
+												elseif E.db.ElvUI_EltreumUI.glow.autocast and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
+													LCG.AutoCastGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberauto, E.db.ElvUI_EltreumUI.glow.frequencyauto, E.db.ElvUI_EltreumUI.glow.autoscale, E.db.ElvUI_EltreumUI.glow.autoxOffset, E.db.ElvUI_EltreumUI.glow.autoyOffset)
+												elseif E.db.ElvUI_EltreumUI.glow.blizzard and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
+													LCG.ButtonGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
+												end
+											elseif not auraid or auraid ~= 34936 or auraid ~= 17941 then
+												if E.db.ElvUI_EltreumUI.glow.pixel and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
+													LCG.PixelGlow_Stop(_G[buttonname])
+												elseif E.db.ElvUI_EltreumUI.glow.autocast and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
+													LCG.AutoCastGlow_Stop(_G[buttonname])
+												elseif E.db.ElvUI_EltreumUI.glow.blizzard and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
+													LCG.ButtonGlow_Stop(_G[buttonname])
+												end
+											end
+										end]]
 									else
 										if E.db.ElvUI_EltreumUI.glow.pixel then
 											LCG.PixelGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
@@ -365,39 +397,16 @@ function ElvUI_EltreumUI:SkillGlow()
 											LCG.ButtonGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
 										end
 									end
-								--[[elseif E.myclass == 'WARLOCK' then
-									procFrame:RegisterUnitEvent('UNIT_AURA', "player")
-									for i = 1, 30 do
-										--auraid = select(10, UnitAura("player", i))
-										--print(auraid)
-										if auraid == 34936 or auraid == 17941 then --backlash and shadow trance
-											if E.db.ElvUI_EltreumUI.glow.pixel and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
-												LCG.PixelGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
-											elseif E.db.ElvUI_EltreumUI.glow.autocast and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
-												LCG.AutoCastGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberauto, E.db.ElvUI_EltreumUI.glow.frequencyauto, E.db.ElvUI_EltreumUI.glow.autoscale, E.db.ElvUI_EltreumUI.glow.autoxOffset, E.db.ElvUI_EltreumUI.glow.autoyOffset)
-											elseif E.db.ElvUI_EltreumUI.glow.blizzard and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
-												LCG.ButtonGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
-											end
-										elseif not auraid or auraid ~= 34936 or auraid ~= 17941 then
-											if E.db.ElvUI_EltreumUI.glow.pixel and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
-												LCG.PixelGlow_Stop(_G[buttonname])
-											elseif E.db.ElvUI_EltreumUI.glow.autocast and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
-												LCG.AutoCastGlow_Stop(_G[buttonname])
-											elseif E.db.ElvUI_EltreumUI.glow.blizzard and not IsAddOnLoaded("ElvUI_ActionBarMasks") then
-												LCG.ButtonGlow_Stop(_G[buttonname])
-											end
-										end
-									end]]
 								else
 									if E.db.ElvUI_EltreumUI.glow.pixel then
-										LCG.PixelGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
+										LCG.PixelGlow_Stop(_G[buttonname])
 									elseif E.db.ElvUI_EltreumUI.glow.autocast then
-										LCG.AutoCastGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.numberauto, E.db.ElvUI_EltreumUI.glow.frequencyauto, E.db.ElvUI_EltreumUI.glow.autoscale, E.db.ElvUI_EltreumUI.glow.autoxOffset, E.db.ElvUI_EltreumUI.glow.autoyOffset)
+										LCG.AutoCastGlow_Stop(_G[buttonname])
 									elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-										LCG.ButtonGlow_Start(_G[buttonname], skillglowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
+										LCG.ButtonGlow_Stop(_G[buttonname])
 									end
 								end
-							else
+							elseif SPELL_ID[proc] == nil then
 								if E.db.ElvUI_EltreumUI.glow.pixel then
 									LCG.PixelGlow_Stop(_G[buttonname])
 								elseif E.db.ElvUI_EltreumUI.glow.autocast then
@@ -406,31 +415,23 @@ function ElvUI_EltreumUI:SkillGlow()
 									LCG.ButtonGlow_Stop(_G[buttonname])
 								end
 							end
-						elseif SPELL_ID[proc] == nil then
-							if E.db.ElvUI_EltreumUI.glow.pixel then
-								LCG.PixelGlow_Stop(_G[buttonname])
-							elseif E.db.ElvUI_EltreumUI.glow.autocast then
-								LCG.AutoCastGlow_Stop(_G[buttonname])
-							elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-								LCG.ButtonGlow_Stop(_G[buttonname])
-							end
+						end
+					end)
+				end
+
+				local classicglowframe = CreateFrame("FRAME")
+				classicglowframe:RegisterEvent("PLAYER_STARTED_MOVING")
+				--classicglowframe:RegisterEvent('ACTIONBAR_UPDATE_USABLE')
+				classicglowframe:SetScript("OnEvent", function()
+					classicglowframe:UnregisterAllEvents()
+					if not InCombatLockdown() then
+						hooksecurefunc(AB, 'PositionAndSizeBar', ElvUI_EltreumUI.ClassicGlow)
+						for i = 1, 10 do
+							AB:PositionAndSizeBar('bar'..i)
 						end
 					end
 				end)
 			end
-
-			local classicglowframe = CreateFrame("FRAME")
-			classicglowframe:RegisterEvent("PLAYER_STARTED_MOVING")
-			--classicglowframe:RegisterEvent('ACTIONBAR_UPDATE_USABLE')
-			classicglowframe:SetScript("OnEvent", function()
-				classicglowframe:UnregisterAllEvents()
-				if not InCombatLockdown() then
-					hooksecurefunc(AB, 'PositionAndSizeBar', ElvUI_EltreumUI.ClassicGlow)
-					for i = 1, 10 do
-						AB:PositionAndSizeBar('bar'..i)
-					end
-				end
-			end)
 		end
 	end
 end
