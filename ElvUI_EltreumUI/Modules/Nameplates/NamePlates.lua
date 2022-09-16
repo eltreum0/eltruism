023da5d76b4a27e2465dce287b7a88bc55bbf9a1
Eltreum <eltreum2048@gmail.com>
2022-09-16 16:00:02 -0300

    better check for health class

--- ElvUI_EltreumUI/Modules/Nameplates/NamePlates.lua
+++ ElvUI_EltreumUI/Modules/Nameplates/NamePlates.lua
@@ -245,11 +245,11 @@ local nameplateclasscolors
 --sets them enabled/disabled
 function ElvUI_EltreumUI:SetStyleFilters()
 
-	--gradient check for my filters
-	if E.db.ElvUI_EltreumUI.unitframes.gradientmode.npenable then
-		if E.global.nameplates.filters.EltreumTarget then
-			E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["health"] = false
-			E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["healthClass"] = false
+	--fix threat
+	if E.global.nameplates.filters.EltreumTarget then
+		E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["health"] = false
+		E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["healthClass"] = false
+		if E.db.ElvUI_EltreumUI.unitframes.gradientmode.npenable then --gradient check for my filters
 			E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["texture"]["enable"] = false
 		end
 	end
@@ -364,15 +364,13 @@ function ElvUI_EltreumUI:NamePlateOptions()
 	nameplateclasscolors = E:ClassColor(E.myclass, true)
 	if E.private.nameplates.enable then
 
-		--gradient check for my filters
-		if E.db.ElvUI_EltreumUI.unitframes.gradientmode.npenable then
-			if E.global.nameplates.filters.EltreumTarget then
-				E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["health"] = false
-				E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["healthClass"] = false
+		--fix threat
+		if E.global.nameplates.filters.EltreumTarget then
+			E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["health"] = false
+			E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["color"]["healthClass"] = false
+			if E.db.ElvUI_EltreumUI.unitframes.gradientmode.npenable then --gradient check for my filters
 				E.global["nameplates"]["filters"]["EltreumTarget"]["actions"]["texture"]["enable"] = false
 			end
-			E.global["nameplates"]["filters"]["ElvUI_Boss"]["actions"]["color"]["health"] = false
-			E.global["nameplates"]["filters"]["ElvUI_Boss"]["actions"]["color"]["healthClass"] = false
 		end
 
 		--glow color
