e5cd21d7a75c467eff67224f133fd59534a4970e
Eltreum <eltreum2048@gmail.com>
2022-09-17 18:06:11 -0300

    test

--- ElvUI_EltreumUI/Modules/Skins/Unitframes.lua
+++ ElvUI_EltreumUI/Modules/Skins/Unitframes.lua
@@ -548,26 +548,32 @@ hooksecurefunc(UF, "Style", ElvUI_EltreumUI.GradientCustomTexture) --if not hook
 hooksecurefunc(UF, "PostUpdateHealthColor", ElvUI_EltreumUI.GradientCustomTexture)
 
 --glow dispellable debuffs
+--local filtercheck = E:GetModule('NamePlates')
 function ElvUI_EltreumUI:UFGlow(object, debuffType, _, wasFiltered)
 	if E.private.unitframe.enable and E.db.ElvUI_EltreumUI.unitframes.UFmodifications then
 		if E.db.ElvUI_EltreumUI.glow.enableUFs then
 			local name = object:GetName()
-			if debuffType and not wasFiltered and not (name == "ElvUF_Player" or name == "ElvUF_Target")then
-				local color = UF.db.colors.debuffHighlight[debuffType]
-				if E.db.ElvUI_EltreumUI.glow.pixel then
-					LCG.PixelGlow_Start(object, {color.r, color.g, color.b, 1}, 7, 0.25, 14, 4, 3, 3, false, nil, 6)
-				elseif E.db.ElvUI_EltreumUI.glow.autocast then
-					LCG.AutoCastGlow_Start(object, {color.r, color.g, color.b, 1}, 8, 0.4, 2, 3, 3)
-				elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-					LCG.ButtonGlow_Start(object, {color.r, color.g, color.b, 1}, 0.5)
-				end
-			else
-				if E.db.ElvUI_EltreumUI.glow.pixel then
-					LCG.PixelGlow_Stop(object)
-				elseif E.db.ElvUI_EltreumUI.glow.autocast then
-					LCG.AutoCastGlow_Stop(object)
-				elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-					LCG.ButtonGlow_Stop(object)
+			if (name ~= "ElvUF_Player" and name ~= "ElvUF_Target") then
+				if debuffType then
+					if not wasFiltered then
+						--local canDispel = filtercheck:StyleFilterDispelCheck(object, debuffType)
+						local color = UF.db.colors.debuffHighlight[debuffType]
+						if E.db.ElvUI_EltreumUI.glow.pixel then
+							LCG.PixelGlow_Start(object, {color.r, color.g, color.b, 1}, 7, 0.25, 14, 4, 3, 3, false, nil, 6)
+						elseif E.db.ElvUI_EltreumUI.glow.autocast then
+							LCG.AutoCastGlow_Start(object, {color.r, color.g, color.b, 1}, 8, 0.4, 2, 3, 3)
+						elseif E.db.ElvUI_EltreumUI.glow.blizzard then
+							LCG.ButtonGlow_Start(object, {color.r, color.g, color.b, 1}, 0.5)
+						end
+					end
+				else
+					if E.db.ElvUI_EltreumUI.glow.pixel then
+						LCG.PixelGlow_Stop(object)
+					elseif E.db.ElvUI_EltreumUI.glow.autocast then
+						LCG.AutoCastGlow_Stop(object)
+					elseif E.db.ElvUI_EltreumUI.glow.blizzard then
+						LCG.ButtonGlow_Stop(object)
+					end
 				end
 			end
 		end
