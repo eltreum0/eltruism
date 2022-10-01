944e637316a412b9384faf4ea85d4fdd7d05e7e8
Eltreum <eltreum2048@gmail.com>
2022-10-01 12:10:03 -0300

    locale

--- ElvUI_EltreumUI/Localization/zhCN.lua
+++ ElvUI_EltreumUI/Localization/zhCN.lua
@@ -1169,3 +1169,4 @@ L["Cardinal Text Offset"] = true
 L["You have another Nameplate Addon installed and loaded, and many nameplate features will not work with it"] = true
 L["Reset Nameplate Style Filters"] = true
 L["Enable adding more functions to ElvUI Chat"] = true
+L["Shows the current stance"] = true
