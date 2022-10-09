18f911aafd7a57fbb1f8661226203605d770b1a2
Eltreum <eltreum2048@gmail.com>
2022-10-09 20:36:33 -0300

    cinematic shadow

--- ElvUI_EltreumUI/Modules/Skins/Shadows.lua
+++ ElvUI_EltreumUI/Modules/Skins/Shadows.lua
@@ -203,6 +203,7 @@ function ElvUI_EltreumUI:Shadows()
 			_G.VoidStorageFrame,
 			_G.BonusRollFrame,
 			_G.ItemRefTooltip,
+			_G.CinematicFrameCloseDialog,
 			--_G.ImmersionFrame.TalkBox,
 		}
 		for _, frame in pairs(blizzardframes) do
