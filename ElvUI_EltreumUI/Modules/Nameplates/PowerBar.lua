ffefd6ef6fbd2f45fe828561548ac8be64f5fc25
Eltreum <eltreum2048@gmail.com>
2022-10-18 18:52:20 -0300

    test

--- ElvUI_EltreumUI/Modules/Nameplates/PowerBar.lua
+++ ElvUI_EltreumUI/Modules/Nameplates/PowerBar.lua
@@ -914,7 +914,8 @@ function ElvUI_EltreumUI:NameplatePower(nameplate)
 						powerbareffect:SetModel(1715069) --better for retail, inspired by asakawa's bar model
 						--powerbareffect:SetModel(1630153)
 						powerbareffect:MakeCurrentCameraCustom()
-						powerbareffect:SetTransform(-0.035, 0, 0, rad(270), 0, 0, 0.785)
+						--powerbareffect:SetTransform(-0.035, 0, 0, rad(270), 0, 0, 0.785)
+						powerbareffect:SetTransform(0, rad(270), 0.785)
 						powerbareffect:SetPortraitZoom(1)
 						powerbareffect:SetAlpha(0.4) --might do this
 					else
