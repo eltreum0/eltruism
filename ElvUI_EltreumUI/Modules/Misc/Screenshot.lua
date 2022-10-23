4e489b15381bab10a99c76013bb0469b61d014bb
Eltreum <eltreum2048@gmail.com>
2022-10-23 18:08:39 -0300

    test

--- ElvUI_EltreumUI/Modules/Misc/Screenshot.lua
+++ ElvUI_EltreumUI/Modules/Misc/Screenshot.lua
@@ -18,3 +18,51 @@ function ElvUI_EltreumUI:AutoScreenshot()
 		end)
 	end
 end
+
+
+
+local healthbar = CreateFrame("StatusBar")
+healthbar:SetValue(0)
+
+healthbar:SetSize(200,20)
+healthbar:SetStatusBarTexture(E.media.blankTex)
+
+healthbar:SetPoint("CENTER", UIParent, "CENTER", 0, 100)
+healthbar:SetStatusBarColor(0, 0.7, 0)
+
+
+local healthbarText = CreateFrame("Frame", nil, healthbar)
+healthbarText:SetPoint("CENTER", healthbar)
+healthbarText:SetWidth(1)
+healthbarText:SetHeight(1)
+healthbarText:SetFrameStrata('DIALOG')
+healthbar.Text = healthbarText:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+healthbar.Text:SetTextColor(1, 1, 1)
+healthbar.Text:SetPoint("CENTER")
+healthbar.Text:SetJustifyH("CENTER")
+healthbar.Text:SetJustifyV("CENTER")
+
+
+local test = CreateFrame("FRAME")
+--test:RegisterEvent("UNIT_HEALTH")
+--test:RegisterEvent("PLAYER_TARGET_CHANGED")
+test:SetScript("OnUpdate", function()
+	if UnitExists("target") then
+		print(UnitHealth("target"))
+		healthbar:SetMinMaxValues(0, UnitHealthMax("Target"))
+		healthbar:SetValue(UnitHealth("target"))
+		healthbar:Show()
+		--healthbar.Text:SetText(UnitHealth("target"))
+		healthbar.Text:SetText(E:GetFormattedText('PERCENT', UnitHealth("target"), UnitHealthMax("target")))
+
+
+
+
+		if UnitHealth("target") < 60000 then
+			healthbar:SetStatusBarTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdroptexture))
+		else
+			healthbar:SetStatusBarTexture(E.media.blankTex)
+		end
+	end
+end)
+
