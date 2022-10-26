d5bba7174b2a7a13a2d71d92e4dc317ddca753d8
Eltreum <eltreum2048@gmail.com>
2022-10-26 03:44:44 -0300

    retail only

--- ElvUI_EltreumUI/Layouts/UpdateSettings.lua
+++ ElvUI_EltreumUI/Layouts/UpdateSettings.lua
@@ -75,7 +75,9 @@ function ElvUI_EltreumUI:UpdateSettings()
 	E.db.ElvUI_EltreumUI.unitframes.uftextureversion = "V2"
 	E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.classdetect = true
 
-	E.db.ElvUI_EltreumUI.unitframes.gradientmode.useUFtexture = true --while the issue is not resolved
+	if E.Retail then
+		E.db.ElvUI_EltreumUI.unitframes.gradientmode.useUFtexture = true --while the issue is not resolved
+	end
 
 	-- Skins
 	E.db.ElvUI_EltreumUI.skins.levelbossinstance = true
