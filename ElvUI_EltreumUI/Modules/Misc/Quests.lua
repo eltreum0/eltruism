300befad9938215b9b65d92092814522de846fa6
Eltreum <eltreum2048@gmail.com>
2022-12-02 09:57:58 -0300

    another therazal

--- ElvUI_EltreumUI/Modules/Misc/Quests.lua
+++ ElvUI_EltreumUI/Modules/Misc/Quests.lua
@@ -341,6 +341,7 @@ function ElvUI_EltreumUI:AutoAcceptQuests()
 						[29155] = true, --magical kingdom of dalaran horde free tp to dalaran
 						[29156] = true, --magical kingdom of dalaran free tp to dalaran
 						[196499] = true, --therazal, aiding the accord quest that drops scaling gear, so should be delayed
+						[199366] = true, --therazal, aiding the accord quest that drops scaling gear, so should be delayed
 					}
 					if ignoredNPCS[NPC_ID] then
 						if E.db.ElvUI_EltreumUI.dev then
