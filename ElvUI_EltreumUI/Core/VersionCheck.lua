65b98492f5462c89febc148f1629194b89d9c351
Eltreum <eltreum2048@gmail.com>
2022-12-02 07:48:47 -0300

    bump elvui

--- ElvUI_EltreumUI/Core/VersionCheck.lua
+++ ElvUI_EltreumUI/Core/VersionCheck.lua
@@ -30,10 +30,10 @@ function ElvUI_EltreumUI:ElvUIVersionCheck()
 		whileDead = 1,
 		hideOnEscape = false,
 	}
-	if E.version < 13.07 then
+	if E.version < 13.08 then
 		E:StaticPopup_Show('ELVUIVERSIONCHECK')
 		ElvUI_EltreumUI:Print("Your ElvUI version is out of date, please update to avoid issues!")
-	elseif E.version > 13.13 then
+	elseif E.version > 13.10 then
 		E:StaticPopup_Show('ELVUIVERSIONCHECK2')
 		ElvUI_EltreumUI:Print("Your ElvUI version is newer than Eltruism, you might run into issues unless you update Eltruism!")
 	end
