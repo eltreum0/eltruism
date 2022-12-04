4347b3cac58237345ca94a227cd57fe3c97ebd28
Eltreum <eltreum2048@gmail.com>
2022-12-04 18:03:14 -0300

    add windtools

--- ElvUI_EltreumUI/Core/Compatibility.lua
+++ ElvUI_EltreumUI/Core/Compatibility.lua
@@ -167,7 +167,107 @@ function ElvUI_EltreumUI:CheckCompatibility()
 			compatibilityran = true
 			addonname = "MerathilisUI"
 		end
+	end
 
+	if IsAddOnLoaded("ElvUI_WindTools") then
+		if E.db.ElvUI_EltreumUI.waypoints.waypointetasetting.enable and (E.private.WT.maps.superTracker.enable or E.private.WT.maps.superTracker.enable == nil) then
+			E.private.WT.maps.superTracker.enable = false
+			E.private.WT.maps.superTracker.noUnit = false
+			E.private.WT.maps.superTracker.waypointParse.command = false
+			E.private.WT.maps.superTracker.waypointParse.enable = false
+			E.private.WT.maps.superTracker.waypointParse.worldMapInput = false
+			E.db.ElvUI_EltreumUI.waypoints.waypointetasetting.enable = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.skins.quests and (E.private.WT.quest.objectiveTracker.enable or E.private.WT.quest.objectiveTracker.enable == nil) then
+			E.private.WT.quest.objectiveTracker.enable = false
+			E.db.ElvUI_EltreumUI.skins.quests = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.waypoints.waypointetasetting.autopin and (E.private.WT.maps.superTracker.autoTrackWaypoint or E.private.WT.maps.superTracker.autoTrackWaypoint == nil) then
+			E.private.WT.maps.superTracker.autoTrackWaypoint = false
+			E.db.ElvUI_EltreumUI.waypoints.waypointetasetting.autopin = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.private.WT.maps.worldMap.scale.enable or E.private.WT.maps.worldMap.scale.enable == nil then
+			E.private.WT.maps.worldMap.scale.enable = false
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.skins.shadow.enable and (E.private.WT.skins.shadow or E.private.WT.skins.shadow == nil) then
+			E.private.WT.skins.shadow = false
+			E.db.ElvUI_EltreumUI.skins.shadow.enable = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.otherstuff.eltruismroleicons and (E.private.WT.unitFrames.roleIcon.enable or E.private.WT.unitFrames.roleIcon.enable == nil) then
+			E.private.WT.unitFrames.roleIcon.enable = false
+			E.db.ElvUI_EltreumUI.otherstuff.eltruismroleicons = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.loot.loottext.combatindicator and (E.db.WT.combat.combatAlert.text or E.db.WT.combat.combatAlert.text == nil) then
+			E.db.WT.combat.combatAlert.text = false
+			E.db.ElvUI_EltreumUI.loot.loottext.combatindicator = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.otherstuff.mpluskeys and (E.db.WT.combat.quickKeystone.enable or E.db.WT.combat.quickKeystone.enable == nil) then
+			E.db.WT.combat.quickKeystone.enable = false
+			E.db.ElvUI_EltreumUI.otherstuff.mpluskeys = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if (E.db.ElvUI_EltreumUI.loot.fastloot or E.db.ElvUI_EltreumUI.loot.lootwishlistwarning or E.db.ElvUI_EltreumUI.loot.fastlootfilter or E.db.ElvUI_EltreumUI.loot.lootwishlistfilter) and (E.db.WT.item.fastLoot.enable or E.db.WT.item.fastLoot.enable == nil) then
+			E.db.WT.item.fastLoot.enable = false
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.otherstuff.delete and (E.db.WT.item.delete.enable or E.db.WT.item.delete.enable == nil) then
+			E.db.WT.item.delete.enable = false
+			E.db.ElvUI_EltreumUI.otherstuff.delete = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.quests.autoaccept and (E.db.WT.quest.turnIn.enable or E.db.WT.quest.turnIn.enable == nil) then
+			E.db.WT.quest.turnIn.enable = false
+			E.db.ElvUI_EltreumUI.quests.autoaccept = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.chat.itemlevels and (E.db.WT.social.chatLink.level or E.db.WT.social.chatLink.level == nil) then
+			E.db.WT.social.chatLink.level = false
+			E.db.ElvUI_EltreumUI.chat.itemlevels = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+
+		if E.db.ElvUI_EltreumUI.chat.looticons and (E.db.WT.social.chatLink.icon or E.db.WT.social.chatLink.icon == nil) then
+			E.db.WT.social.chatLink.icon = false
+			E.db.WT.social.chatLink.enable = false
+			E.db.WT.social.chatText.enable = false
+			E.db.ElvUI_EltreumUI.chat.looticons = true
+			compatibilityran = true
+			addonname = "WindTools"
+		end
+	end
+
+	if IsAddOnLoaded("ElvUI_MerathilisUI") and IsAddOnLoaded("ElvUI_WindTools") and compatibilityran then
+		addonname = "MerathilisUI and WindTools"
 	end
 
 	E.PopupDialogs["ELTRUISMCOMPATIBILITYFIX"] = {
