0f0605197f2db5f8828b22072861ed030b26ba0c
Eltreum <eltreum2048@gmail.com>
2022-12-26 05:28:17 -0300

    locale

--- ElvUI_EltreumUI/Localization/zhCN.lua
+++ ElvUI_EltreumUI/Localization/zhCN.lua
@@ -1210,3 +1210,5 @@ L["Shuffle"] = true
 L["Randomize Music Order"] = true
 L["Shuffle List"] = true
 L["Split files with a comma, such as: file1.mp3,file2.mp3"] = true
+L["Enable World Map Scaling"] = true
+L["World Map Scale"] = true
