0e8b45a5358b2b3e2680666091b14611bf8ed94c
Eltreum <eltreum2048@gmail.com>
2023-01-18 05:02:39 -0300

    fix mirror timers in wrath

--- ElvUI_EltreumUI/Modules/Skins/Other/Other.lua
+++ ElvUI_EltreumUI/Modules/Skins/Other/Other.lua
@@ -140,19 +140,19 @@ function S:HandleMirrorTimer() --(timer, value, maxvalue, scale, paused, label)
 					["FEIGNDEATH"] = "UI-CastingBar-Filling-Channel", --green
 				}
 				if atlas == MirrorTimerAtlasCheck["EXHAUSTION"] then
-					if E.Retail then
+					if E.Retail or E.Wrath then
 						frame.StatusBar:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=1,g= 0.68,b= 0,a= alpha}, {r=1,g= 0.83,b= 0.25,a= alpha})
 					else
 						frame.StatusBar:GetStatusBarTexture():SetGradientAlpha("HORIZONTAL", 1, 0.68, 0, alpha, 1, 0.83, 0.25, alpha)
 					end
 				elseif atlas == MirrorTimerAtlasCheck["BREATH"] then
-					if E.Retail then
+					if E.Retail or E.Wrath then
 						frame.StatusBar:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=0,g= 0.33,b= 0.53,a= alpha}, {r=0.49,g= 0.87,b= 1,a= alpha})
 					else
 						frame.StatusBar:GetStatusBarTexture():SetGradientAlpha("HORIZONTAL", 0, 0.33, 0.53, alpha, 0.49, 0.87, 1, alpha)
 					end
 				elseif atlas == MirrorTimerAtlasCheck["FEIGNDEATH"] then
-					if E.Retail then
+					if E.Retail or E.Wrath then
 						frame.StatusBar:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=0.01,g= 0.6,b= 0.36,a= alpha}, {r=0,g= 1,b= 0.58,a= alpha})
 					else
 						frame.StatusBar:GetStatusBarTexture():SetGradientAlpha("HORIZONTAL", 0.01, 0.6, 0.36, alpha, 0, 1, 0.58, alpha)
@@ -177,7 +177,7 @@ function ElvUI_EltreumUI:GradientMirrorLoot()
 				if statusBar then
 					statusBar:HookScript("OnShow", function()
 						local r,g,b,a = statusBar:GetStatusBarColor()
-						if E.Retail then
+						if E.Wrath then
 							statusBar:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=r - 0.3,g= g - 0.3,b= b - 0.3,a= a}, {r=r + 0.2,g= g + 0.2,b= b + 0.2,a= a})
 						else
 							statusBar:GetStatusBarTexture():SetGradientAlpha("HORIZONTAL", r - 0.3, g - 0.3, b - 0.3, a, r + 0.2, g + 0.2, b + 0.2, a)
@@ -201,7 +201,7 @@ function ElvUI_EltreumUI:GradientMirrorLoot()
 				if not frame.GradientHook then
 					frame:HookScript("OnShow", function()
 						local r,g,b = frame.status:GetStatusBarColor()
-						if E.Retail then
+						if E.Retail or E.Wrath then
 							frame.status:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=r - 0.5,g= g - 0.5,b= b - 0.5,a= 1}, {r=r + 0.3,g= g + 0.3,b= b + 0.3,a= 1})
 						else
 							frame.status:GetStatusBarTexture():SetGradientAlpha("HORIZONTAL", r - 0.5, g - 0.5, b - 0.5, 1, r + 0.3, g + 0.3, b + 0.3, 1)
