aff8d8eedbaf01fdcc3fd93212ae4d2ff5f070bc
Eltreum <eltreum2048@gmail.com>
2023-01-26 12:07:47 -0300

    workaround name only by acidweb/meorawr

--- ElvUI_EltreumUI/Modules/Nameplates/NamePlates.lua
+++ ElvUI_EltreumUI/Modules/Nameplates/NamePlates.lua
@@ -704,10 +704,24 @@ function ElvUI_EltreumUI:FriendlyNameplates()
 				end
 			end
 		end
-		if not E.Retail and E.db.ElvUI_EltreumUI.nameplates.friendlynameplatetoggle.friendlynames then
+		if E.db.ElvUI_EltreumUI.nameplates.friendlynameplatetoggle.friendlynames then
 			if instanceType == "party" or instanceType == "raid" or instanceType == "pvp" or instanceType == "arena" or instanceType == "scenario" or instanceType == "none" or mapID == 1662 or mapID == 582 or mapID == 590 then
-				if nameplateShowOnlyNames == "0" then
-					SetCVar("nameplateShowOnlyNames", 1)
+				if E.Retail then
+					C_CVar.RegisterCVar("nameplateShowOnlyNames")
+					hooksecurefunc(NamePlateDriverFrame, "UpdateNamePlateOptions", function()
+						if GetCVarBool("nameplateShowOnlyNames") then
+							TableUtil.TrySet(DefaultCompactNamePlateFrameSetUpOptions, "hideHealthbar")
+							TableUtil.TrySet(DefaultCompactNamePlateFrameSetUpOptions, "hideCastbar")
+							TableUtil.TrySet(DefaultCompactNamePlateFriendlyFrameOptions, "hideHealthbar")
+							TableUtil.TrySet(DefaultCompactNamePlateFriendlyFrameOptions, "hideCastbar")
+							TableUtil.TrySet(DefaultCompactNamePlateEnemyFrameOptions, "hideHealthbar")
+							TableUtil.TrySet(DefaultCompactNamePlateEnemyFrameOptions, "hideCastbar")
+						end
+					end)
+				else
+					if nameplateShowOnlyNames == "0" then
+						SetCVar("nameplateShowOnlyNames", 1)
+					end
 				end
 			end
 		end
