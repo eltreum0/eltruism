ac2482029baa61ea2960f962444ff83d73d9029b
Eltreum <eltreum2048@gmail.com>
2023-01-29 22:07:00 -0300

    update ids, reformat (ty blinkii)

--- ElvUI_EltreumUI/Modules/Nameplates/ClassificationIcons.lua
+++ ElvUI_EltreumUI/Modules/Nameplates/ClassificationIcons.lua
@@ -7,50 +7,90 @@ local elementAdded = false
 local bossIDs = {
     --DF
     --AA
-    [194181] = true,
-    [191736] = true,
-    [196482] = true,
-    [190609] = true,
+    ["194181"] = true,
+    ["191736"] = true,
+    ["196482"] = true,
+    ["190609"] = true,
     --BH
-    [186122] = true,
-    [186124] = true,
-    [186125] = true,
-    [186120] = true,
-    [186121] = true,
-    [186121] = true,
-    [186116] = true,
+    ["186122"] = true,
+    ["186124"] = true,
+    ["186125"] = true,
+    ["186120"] = true,
+    ["186121"] = true,
+    ["186116"] = true,
     --HOF
-    [189719] = true,
-    [189729] = true,
-    [189722] = true,
-    [189727] = true,
+    ["189719"] = true,
+    ["189729"] = true,
+    ["189722"] = true,
+    ["189727"] = true,
     --NEL
-    [189901] = true,
-    [189478] = true,
-    [181861] = true,
-    [189340] = true,
+    ["189901"] = true,
+    ["189478"] = true,
+    ["181861"] = true,
+    ["189340"] = true,
     --RLP
-    [189232] = true,
-    [188252] = true,
-    [199790] = true,
-    [199791] = true,
+    ["188252"] = true,
+    ["189232"] = true,
+    ["190485"] = true,
+	["190484"] = true,
     --TAV
-    [186738] = true,
-    [186644] = true,
-    [186739] = true,
-    [199614] = true,
+    ["186738"] = true,
+    ["186644"] = true,
+    ["186739"] = true,
+    ["199614"] = true,
     --TNO
-    [186615] = true,
-    [195723] = true,
-    [186338] = true,
-    [186151] = true,
-    [186616] = true,
+    ["186615"] = true,
+    ["195723"] = true,
+    ["186338"] = true,
+    ["186151"] = true,
+    ["186616"] = true,
+	["186339"] = true,
     --ULD
-    [184125] = true,
-    [184124] = true,
-    [184018] = true,
-    [184422] = true,
+    ["184125"] = true,
+    ["184124"] = true,
+    ["184018"] = true,
+    ["184422"] = true,
+	-- DF S1
+	--HOV
+	["95674"] = true,
+	["99868"] = true,
+	["94960"] = true,
+	["95833"] = true,
+	["95675"] = true,
+	["95676"] = true,
+	-- COS
+	["104218"] = true,
+	["104217"] = true,
+	["104215"] = true,
+	-- SBG
+	["76407"] = true,
+	["75452"] = true,
+	["75509"] = true,
+	-- TJS
+	["56448"] = true,
+	["59051"] = true,
+	["59726"] = true,
+	["56732"] = true,
+	["56439"] = true,
+	-- raid VOTI
+	["190245"] = true,
+	["184972"] = true,
+	["189492"] = true,
+	["189813"] = true,
+	["187771"] = true,
+	["181378"] = true,
+	["187967"] = true,
+	["190496"] = true,
 }
+
+local textureDB = {
+		["TYPE1"] = "Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star.tga",
+		["TYPE2"] = "Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star2.tga",
+		["TYPE3"] = "Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star3.tga",
+		["TYPE4"] = "Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon.tga",
+		["TYPE5"] = "Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon2.tga",
+		["TYPE6"] = "Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\treasure.tga"
+	}
 
 local function Update(self)
 	local element = self.EltruismClassificationIndicator
@@ -60,83 +100,45 @@ local function Update(self)
 	end
 
 	if E.db.ElvUI_EltreumUI.dev then
-		ElvUI_EltreumUI:Print("npcID: ",self.npcID)
+		ElvUI_EltreumUI:Print("npcID: ", self.npcID)
 	end
 
 	local classification = self.classification
+
 	if classification == 'worldboss' or bossIDs[self.npcID] then
-		if E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "TYPE1" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "TYPE2" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "TYPE3" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star3.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "TYPE4" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "TYPE5" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "TYPE6" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\treasure.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "CUSTOM" then
+		if E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss == "CUSTOM" then
 			element:SetTexture([[Interface\AddOns\]]..E.db.ElvUI_EltreumUI.nameplates.classification.customboss)
+		else
+			element:SetTexture(textureDB[E.db.ElvUI_EltreumUI.nameplates.classification.icontypeboss])
 		end
 		-- color
 		element:SetVertexColor(E.db.ElvUI_EltreumUI.nameplates.classification.bossR, E.db.ElvUI_EltreumUI.nameplates.classification.bossG, E.db.ElvUI_EltreumUI.nameplates.classification.bossB, 1)
 		element:Show()
 	elseif classification == 'elite' then
-		if E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "TYPE1" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "TYPE2" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "TYPE3" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star3.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "TYPE4" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "TYPE5" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "TYPE6" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\treasure.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "CUSTOM" then
+		if E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite == "CUSTOM" then
 			element:SetTexture([[Interface\AddOns\]]..E.db.ElvUI_EltreumUI.nameplates.classification.customelite)
+		else
+			element:SetTexture(textureDB[E.db.ElvUI_EltreumUI.nameplates.classification.icontypeelite])
 		end
 		-- color
 		element:SetVertexColor(E.db.ElvUI_EltreumUI.nameplates.classification.eliteR, E.db.ElvUI_EltreumUI.nameplates.classification.eliteG, E.db.ElvUI_EltreumUI.nameplates.classification.eliteB, 1)
 		element:Show()
 	elseif classification == 'rareelite' then
-		if E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "TYPE1" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "TYPE2" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "TYPE3" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star3.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "TYPE4" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "TYPE5" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "TYPE6" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\treasure.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "CUSTOM" then
+		if E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite == "CUSTOM" then
 			element:SetTexture([[Interface\AddOns\]]..E.db.ElvUI_EltreumUI.nameplates.classification.customrareelite)
+		else
+			element:SetTexture(textureDB[E.db.ElvUI_EltreumUI.nameplates.classification.icontyperareelite])
 		end
 		element:SetVertexColor(E.db.ElvUI_EltreumUI.nameplates.classification.rareeliteR, E.db.ElvUI_EltreumUI.nameplates.classification.rareeliteG, E.db.ElvUI_EltreumUI.nameplates.classification.rareeliteB, 1)
 		--element:SetAtlas('nameplates-icon-elite-silver')
 		element:Show()
 	elseif classification == 'rare' then
-		if E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "TYPE1" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "TYPE2" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "TYPE3" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\star3.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "TYPE4" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "TYPE5" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\dragon2.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "TYPE6" then
-			element:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\treasure.tga")
-		elseif E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "CUSTOM" then
+		if E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare == "CUSTOM" then
 			element:SetTexture([[Interface\AddOns\]]..E.db.ElvUI_EltreumUI.nameplates.classification.customrare)
+		else
+			element:SetTexture(textureDB[E.db.ElvUI_EltreumUI.nameplates.classification.icontyperare])
 		end
+
 		element:SetVertexColor(E.db.ElvUI_EltreumUI.nameplates.classification.rareR, E.db.ElvUI_EltreumUI.nameplates.classification.rareG, E.db.ElvUI_EltreumUI.nameplates.classification.rareB, 1)
 		element:Show()
 	else
