16ed5eb423341e77c4248fdbb109b094d0dd7734
Eltreum <eltreum2048@gmail.com>
2023-02-12 11:03:41 -0300

    check for instance

--- ElvUI_EltreumUI/Modules/Misc/Maps.lua
+++ ElvUI_EltreumUI/Modules/Misc/Maps.lua
@@ -18,8 +18,10 @@ local UiMapPoint = _G.UiMapPoint
 
 --Conversion of Time to Arrive weakaura (new version)
 local EltruismAutopin = CreateFrame("Frame", "EltruismAutoPin")
+
 local EltruismTimeToArriveParent = CreateFrame("Frame", "EltruismTimeToArriveParent")
 EltruismTimeToArriveParent:RegisterEvent("PLAYER_ENTERING_WORLD")
+
 local EltruismTimeToArrive = CreateFrame("Frame", "EltruismTimeToArrive", UIParent)
 EltruismTimeToArrive.TimeText = EltruismTimeToArrive:CreateFontString(nil, "BACKGROUND", "GameFontNormal")
 EltruismTimeToArrive.TimeText:SetJustifyV("TOP")
@@ -67,8 +69,9 @@ function ElvUI_EltreumUI:WaypointTimeToArrive()
 		EltruismTimeToArriveParent:RegisterEvent("SUPER_TRACKING_CHANGED")
 		--EltruismTimeToArriveParent:SetScript("OnEvent", function(_, event)
 		EltruismTimeToArriveParent:SetScript("OnEvent", function()
-			--print(event.."waypoint")
-			if C_Map.HasUserWaypoint() == true or C_SuperTrack.IsSuperTrackingAnything() == true then
+			local _, instanceType = IsInInstance()
+			--print(instanceType,event,"waypoint")
+			if (C_Map.HasUserWaypoint() == true or C_SuperTrack.IsSuperTrackingAnything() == true) and (instanceType == "none") then
 				--use throttled onupdate to udpate the text (once per second)
 				EltruismTimeToArrive:SetScript("OnUpdate", function(self, elapsed)
 					--print("onupdate spam"..math.random(1,99))
