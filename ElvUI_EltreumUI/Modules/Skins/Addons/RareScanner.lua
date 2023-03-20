cdbc7e7524e19a2790249232dee3451b57522d9a
Eltreum <eltreum2048@gmail.com>
2023-03-20 00:55:22 -0300

    forgot glues replaced these, skin them + add a delay
    and add a + and - texture since i needed the minus for the button

--- ElvUI_EltreumUI/Modules/Skins/Addons/RareScanner.lua
+++ ElvUI_EltreumUI/Modules/Skins/Addons/RareScanner.lua
@@ -16,6 +16,22 @@ function ElvUI_EltreumUI:EltruismRareScanner()
 			--button is too small sometimes, resize
 			_G["scanner_button"].CloseButton:SetSize(25,25)
 
+			-- the minus button
+			if _G["scanner_button"].FilterEntityButton then
+				S:HandleButton(_G["scanner_button"].FilterEntityButton)
+				_G["scanner_button"].FilterEntityButton:SetNormalTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\minus")
+				_G["scanner_button"].FilterEntityButton:SetPushedTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\minus")
+			end
+
+			--the skull
+			if _G["scanner_button"].UnfilterEnabledButton then
+				S:HandleButton(_G["scanner_button"].UnfilterEnabledButton)
+				_G["scanner_button"].UnfilterEnabledButton:SetNormalTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\skull5")
+				_G["scanner_button"].UnfilterEnabledButton:GetNormalTexture():SetTexCoord(0, 1, 0, 1)
+				_G["scanner_button"].UnfilterEnabledButton:SetPushedTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Nameplates\\skull5")
+				_G["scanner_button"].UnfilterEnabledButton:GetPushedTexture():SetTexCoord(0, 1, 0, 1)
+			end
+
 			if E.db.ElvUI_EltreumUI.skins.shadow.enable then
 				if not E.Classic then
 					if _G["scanner_button"] and not _G["scanner_button"].shadow then
@@ -35,17 +51,20 @@ function ElvUI_EltreumUI:EltruismRareScanner()
 
 		hooksecurefunc(scanner_button,"ShowButton",function()
 			if _G["LootBar"] then
-				for _, v in pairs{_G["LootBar"]:GetChildren()} do
-					if v and v.Icon and not v.EltruismSkin then
-						--v.Icon:SetTexCoord(unpack(E.TexCoords))
-						S:HandleFrame(v)
-						if not v.shadow then
-							v:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
-							if EnhancedShadows then EnhancedShadows:RegisterShadow(v.shadow) end
+				E:Delay(0.1, function()
+					for _, v in pairs{_G["LootBar"]:GetChildren()} do
+						if v and v.Icon and not v.EltruismSkin then
+							v.Icon:SetTexCoord(unpack(E.TexCoords))
+							--S:HandleFrame(v) --can end up erasing icon
+							v:CreateBackdrop()
+							if E.db.ElvUI_EltreumUI.skins.shadow.enable and not v.shadow then
+								v:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
+								if EnhancedShadows then EnhancedShadows:RegisterShadow(v.shadow) end
+							end
+							v.EltruismSkin = true
 						end
-						v.EltruismSkin = true
 					end
-				end
+				end)
 			end
 		end)
 
