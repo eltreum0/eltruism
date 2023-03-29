4199a4e86d994665c96815576a278a54554f8ffb
Eltreum <eltreum2048@gmail.com>
2023-03-29 04:55:58 -0300

    fix paladin bar not hiding due to typo

--- ElvUI_EltreumUI/Modules/Nameplates/NamePlates.lua
+++ ElvUI_EltreumUI/Modules/Nameplates/NamePlates.lua
@@ -529,7 +529,7 @@ function ElvUI_EltreumUI:NamePlateOptions()
 					--print(id)
 					if E.myclass == 'DEATHKNIGHT' then
 						_G["ElvNP_TargetClassPowerRunes"]:Show()
-					elseif E.myclass == 'PALADIN ' or E.myclass == 'ROGUE' or E.myclass == 'WARLOCK' or E.myclass == 'EVOKER' then
+					elseif E.myclass == 'PALADIN' or E.myclass == 'ROGUE' or E.myclass == 'WARLOCK' or E.myclass == 'EVOKER' then
 						_G["ElvNP_TargetClassPowerClassPower"]:Show()
 					elseif E.myclass == 'DRUID' then
 						stance = GetShapeshiftForm()
@@ -567,7 +567,7 @@ function ElvUI_EltreumUI:NamePlateOptions()
 				if E.Retail then
 					if E.myclass == 'DEATHKNIGHT' then
 						_G["ElvNP_TargetClassPowerRunes"]:Hide()
-					elseif E.myclass == 'PALADIN ' or E.myclass == 'ROGUE' or E.myclass == 'WARLOCK' or E.myclass == 'EVOKER' then
+					elseif E.myclass == 'PALADIN' or E.myclass == 'ROGUE' or E.myclass == 'WARLOCK' or E.myclass == 'EVOKER' then
 						_G["ElvNP_TargetClassPowerClassPower"]:Hide()
 					elseif E.myclass == 'DRUID' then
 							_G["ElvNP_TargetClassPowerClassPower"]:Hide()
