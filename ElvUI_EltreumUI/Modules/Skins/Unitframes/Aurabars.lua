321ad586663fa526a70067c91281dbe8a20d6689
Eltreum <eltreum2048@gmail.com>
2023-04-05 02:48:44 -0300

    fix target shadows, rework function a bit

--- ElvUI_EltreumUI/Modules/Skins/Unitframes/Aurabars.lua
+++ ElvUI_EltreumUI/Modules/Skins/Unitframes/Aurabars.lua
@@ -5,15 +5,12 @@ local hooksecurefunc = _G.hooksecurefunc
 
 --Gradient Aurabars
 function ElvUI_EltreumUI:AuraBarGradient(unit, bar, _, _, _, _, debuffType, isStealable) --could use isStealable to add a glow or something
+	if not unit and not bar then return end
 	if E.db.ElvUI_EltreumUI.unitframes.UFmodifications and bar and unit then
-		--bar:SetStatusBarTexture(E.LSM:Fetch("statusbar", E.db.unitframe.statusbar)) --causes issues in 10.0, have to split
-		--[[if isStealable then --maybe later
-			LCG.PixelGlow_Start(bar, skillglowcolor, 7, 0.25, 14, 4, 3, 3, false, nil, 6)
-		end]]
 		if E.db.ElvUI_EltreumUI.unitframes.gradientmode.enableaurabars then
 			local r,g,b = bar:GetStatusBarColor()
 			if unit == "player" then
-				if E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.enableaurabars then
+				if E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.enableaurabars and not bar.EltruismSparkPlayer then
 					bar.spark:SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.texture))
 					if E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.texture == 'Eltreum-Fade' and not E.db.unitframe.units.player.aurabar.reverseFill then --flip otherwise it will look wrong
 						bar.spark:SetTexCoord(1, 0, 0, 1)
@@ -21,22 +18,23 @@ function ElvUI_EltreumUI:AuraBarGradient(unit, bar, _, _, _, _, debuffType, isSt
 					bar.spark:SetBlendMode('BLEND')
 					--bar.spark:SetVertexColor(E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.r, E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.g, E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.b, 1)
 					bar.spark:SetWidth(E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.width)
+					bar.EltruismSparkPlayer = true
 				end
-				if E.db.unitframe.colors.transparentAurabars then
-					if E.Retail or E.Wrath then
+				if E.Retail or E.Wrath then
+					if E.db.unitframe.colors.transparentAurabars then
 						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, {r=r-0.3,g= g-0.3,b=b-0.3,a= E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha}, {r=r,g= g,b= b,a= E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha})
 					else
-						bar:GetStatusBarTexture():SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, r-0.3, g-0.3, b-0.3, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha, r, g, b, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha)
+						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, {r=r-0.3,g= g-0.3,b= b-0.3,a= 1}, {r=r,g= g,b= b,a= 1})
 					end
 				else
-					if E.Retail or E.Wrath then
-						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, {r=r-0.3,g= g-0.3,b= b-0.3,a= 1}, {r=r,g= g,b= b,a= 1})
+					if E.db.unitframe.colors.transparentAurabars then
+						bar:GetStatusBarTexture():SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, r-0.3, g-0.3, b-0.3, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha, r, g, b, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha)
 					else
 						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, r-0.3, g-0.3, b-0.3, r, g, b)
 					end
 				end
 			elseif unit == "target" then
-				if E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.enableaurabars then
+				if E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.enableaurabars and not bar.EltruismSparkTarget then
 					bar.spark:SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.texture))
 					if E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.texture == 'Eltreum-Fade' and not E.db.unitframe.units.target.aurabar.reverseFill then --flip otherwise it will look wrong
 						bar.spark:SetTexCoord(1, 0, 0, 1)
@@ -44,16 +42,17 @@ function ElvUI_EltreumUI:AuraBarGradient(unit, bar, _, _, _, _, debuffType, isSt
 					bar.spark:SetBlendMode('BLEND')
 					--bar.spark:SetVertexColor(E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.r, E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.g, E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.b, 1)
 					bar.spark:SetWidth(E.db.ElvUI_EltreumUI.unitframes.sparkcustomcolor.width)
+					bar.EltruismSparkTarget = true
 				end
-				if E.db.unitframe.colors.transparentAurabars then
-					if E.Retail or E.Wrath then
+				if E.Retail or E.Wrath then
+					if E.db.unitframe.colors.transparentAurabars then
 						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, {r=r,g= g,b= b,a= E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha}, {r=r-0.3,g= g-0.3,b= b-0.3,a= E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha})
 					else
-						bar:GetStatusBarTexture():SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, r, g, b, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha, r-0.3, g-0.3, b-0.3, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha)
+						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, {r=r,g= g,b= b,a= 1}, {r=r-0.3,g= g-0.3,b= b-0.3,a= 1})
 					end
 				else
-					if E.Retail or E.Wrath then
-						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, {r=r,g= g,b= b,a= 1}, {r=r-0.3,g= g-0.3,b= b-0.3,a= 1})
+					if E.db.unitframe.colors.transparentAurabars then
+						bar:GetStatusBarTexture():SetGradientAlpha(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, r, g, b, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha, r-0.3, g-0.3, b-0.3, E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdropalpha)
 					else
 						bar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, r, g, b, r-0.3, g-0.3, b-0.3)
 					end
@@ -68,7 +67,7 @@ hooksecurefunc(UF, "PostUpdateBar_AuraBars", ElvUI_EltreumUI.AuraBarGradient)
 function ElvUI_EltreumUI:AuraBarTexture(frame)
 	if not frame then return end
 	if E.db.ElvUI_EltreumUI.unitframes.UFmodifications and E.db.ElvUI_EltreumUI.unitframes.gradientmode.enableaurabars then
-		if frame and frame.AuraBars and not self.AuraBarHook then
+		if frame and frame.AuraBars and not frame.AuraBarHook then
 			hooksecurefunc(frame.AuraBars, 'PostUpdateBar', function(_,_,bar)
 				if bar then
 					if E.db.ElvUI_EltreumUI.skins.shadow.enable and E.db.ElvUI_EltreumUI.skins.shadow.aura and not bar.shadow then
@@ -86,7 +85,7 @@ function ElvUI_EltreumUI:AuraBarTexture(frame)
 					end
 				end
 			end)
-			self.AuraBarHook = true
+			frame.AuraBarHook = true
 		end
 	end
 end
