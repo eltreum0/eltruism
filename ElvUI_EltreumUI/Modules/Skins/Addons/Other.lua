3f916bc0a124c1b26fcdd7180d8b9948d8cbbe85
Eltreum <eltreum2048@gmail.com>
2023-04-08 23:49:27 -0300

    not used

--- ElvUI_EltreumUI/Modules/Skins/Addons/Other.lua
+++ ElvUI_EltreumUI/Modules/Skins/Addons/Other.lua
@@ -2,7 +2,6 @@ local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule('Skins')
 local _G = _G
 local hooksecurefunc = _G.hooksecurefunc
-local next = _G.next
 local tabSkinned = false
 
 --simple tab skin
