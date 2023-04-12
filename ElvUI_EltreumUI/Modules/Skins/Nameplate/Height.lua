3d0f67626fb037b3814808989a98f61b04ac0cb5
Eltreum <eltreum2048@gmail.com>
2023-04-11 22:36:58 -0300

    Revert "test explosive np glow"
    This reverts commit f15c5e3ea59130c013de295e413a34e55c8698c1.

--- ElvUI_EltreumUI/Modules/Skins/Nameplate/Height.lua
+++ ElvUI_EltreumUI/Modules/Skins/Nameplate/Height.lua
@@ -28,40 +28,8 @@ tableupdate:SetScript("OnEvent",function()
 		["PLAYER"] = E.db.nameplates.units.PLAYER.health.height or P.nameplates.units.PLAYER.health.height,
 	}
 end)
-
-local LCG = E.Libs.CustomGlow
-local classcolor = E:ClassColor(E.myclass, true)
-local glowcolor
-
-
 function ElvUI_EltreumUI:NameplateCustomOptions(unit)
 	if (E.db.ElvUI_EltreumUI.nameplates.nameplateOptions.enableHealthHeight) and unit and unit.unit and unit.unit:match("nameplate") then
-
-		--test np glow
-		if E.db.ElvUI_EltreumUI.nameplates.nameplateOptions.npglow then
-			if (unit.unitGUID and unit.unitGUID:match("-120651-")) then
-				if E.db.ElvUI_EltreumUI.glow.colorclassnp then
-					glowcolor = {classcolor.r, classcolor.g, classcolor.b, 1}
-				else
-					glowcolor = {E.db.ElvUI_EltreumUI.glow.glowcustomcolornp.r, E.db.ElvUI_EltreumUI.glow.glowcustomcolornp.g, E.db.ElvUI_EltreumUI.glow.glowcustomcolornp.b, 1}
-				end
-				if E.db.ElvUI_EltreumUI.glow.pixel then
-					LCG.PixelGlow_Start(unit.Health, glowcolor, E.db.ElvUI_EltreumUI.glow.numberpixel, E.db.ElvUI_EltreumUI.glow.frequencypixel, E.db.ElvUI_EltreumUI.glow.lengthpixel, E.db.ElvUI_EltreumUI.glow.thicknesspixel, E.db.ElvUI_EltreumUI.glow.pixelxOffset, E.db.ElvUI_EltreumUI.glow.pixelyOffset, E.db.ElvUI_EltreumUI.glow.borderpixel, nil, 6)
-				elseif E.db.ElvUI_EltreumUI.glow.autocast then
-					LCG.AutoCastGlow_Start(unit.Health, glowcolor, E.db.ElvUI_EltreumUI.glow.numberauto, E.db.ElvUI_EltreumUI.glow.frequencyauto, E.db.ElvUI_EltreumUI.glow.autoscale, E.db.ElvUI_EltreumUI.glow.autoxOffset, E.db.ElvUI_EltreumUI.glow.autoyOffset)
-				elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-					LCG.ButtonGlow_Start(unit.Health, glowcolor, E.db.ElvUI_EltreumUI.glow.frequencyblizz)
-				end
-			else
-				if E.db.ElvUI_EltreumUI.glow.pixel then
-					LCG.PixelGlow_Stop(unit.Health)
-				elseif E.db.ElvUI_EltreumUI.glow.autocast then
-					LCG.AutoCastGlow_Stop(unit.Health)
-				elseif E.db.ElvUI_EltreumUI.glow.blizzard then
-					LCG.ButtonGlow_Stop(unit.Health)
-				end
-			end
-		end
 
 		--check if its not explosive
 		if (unit.unitGUID and unit.unitGUID:match("-120651-")) or UnitIsUnit(unit.unit,"player") then
