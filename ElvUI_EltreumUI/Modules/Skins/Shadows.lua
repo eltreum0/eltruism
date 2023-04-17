594ddeec5a72e913d4cd3d7de5b924dc815e476d
Eltreum <eltreum2048@gmail.com>
2023-04-17 18:26:34 -0300

    clock shadows

--- ElvUI_EltreumUI/Modules/Skins/Shadows.lua
+++ ElvUI_EltreumUI/Modules/Skins/Shadows.lua
@@ -764,6 +764,7 @@ function ElvUI_EltreumUI:Shadows()
 				_G.FriendsTooltip,
 				_G.AddFriendFrame,
 				_G.MinimapRightClickMenu,
+				_G.TimeManagerFrame,
 			}
 			for _, frame in pairs(blizzardframes) do
 				if frame and not frame.shadow then
