49338dff43ac324b184c04f0323c0ed12ebfc7cb
Eltreum <eltreum2048@gmail.com>
2023-04-19 21:00:59 -0300

    loot

--- ElvUI_EltreumUI/Core/tempach.lua
+++ ElvUI_EltreumUI/Core/tempach.lua
@@ -1148,6 +1148,116 @@ function ElvUI_EltreumUI:Configtable()
 	ElvUI_EltreumUI.Options.args.cursor.args.texture.args.ring10 = ACH:Execute(L["Type 10"], L["The Cursor will use this type as its texture"], 12, function() E.db.ElvUI_EltreumUI.cursors.cursor.ring = 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Cursor\\ring10.tga' ElvUI_EltreumUI:CastCursor() end)
 	ElvUI_EltreumUI.Options.args.cursor.args.texture.args.ring10.image = 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Cursor\\ring10'
 
+	ElvUI_EltreumUI.Options.args.loot = ACH:Group(E:TextGradient(LOOT, 0.50, 0.70, 1, 0.67, 0.95, 1), L["Add a scrolling loot text that will show items that you loot, filter which items you can loot, enable fast looting and create your own item wishlist"], 85, 'tab')
+	ElvUI_EltreumUI.Options.args.loot.icon = 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Icons\\loot'
+	ElvUI_EltreumUI.Options.args.loot.args.loottext = ACH:Group(L["LootText"], nil, 1)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.setting = ACH:Group("", nil, 1)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.setting.inline = true
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.setting.args.header1 = ACH:Description(L["Show a floating loot text"], 1, nil, nil, nil, nil, nil, "full")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.setting.args.enable = ACH:Toggle(L["Enable"], L["Toggle On and Off"], 9, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.enable end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.enable = value E:StaticPopup_Show('CONFIG_RL') end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.types = ACH:Group(L["Text Types"], nil, 1, "tab")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.types.args.honor = ACH:Toggle(HONOR, nil, 1, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.honor end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.honor = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.types.args.currency = ACH:Toggle(CURRENCY, nil, 1, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.currency end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.currency = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.types.args.pet = ACH:Toggle(L["Pets"], nil, 1, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.pet end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.pet = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.types.args.skill = ACH:Toggle(E.NewSign..SKILL, nil, 1, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.skill end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.skill = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.filter = ACH:Group(L["Filter"], nil, 1, "tab")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.filter.args.quality = ACH:Select(L["Minimum Loot Quality Filter"], L["Only items of this quality or better will be displayed"], 10, {
+		["0"] = ITEM_QUALITY0_DESC,
+		["1"] = ITEM_QUALITY1_DESC,
+		["2"] = ITEM_QUALITY2_DESC,
+		["3"] = ITEM_QUALITY3_DESC,
+		["4"] = ITEM_QUALITY4_DESC,
+	}, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.quality end, function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.quality = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.filter.args.quality.style = "radio"
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.scale = ACH:Group(L["Scale"], nil, 1, "tab")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.scale.args.header1 = ACH:Description(L["Change the scale of LootText"], 1, nil, function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end, nil, nil, nil, "full")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.scale.args.lootscale = ACH:Range(L["Set the size of LootText"], nil, 2, { min = 0.1, max = 1.5, step = 0.05 }, "full", function() return E.db.ElvUI_EltreumUI.loot.loottext.scale end, function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.scale = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.scale.args.reset = ACH:Execute(L["Reset Scale"], L["Return LootText to its default scale"], 3, function() E.db.ElvUI_EltreumUI.loot.loottext.scale = 0.65 end, nil, false, "full", nil, nil, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.strata = ACH:Group(L["Strata"], nil, 1, "tab")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.strata.args.header1 = ACH:Description(L["Change the strata of the Loot Text"], 1, nil, function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end, nil, nil, nil, "full")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.strata.args.lootstrata = ACH:Select(" ", L["Set the level that LootText will be drawn to this"], 2, {
+		["BACKGROUND"] = L["Background"],
+		["LOW"] = L["Low"],
+		["MEDIUM"] = L["Medium"],
+		["HIGH"] = L["High"],
+		["DIALOG"] = L["Dialog"],
+	}, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.strata end, function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.strata = value E:StaticPopup_Show('CONFIG_RL') end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.strata.args.lootstrata.style = "radio"
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.position = ACH:Group(L["Position"], nil, 1, "tab")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.position.args.header1 = ACH:Description("", 1, nil, function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end, nil, nil, nil, "full")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.position.args.x = ACH:Range(L["Change the Horizontal Position of LootText (Default = 0)"], nil, 2, { min = -1000, max = 1000, step = 1 }, "full", function() return E.db.ElvUI_EltreumUI.loot.loottext.xOffset end, function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.xOffset = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.position.args.y = ACH:Range(L["Change the Vertical Position of LootText (Default = 200)"], nil, 3, { min = -1000, max = 1000, step = 1 }, "full", function() return E.db.ElvUI_EltreumUI.loot.loottext.yOffset end, function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.yOffset = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts = ACH:Group(L["Fonts"], nil, 1, "tab")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.header1 = ACH:Description("", 1, nil, function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end, nil, nil, nil, "full")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.size = ACH:Range(L["Font Size"], nil, 2, { min = 4, max = 160, step = 1 }, "full", function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsize end, function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.fontsize = value end, function() return not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.header2 = ACH:Description(L["Change the font of LootText to be the ElvUI Default Font found in General"], 3, nil, function() return 'Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\EltreumHeader', 3240, 1 end, nil, nil, nil, "full")
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.customfont = ACH:Toggle(L["Use ElvUI General Font"], L["Change the font of LootText"], 4, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsetting end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.fontsetting = value E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsettingdmg or E.db.ElvUI_EltreumUI.loot.loottext.fontLSMenable or not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.customfontdmg = ACH:Toggle(L["Use ElvUI Damage Font"], L["Change the font of LootText"], 5, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsettingdmg end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.fontsettingdmg = value E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsetting or E.db.ElvUI_EltreumUI.loot.loottext.fontLSMenable or not E.db.ElvUI_EltreumUI.loot.loottext.enable end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.customLSMenable = ACH:Toggle(L["Use Custom Font"], L["Change the font of LootText"], 6, nil, false, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontLSMenable end,function(_, value) E.db.ElvUI_EltreumUI.loot.loottext.fontLSMenable = value E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsettingdmg or E.db.ElvUI_EltreumUI.loot.loottext.fontsetting end)
+	ElvUI_EltreumUI.Options.args.loot.args.loottext.args.fonts.args.customLSMfont = ACH:SharedMediaFont(L["Font"], L["Choose a different font from the preselected ones"], 7, nil, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontLSM end, function(self,fontvalue) E.db.ElvUI_EltreumUI.loot.loottext.fontLSM = fontvalue end, function() return E.db.ElvUI_EltreumUI.loot.loottext.fontsettingdmg or E.db.ElvUI_EltreumUI.loot.loottext.fontsetting or not E.db.ElvUI_EltreumUI.loot.loottext.fontLSMenable end)
+
+
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot = ACH:Group("Fast Loot", nil, 2)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.enable = ACH:Toggle(L["Enable Fast Loot"], L["Decrease the time it takes for auto loot to work, works with TSM"], 1, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.fastloot end,function(_, value) E.db.ElvUI_EltreumUI.loot.fastloot = value SetCVar('autoLootDefault', 1) E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.fastlootfilter or E.db.ElvUI_EltreumUI.loot.lootwishlistfilter end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.enablefilter = ACH:Toggle(L["Enable Fast Loot with Loot Quality Filter"], L["Enable filtering item quality, items can still be looted by holding Shift or the mod key setup in Interface"], 2, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.fastlootfilter end, function(_, value) E.db.ElvUI_EltreumUI.loot.fastlootfilter = value SetCVar('autoLootDefault', 0) E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.fastloot or E.db.ElvUI_EltreumUI.loot.lootwishlistfilter end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.enablewishlist = ACH:Toggle(L["Enable Fast Loot with Wishlist Filter"], L["Items not on the wishlist will not be looted, items can still be looted by holding Shift or the mod key setup in Interface"], 3, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.lootwishlistfilter end, function(_, value) E.db.ElvUI_EltreumUI.loot.lootwishlistfilter = value SetCVar('autoLootDefault', 0) E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.fastloot or E.db.ElvUI_EltreumUI.loot.fastlootfilter or #E.private.ElvUI_EltreumUI.wishlistID == 0 end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.enablewishlistwarning = ACH:Toggle(L["Enable Wishlist Loot Warning"], L["Show a toast if an item in the wishlist is looted"], 4, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.lootwishlistwarning end, function(_, value) E.db.ElvUI_EltreumUI.loot.lootwishlistwarning = value E:StaticPopup_Show('CONFIG_RL') end, function() return #E.private.ElvUI_EltreumUI.wishlistID == 0 end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.autoclose = ACH:Toggle(L["Enable Automatically Closing Loot Window"], L["Loot Window will automatically close when using filters and the item desired is not found"], 4, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.lootautoclose end, function(_, value) E.db.ElvUI_EltreumUI.loot.lootautoclose = value E:StaticPopup_Show('CONFIG_RL') end)
+
+
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.confirmbop = ACH:Toggle(L["Enable Automatically Confirming Bind on Pickup"], L["Loot Window will automatically confirm looting Bind on Pickup items"], 4, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.confirmbop end, function(_, value) E.db.ElvUI_EltreumUI.loot.confirmbop = value end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.screenshot = ACH:Toggle(L["Enable Screenshotting if an item from the wishlist is looted"], L["Automatically screenshot when you loot an item from the wishlist"], 4, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.lootwishlistscreenshot end, function(_, value) E.db.ElvUI_EltreumUI.loot.lootwishlistscreenshot = value E:StaticPopup_Show('CONFIG_RL') end, function() return #E.private.ElvUI_EltreumUI.wishlistID == 0 end, E.Retail)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.filterquality = ACH:Select(L["Minimum Loot Quality Filter"], L["Only items of this quality or better will be looted when using Fast Loot Filter"], 4, {
+		["0"] = ITEM_QUALITY0_DESC,
+		["1"] = ITEM_QUALITY1_DESC,
+		["2"] = ITEM_QUALITY2_DESC,
+		["3"] = ITEM_QUALITY3_DESC,
+		["4"] = ITEM_QUALITY4_DESC,
+	}, false, "full", function() return E.db.ElvUI_EltreumUI.loot.fastlootquality end, function(_, value) E.db.ElvUI_EltreumUI.loot.fastlootquality = value E:StaticPopup_Show('CONFIG_RL') end, function() return E.db.ElvUI_EltreumUI.loot.fastloot or not E.db.ElvUI_EltreumUI.loot.fastlootfilter or E.db.ElvUI_EltreumUI.loot.lootwishlistfilter end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.sound = ACH:Toggle(L["Enable Playing a Sound if an item from the wishlist is looted"], nil, 4, nil, false, "full", function() return E.db.ElvUI_EltreumUI.loot.lootwishlistsoundenable end, function(_, value) E.db.ElvUI_EltreumUI.loot.lootwishlistsoundenable = value E:StaticPopup_Show('CONFIG_RL') end, function() return #E.private.ElvUI_EltreumUI.wishlistID == 0 end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.soundLSM = ACH:SharedMediaSound(L["Select a Sound"], L["Choose a Sound from SharedMedia files"], 5, "double", function() return E.db.ElvUI_EltreumUI.loot.lootwishlistsound end, function(self,key) E.db.ElvUI_EltreumUI.loot.lootwishlistsound = key E:StaticPopup_Show('CONFIG_RL') end, function() return not E.db.ElvUI_EltreumUI.loot.lootwishlistsoundenable or (#E.private.ElvUI_EltreumUI.wishlistID == 0) end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.lootwishlistenter = ACH:Input(L["Item Wishlist (type the Item ID)"], L["Items in your wishlist will display a warning when looted"], 6, nil, "double", function() return E.private.ElvUI_EltreumUI.wishlistID end,
+	function(_, value)
+			value = tonumber(value)
+			local item = Item:CreateFromItemID(value)
+			if not item:IsItemEmpty() then
+				item:ContinueOnItemLoad(function()
+					local itemName = item:GetItemName()
+					local itemID = tonumber(value)
+					tinsert(E.private.ElvUI_EltreumUI.wishlistName, itemName)
+					tinsert(E.private.ElvUI_EltreumUI.wishlistID, itemID)
+				end)
+			end
+	end, nil, nil,
+	function(_, value)
+		E.PopupDialogs["ELTRUISMINVALID"] = {
+			text = L["Invalid Item, you need to add an itemID which can be found in the tooltip or in Wowhead"],
+			button1 = OKAY,
+			timeout = 0,
+			whileDead = 1,
+			hideOnEscape = true,
+		}
+		if tonumber(value) ~= nil then
+			value = tonumber(value)
+			local item = Item:CreateFromItemID(value)
+			if item == nil then
+				return E:StaticPopup_Show('ELTRUISMINVALID') and false
+			elseif item:IsItemEmpty() then
+				return E:StaticPopup_Show('ELTRUISMINVALID') and false
+			else
+				return true
+			end
+		else
+			return E:StaticPopup_Show('ELTRUISMINVALID') and false
+		end
+	end)
+	ElvUI_EltreumUI.Options.args.loot.args.fastloot.args.lootwishlistremove = ACH:Select(L["Remove item from Wishlist"], REMOVE, 7, E.private.ElvUI_EltreumUI.wishlistName, true, "double", function() return E.private.ElvUI_EltreumUI.wishlistName end, function(_,value)
+		local item = tonumber(value)
+		tremove(E.private.ElvUI_EltreumUI.wishlistID, item)
+		tremove(E.private.ElvUI_EltreumUI.wishlistName, item)
+	end)
+
 
 
 	--ACH:Group(name, desc, order, childGroups, get, set, disabled, hidden, func
