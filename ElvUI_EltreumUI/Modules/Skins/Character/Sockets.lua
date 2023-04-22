9280c46b592c915fa5efab4e5eb0306c21e84144
Eltreum <eltreum2048@gmail.com>
2023-04-22 13:33:26 -0300

    classic needs C_Timer

--- ElvUI_EltreumUI/Modules/Skins/Character/Sockets.lua
+++ ElvUI_EltreumUI/Modules/Skins/Character/Sockets.lua
@@ -7,6 +7,7 @@ local CreateFrame = _G.CreateFrame
 local setmetatable = _G.setmetatable
 local table = _G.table
 local IsModifiedClick = _G.IsModifiedClick
+local C_Timer = _G.C_Timer
 local pairs = _G.pairs
 local GetInventoryItemLink = _G.GetInventoryItemLink
 local GetInventorySlotInfo = _G.GetInventorySlotInfo
@@ -1382,16 +1383,16 @@ function ElvUI_EltreumUI:ClassicSockets()
 		end)]]
 
 		--Eltreum: use E:Delay to decrease memory/cpu usage and also possibly make it faster
-		self.refreshTimer0 = E:Delay(0.1, function()
+		self.refreshTimer0 = C_Timer.After(0.1, function()
 			self:_Refresh()
 		end)
-		self.refreshTimer1 = E:Delay(1, function()
+		self.refreshTimer1 = C_Timer.After(1, function()
 			self:_Refresh()
 		end)
-		self.refreshTimer2 = E:Delay(2, function()
+		self.refreshTimer2 = C_Timer.After(2, function()
 			self:_Refresh()
 		end)
-		self.refreshTimer3 = E:Delay(3, function()
+		self.refreshTimer3 = C_Timer.After(3, function()
 			self:_Refresh()
 		end)
 	end
