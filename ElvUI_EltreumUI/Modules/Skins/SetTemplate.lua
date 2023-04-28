7b828b38ca2f55b4bb0f725c2d193d85531cacc1
Eltreum <eltreum2048@gmail.com>
2023-04-27 23:06:32 -0300

    fix issue with elvui cpu

--- ElvUI_EltreumUI/Modules/Skins/SetTemplate.lua
+++ ElvUI_EltreumUI/Modules/Skins/SetTemplate.lua
@@ -20,6 +20,7 @@ function ElvUI_EltreumUI:SetTemplateSkin()
 		local function SkinFrame(frame)
 			if frame:GetObjectType() == "Texture" then frame = frame:GetParent() end
 			local mt = getmetatable(frame).__index
+			if type(mt) == 'function' then return end
 			if mt.SetTemplate then
 				hooksecurefunc(mt, "SetTemplate", function(frame, template, _, _, _, isUnitFrameElement, isNamePlateElement)
 					if isUnitFrameElement and not E.db.ElvUI_EltreumUI.skins.elvui.unitframes then return end
