998743ebc263921232623a1905fa2629eba63ff1
Eltreum <eltreum2048@gmail.com>
2023-04-28 16:37:06 -0300

    no longer used

--- ElvUI_EltreumUI/Modules/Fonts/SetupFont.lua
+++ ElvUI_EltreumUI/Modules/Fonts/SetupFont.lua
@@ -1,7 +1,6 @@
 local E, L = unpack(ElvUI)
 local _G = _G
 local IsAddOnLoaded = _G.IsAddOnLoaded
-local GetAddOnMetadata = _G.C_AddOns and _G.C_AddOns.GetAddOnMetadata or _G.GetAddOnMetadata
 
 function ElvUI_EltreumUI:SetupFont(fontvalue, custom)
 	if not E.db.movers then E.db.movers = {} end
