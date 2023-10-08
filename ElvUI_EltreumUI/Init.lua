39d87c8eadc314830c309c05b41aaf5ecb6db38b
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-10-08 06:13:53 -0300

    add lfg for wrath

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -97,12 +97,14 @@ function ElvUI_EltreumUI:PLAYER_ENTERING_WORLD(_, initLogin)
 		if E.myclass == 'HUNTER' then
 			ElvUI_EltreumUI:ExpandedStable() --expands pet stable for hunters
 		end
-		ElvUI_EltreumUI:DungeonRoleIcons() -- add spec icons to the lfg frame
 	elseif E.Wrath or E.Classic then
 		ElvUI_EltreumUI:ClassicSockets() --adds sockets and enchants into the character panel, based on Kibs Item Level by Kibsgaard
 		ElvUI_EltreumUI:DynamicClassicDatatext() --toggles datatext for warlocks/hunters to show soulshards/ammo
 		ElvUI_EltreumUI:UpdateAvgIlvl() --updates the ilvl of the character at login so its not 0
 		ElvUI_EltreumUI:SkinProfessions() --makes professions wider
+	end
+	if not E.Classic then
+		ElvUI_EltreumUI:DungeonRoleIcons() -- add spec icons to the lfg frame
 	end
 	if E.private.nameplates.enable then
 		ElvUI_EltreumUI:NamePlateOptions() --adds dynamic class based color filters to elvui nameplates
