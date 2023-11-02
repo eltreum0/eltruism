813f003a15724630c8601cc43ecc24889fc5ba6d
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-11-02 14:18:52 -0300

    fix flashing on quest progress bar

--- ElvUI_EltreumUI/Modules/Skins/Other/QuestSkin.lua
+++ ElvUI_EltreumUI/Modules/Skins/Other/QuestSkin.lua
@@ -460,18 +460,16 @@ function ElvUI_EltreumUI:SkinQuests()
 							end
 						end
 
-						E:Delay(0, function()
-							progressBar.Bar.backdrop:SetAlpha(E.db.general.backdropfadecolor.a)
-							progressBar.Bar.backdrop:SetBackdropColor(0,0,0)
-						end)
+						--E:Delay(0, function()
+						progressBar.Bar.backdrop:SetAlpha(E.db.general.backdropfadecolor.a)
+						progressBar.Bar.backdrop:SetBackdropColor(0,0,0)
+						progressBar.Bar.backdrop.SetBackdropColor = E.noop
+						--end)
 						progressBar.Bar:GetStatusBarTexture():SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.skins.queststatusbartexture))
 						hooksecurefunc(progressBar.Bar, "SetStatusBarColor", function(_, r, g, b)
 							progressBar.Bar:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=r - 0.4,g= g - 0.4,b= b - 0.4,a= E.db.general.backdropfadecolor.a}, {r=r + 0.2,g= g + 0.2,b= b + 0.2,a= E.db.general.backdropfadecolor.a})
 							progressBar.Bar.backdrop:SetAlpha(E.db.general.backdropfadecolor.a)
-							E:Delay(0, function()
-								progressBar.Bar.backdrop:SetAlpha(E.db.general.backdropfadecolor.a)
-								progressBar.Bar.backdrop:SetBackdropColor(0,0,0)
-							end)
+							progressBar.Bar.backdrop:SetBackdropColor(0,0,0)
 						end)
 						progressBar.EltruismSkin = true
 					end
