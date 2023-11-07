6c1be417c485efd67df389669ab189bbfd34c2ed
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-11-07 16:01:04 -0300

    raise strata

--- ElvUI_EltreumUI/Modules/Skins/Borders/Borders.lua
+++ ElvUI_EltreumUI/Modules/Skins/Borders/Borders.lua
@@ -281,7 +281,7 @@ function ElvUI_EltreumUI:Borders()
 						else
 							partyborder:SetBackdropBorderColor(classcolor.r, classcolor.g, classcolor.b, 1)
 						end
-						partyborder:SetFrameStrata("LOW")
+						partyborder:SetFrameStrata("MEDIUM")
 					end
 				end
 			end
