8a71b507a1e87ec332edfaeba1583f18ce81c099
Eltreum <30246110+eltreum0@users.noreply.github.com>
2024-03-17 19:24:21 -0300

    also skin the party version

--- ElvUI_EltreumUI/Modules/Skins/Unitframes/Blizzard.lua
+++ ElvUI_EltreumUI/Modules/Skins/Unitframes/Blizzard.lua
@@ -6,7 +6,7 @@ local UnitClass = _G.UnitClass
 function ElvUI_EltreumUI:BlizzardTexturesGradient()
 	if not E.db.ElvUI_EltreumUI then return end
 	if E.db.ElvUI_EltreumUI.unitframes.UFmodifications and E.db.ElvUI_EltreumUI.unitframes.blizzardraidframes then
-		if _G["CompactRaidFrameContainer"] then
+		if _G["CompactRaidFrameContainer"] and _G["CompactRaidFrameContainer"]:IsVisible() then
 
 			if not _G["CompactRaidFrameContainer"].EltruismHook then
 				hooksecurefunc("CompactUnitFrame_UpdateHealthColor", function()
@@ -149,6 +149,90 @@ function ElvUI_EltreumUI:BlizzardTexturesGradient()
 					end
 				end
 			end
+		elseif _G["CompactPartyFrame"] and _G["CompactPartyFrame"]:IsVisible() then
+			if not _G["CompactPartyFrame"].EltruismHook then
+				hooksecurefunc("CompactUnitFrame_UpdateHealthColor", function()
+					ElvUI_EltreumUI:BlizzardTexturesGradient()
+				end)
+
+				hooksecurefunc("CompactUnitFrame_UpdateAll", function()
+					ElvUI_EltreumUI:BlizzardTexturesGradient()
+				end)
+
+				hooksecurefunc("CompactUnitFrame_UpdateRoleIcon", function()
+					ElvUI_EltreumUI:BlizzardTexturesGradient()
+				end)
+
+				_G["CompactPartyFrame"].EltruismHook = true
+			end
+
+			if _G["CompactPartyFrameMember1"] and _G["CompactPartyFrameMember1"]:IsVisible() then
+				for l = 1, 5 do
+					if _G["CompactPartyFrameMember"..l] then
+						--[[if not _G["CompactPartyFrameMember"..l].templateset then
+							_G["CompactPartyFrameMember"..l]:SetTemplate("Transparent")
+							_G["CompactPartyFrameMember"..l].templateset = true
+						end]] --not sure yet about setting a template since maybe it will break some stuff
+						if _G["CompactPartyFrameMember"..l].displayedUnit then
+							local _ , unitclass = UnitClass(_G["CompactPartyFrameMember"..l].displayedUnit)
+							if unitclass and _G["CompactPartyFrameMember"..l].healthBar then
+
+								--textures
+								_G["CompactPartyFrameMember"..l].background:SetColorTexture(0,0,0,E.db.general.backdropfadecolor.a)
+								if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable then
+									if not E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.noclasstexture then
+										_G["CompactPartyFrameMember"..l].healthBar:GetStatusBarTexture():SetTexture(ElvUI_EltreumUI:UnitframeClassTextureCustom(unitclass))
+									else
+										_G["CompactPartyFrameMember"..l].healthBar:GetStatusBarTexture():SetTexture(ElvUI_EltreumUI:UnitframeClassTexture(unitclass))
+									end
+								else
+									if E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable then
+										if E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablegroupunits then
+											if not E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable then
+												if not E.db.ElvUI_EltreumUI.unitframes.gradientmode.useUFtexture then
+													_G["CompactPartyFrameMember"..l].healthBar:GetStatusBarTexture():SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.gradientmode.texture))
+												end
+											end
+										end
+									else
+										if not E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable and not E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.enable then
+											_G["CompactPartyFrameMember"..l].healthBar:GetStatusBarTexture():SetTexture(E.LSM:Fetch("statusbar", E.db.unitframe.statusbar))
+										end
+									end
+								end
+
+								--shadows
+								if E.db.ElvUI_EltreumUI.skins.shadow.enable then
+									if E.db.ElvUI_EltreumUI.skins.shadow.raid and not (E.db.ElvUI_EltreumUI.borders.borders and E.db.ElvUI_EltreumUI.borders.raidborders) then
+										if not _G["CompactPartyFrameMember"..l].healthBar.shadow then
+											_G["CompactPartyFrameMember"..l].healthBar:CreateShadow()
+											ElvUI_EltreumUI:ShadowColor(_G["CompactPartyFrameMember"..l].healthBar.shadow)
+										end
+									end
+								end
+
+								--gradient
+								if E.db.ElvUI_EltreumUI.unitframes.gradientmode.enable and E.db.ElvUI_EltreumUI.unitframes.gradientmode.enablegroupunits then
+									if E.db.ElvUI_EltreumUI.unitframes.gradientmode.customcolor then
+										_G["CompactPartyFrameMember"..l].healthBar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColorsCustom(unitclass, false, false))
+									else
+										_G["CompactPartyFrameMember"..l].healthBar:GetStatusBarTexture():SetGradient(E.db.ElvUI_EltreumUI.unitframes.gradientmode.orientation, ElvUI_EltreumUI:GradientColors(unitclass, false, false))
+									end
+								end
+
+								--font
+								_G["CompactPartyFrameMember"..l].name:SetFont(E.LSM:Fetch("font", E.db.general.font), 10, ElvUI_EltreumUI:FontFlag(E.db.general.fontStyle))
+
+								--role icons
+								local role = UnitGroupRolesAssigned(_G["CompactPartyFrameMember"..l].displayedUnit)
+								_G["CompactPartyFrameMember"..l].roleIcon:SetTexCoord(0,1,0,1)
+								_G["CompactPartyFrameMember"..l].roleIcon:SetTexture(ElvUI_EltreumUI:GetRoleIcon(role))
+							end
+						end
+					end
+				end
+			end
+
 		end
 	end
 end
