41ccd4b5c69c47bb0cf1f81129e15cde8c9162a5
Eltreum <30246110+eltreum0@users.noreply.github.com>
2024-06-01 10:56:27 -0300

    nil check ilvldb

--- ElvUI_EltreumUI/Modules/Skins/Character/CharacterPanelSkin.lua
+++ ElvUI_EltreumUI/Modules/Skins/Character/CharacterPanelSkin.lua
@@ -2796,7 +2796,7 @@ function ElvUI_EltreumUI:InspectBg(unit)
 							local M = E:GetModule('Misc')
 							hooksecurefunc(M,"UpdateAverageString", function(_, _, which, iLevelDB)
 								if which == "Inspect" then
-									if _G.InspectFrame and _G.InspectFrame.ItemLevelText then
+									if _G.InspectFrame and _G.InspectFrame.ItemLevelText and iLevelDB then
 										_G.InspectFrame.ItemLevelText:SetText("|cffFFCE00"..L["Item Level"]..":|r "..E:CalculateAverageItemLevel(iLevelDB, _G.InspectFrame.unit))
 									end
 								end
