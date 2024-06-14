b5bdb09165181fade0a7f17413fbf8a8510ac406
Eltreum <30246110+eltreum0@users.noreply.github.com>
2024-06-14 17:57:21 -0300

    cata is now unique

--- ElvUI_EltreumUI/Modules/DataTexts/Other.lua
+++ ElvUI_EltreumUI/Modules/DataTexts/Other.lua
@@ -27,14 +27,14 @@ local function EltruismHonorDatatext(dt)
 	local arg2 = E.Retail and COMBAT_HONOR_GAIN or HONOR
 
 	local honorCurrencyID = (E.Cata and Constants.CurrencyConsts.CLASSIC_HONOR_CURRENCY_ID) or (E.Retail and 1792)
-	local arg4 = (not E.Classic and C_CurrencyInfo_GetCurrencyInfo(honorCurrencyID).quantity) or select(2, _G.GetPVPThisWeekStats())
+	local arg4 = (E.Retail and C_CurrencyInfo_GetCurrencyInfo(honorCurrencyID).quantity) or (E.Cata and select(2, _G.GetCurrencyInfo(honorCurrencyID))) or select(2, _G.GetPVPThisWeekStats())
 
 	local arg5 = (E.Retail and PVP_CONQUEST) or (E.Cata and ARENA) or _G.RANK
 
 	local classicRank = E.Classic and _G.UnitPVPRank('player')-4
 	if classicRank and classicRank < 0 then classicRank = 0 end
 	local arenaCurrencyID = (E.Cata and Constants.CurrencyConsts.CLASSIC_ARENA_POINTS_CURRENCY_ID) or (E.Retail and 1602)
-	local arg7 = not E.Classic and C_CurrencyInfo_GetCurrencyInfo(arenaCurrencyID).quantity or classicRank
+	local arg7 = (E.Retail and C_CurrencyInfo_GetCurrencyInfo(arenaCurrencyID).quantity) or (E.Cata and select(2, _G.GetCurrencyInfo(arenaCurrencyID))) or classicRank
 
 	dt.text:SetFormattedText('%s: %s%s|r %s: %s%s|r', arg2, E.media.hexvaluecolor, arg4, arg5, E.media.hexvaluecolor, arg7)
 end
