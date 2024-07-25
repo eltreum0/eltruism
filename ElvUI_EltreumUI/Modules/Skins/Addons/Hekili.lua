bce2a4b14fd8696c5e4acc519ccd7647d90af735
Eltreum <30246110+eltreum0@users.noreply.github.com>
2024-07-25 03:45:15 -0300

    better

--- ElvUI_EltreumUI/Modules/Skins/Addons/Hekili.lua
+++ ElvUI_EltreumUI/Modules/Skins/Addons/Hekili.lua
@@ -8,9 +8,8 @@ function ElvUI_EltreumUI:EltruismHekili()
 		E:Delay(0, function()
 			for i = 1, 10 do
 				if _G["Hekili_Primary_B"..i] then
-					local texture = _G["Hekili_Primary_B"..i].Texture
 					S:HandleButton(_G["Hekili_Primary_B"..i])
-					_G["Hekili_Primary_B"..i].Texture = texture
+					_G["Hekili_Primary_B"..i].Texture:SetAlpha(_G["Hekili_Primary_B"..i]:GetAlpha())
 					if E.db.ElvUI_EltreumUI.skins.shadow.enable and not _G["Hekili_Primary_B"..i].shadow then
 						_G["Hekili_Primary_B"..i]:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
 						ElvUI_EltreumUI:ShadowColor(_G["Hekili_Primary_B"..i].shadow)
@@ -23,9 +22,8 @@ function ElvUI_EltreumUI:EltruismHekili()
 					end
 				end
 				if _G["Hekili_Defensives_B"..i] then
-					local texture2 = _G["Hekili_Defensives_B"..i].Texture
 					S:HandleButton(_G["Hekili_Defensives_B"..i])
-					_G["Hekili_Defensives_B"..i].Texture = texture2
+					_G["Hekili_Defensives_B"..i].Texture:SetAlpha(_G["Hekili_Defensives_B"..i]:GetAlpha())
 					if E.db.ElvUI_EltreumUI.skins.shadow.enable and not _G["Hekili_Defensives_B"..i].shadow then
 						_G["Hekili_Defensives_B"..i]:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
 						ElvUI_EltreumUI:ShadowColor(_G["Hekili_Defensives_B"..i].shadow)
@@ -38,9 +36,8 @@ function ElvUI_EltreumUI:EltruismHekili()
 					end
 				end
 				if _G["Hekili_Cooldowns_B"..i] then
-					local texture3 = _G["Hekili_Cooldowns_B"..i].Texture
 					S:HandleButton(_G["Hekili_Cooldowns_B"..i])
-					_G["Hekili_Cooldowns_B"..i].Texture = texture3
+					_G["Hekili_Cooldowns_B"..i].Texture:SetAlpha(_G["Hekili_Cooldowns_B"..i]:GetAlpha())
 					if E.db.ElvUI_EltreumUI.skins.shadow.enable and not _G["Hekili_Cooldowns_B"..i].shadow then
 						_G["Hekili_Cooldowns_B"..i]:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
 						ElvUI_EltreumUI:ShadowColor(_G["Hekili_Cooldowns_B"..i].shadow)
@@ -53,9 +50,8 @@ function ElvUI_EltreumUI:EltruismHekili()
 					end
 				end
 				if _G["Hekili_AOE_B"..i] then
-					local texture4 = _G["Hekili_AOE_B"..i].Texture
 					S:HandleButton(_G["Hekili_AOE_B"..i])
-					_G["Hekili_AOE_B"..i].Texture = texture4
+					_G["Hekili_AOE_B"..i].Texture:SetAlpha(_G["Hekili_AOE_B"..i]:GetAlpha())
 					if E.db.ElvUI_EltreumUI.skins.shadow.enable and not _G["Hekili_AOE_B"..i].shadow then
 						_G["Hekili_AOE_B"..i]:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
 						ElvUI_EltreumUI:ShadowColor(_G["Hekili_AOE_B"..i].shadow)
@@ -68,9 +64,8 @@ function ElvUI_EltreumUI:EltruismHekili()
 					end
 				end
 				if _G["Hekili_Interrupts_B"..i] then
-					local texture5 = _G["Hekili_Interrupts_B"..i].Texture
 					S:HandleButton(_G["Hekili_Interrupts_B"..i])
-					_G["Hekili_Interrupts_B"..i] = texture5
+					_G["Hekili_Interrupts_B"..i].Texture:SetAlpha(_G["Hekili_Interrupts_B"..i]:GetAlpha())
 					if E.db.ElvUI_EltreumUI.skins.shadow.enable and not _G["Hekili_Interrupts_B"..i].shadow then
 						_G["Hekili_Interrupts_B"..i]:CreateShadow(E.db.ElvUI_EltreumUI.skins.shadow.length)
 						ElvUI_EltreumUI:ShadowColor(_G["Hekili_Interrupts_B"..i].shadow)
