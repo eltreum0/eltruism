b6b9d0718cbfc493cad0322534a723364c7a8fa7
Eltreum <30246110+eltreum0@users.noreply.github.com>
2024-07-30 17:14:37 -0300

    Revert "elvui now does it"
    This reverts commit fa0fcd07fb0d7abdbf70ddeca84bcf17fbde716e.

--- ElvUI_EltreumUI/Modules/Skins/Other/QuestSkin.lua
+++ ElvUI_EltreumUI/Modules/Skins/Other/QuestSkin.lua
@@ -318,9 +318,10 @@ function ElvUI_EltreumUI:SkinQuests()
 						end
 
 						if bar.SetPercent then
-							hooksecurefunc(bar:GetParent(), "SetPercent", function(frame)
+							hooksecurefunc(bar:GetParent(), "SetPercent", function(frame,percent)
 								local statusbar = frame.Bar
 								if not statusbar then return end
+								S:StatusBarColorGradient(statusbar, percent, 100)
 								local r,g,b = statusbar:GetStatusBarColor()
 								statusbar:GetStatusBarTexture():SetGradient("HORIZONTAL", {r=r - 0.4,g= g - 0.4,b= b - 0.4,a= E.db.general.backdropfadecolor.a}, {r=r + 0.2,g= g + 0.2,b= b + 0.2,a= E.db.general.backdropfadecolor.a})
 								statusbar.backdrop:SetAlpha(E.db.general.backdropfadecolor.a)
