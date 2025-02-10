57bc013093d8160a8cb82af7caa354eedff8361f
Eltreum <30246110+eltreum0@users.noreply.github.com>
2025-02-10 06:17:29 -0300

    test avoiding baganator elvui skin

--- ElvUI_EltreumUI/Modules/Skins/SetTemplate.lua
+++ ElvUI_EltreumUI/Modules/Skins/SetTemplate.lua
@@ -66,6 +66,7 @@ function ElvUI_EltreumUI:SetTemplateSkin()
 					if frame:GetObjectType() == "Button" and not E.db.ElvUI_EltreumUI.skins.elvui.button then return end
 					if frame:GetParent() and frame:GetParent():GetObjectType() == "Button" and not E.db.ElvUI_EltreumUI.skins.elvui.button then return end
 					if frame:GetObjectType() == "CheckButton" and frame.mask then return end --actionbar masks fix
+					if frame.bgrElvUISkin then return end --baganator elvui skin
 
 					--if frame.showDispellableDebuff then return end --fix RaidDebufs
 					if template ~= "NoBackdrop" then
