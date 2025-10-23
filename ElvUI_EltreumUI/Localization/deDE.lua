c9a1b7452776ba2a26a32b6f6d860905d378fbaa
Dlarge <dlarge500@googlemail.com>
2025-10-23 18:46:18 +0200

    updated German locals (#202)
    * Update deDE.lua
    
    * Update deDE.lua
    
    * Update deDE.lua
    
    * Update deDE.lua
    
    * Revert "Merge remote-tracking branch 'upstream/main'"
    
    This reverts commit 338ef7057db66f2f620592f2c9ada08e0b19f55c, reversing
    changes made to 2452e0d3f3740e55ebe4244dd318fb68c3e02fb0.
    
    * Update deDE.lua
    
    * Update deDE.lua
    
    * Update deDE.lua

--- ElvUI_EltreumUI/Localization/deDE.lua
+++ ElvUI_EltreumUI/Localization/deDE.lua
@@ -1632,5 +1632,5 @@ L["updated to "] = "aktualisiert auf "
 L["|cffff0000Private Servers are unsupported!"] = "|cffff0000Private Server werden nicht unterstützt!"
 L["Nameplate Castbar Border"] = "Namenspaketten Zauberleistenrahmen"
 L["Nameplate Castbar Thickness"] = "Namensplaketten Zauberleistendicke"
-L["How do I disable the class color texture at the bottom of the screen?"] = true
-L["This is the texture that comes with the Eltruism Datatext, to disable go to:\n\nElvUI > Eltruism > Misc > DataTexts > Uncheck Class Color Texture with Eltruism Datatext"] = true
+L["How do I disable the class color texture at the bottom of the screen?"] = "Wie deaktiviere ich die Klassenfarbtextur am unteren Bildschirmrand?"
+L["This is the texture that comes with the Eltruism Datatext, to disable go to:\n\nElvUI > Eltruism > Misc > DataTexts > Uncheck Class Color Texture with Eltruism Datatext"] = "Dies ist die Textur, die mit dem Eltruism-Datentext geliefert wird. Um sie zu deaktivieren, gehe zu:\an\ElvUI > Eltruism > Verschiedenes > Datentexte > Deaktiviere die Klassenfarbtextur mit Eltruism-Datentext"
