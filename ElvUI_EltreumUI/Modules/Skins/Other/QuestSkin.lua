81d01e7f244afb43b90b344294a09b5bd66addee
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-02-04 23:05:29 -0300

    explain why the message is appearing

--- ElvUI_EltreumUI/Modules/Skins/Other/QuestSkin.lua
+++ ElvUI_EltreumUI/Modules/Skins/Other/QuestSkin.lua
@@ -1841,6 +1841,13 @@ end
 
 --because the objective is removed from frame manager, when swapping to another layout that isnt it can error when trying to export it, so break it from manager again
 if E.Retail then
+	E.PopupDialogs["EDITMODEBEINGSPAMMED"] = {
+		text = "Edit Mode is being spammed by BetterCooldownManager, if you have issues please report to its author.",
+		button1 = _G.ACCEPT,
+		timeout = 0,
+		whileDead = 1,
+		hideOnEscape = true,
+	}
 	local editmodecheck = CreateFrame("FRAME")
 	editmodecheck:RegisterEvent("EDIT_MODE_LAYOUTS_UPDATED")
 	editmodecheck:RegisterEvent("FIRST_FRAME_RENDERED")
@@ -1854,7 +1861,11 @@ if E.Retail then
 
 			if not _G.EditModeManagerFrame.EltruismExitRL then --call for a reload on blizzard's edit mode changes
 				_G.EditModeManagerFrame:HookScript("OnHide", function()
-					E:StaticPopup_Show('CONFIG_RL')
+					if IsAddOnLoaded("BetterCooldownManager") then
+						E:StaticPopup_Show('EDITMODEBEINGSPAMMED')
+					else
+						E:StaticPopup_Show('CONFIG_RL')
+					end
 				end)
 				_G.EditModeManagerFrame.EltruismExitRL = true
 			end
