03b7a6652c990a3c282c4a841298092ead7f0965
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-02-12 05:07:11 -0300

    attempt to fix the raid aura border disable option

--- ElvUI_EltreumUI/Modules/Skins/Borders/Borders.lua
+++ ElvUI_EltreumUI/Modules/Skins/Borders/Borders.lua
@@ -2060,18 +2060,21 @@ local function HandleUFAuraBorder(button,isNamePlateElement)
 	end
 end
 
+local raidFrames ={
+	["raid1"] = true,
+	["raid2"] = true,
+	["raid3"] = true,
+	["raidpet"] = true,
+}
+
 function ElvUI_EltreumUI:UFAuraBorders(_,button)
 	if button and E.db.ElvUI_EltreumUI.borders.borders and E.private.auras.enable then
 		if button.isNamePlateElement and E.db.ElvUI_EltreumUI.borders.aurabordernp then
 			HandleUFAuraBorder(button,button.isNamePlateElement)
 		elseif button.isUnitFrameElement and E.db.ElvUI_EltreumUI.borders.auraborderuf then
 			if E.db.ElvUI_EltreumUI.borders.disableRaidAuraBorder then
-				if ElvUI_EltreumUI:RetailInstanceSecret(button) or ElvUI_EltreumUI:RetailInstanceSecret(button:GetDebugName()) then
-					return
-				else
-					if button:GetDebugName():match("Raid") == false then
-						HandleUFAuraBorder(button,false)
-					end
+				if not raidFrames[button:GetParent():GetParent().unitframeType] then
+					HandleUFAuraBorder(button,false)
 				end
 			else
 				HandleUFAuraBorder(button,false)
