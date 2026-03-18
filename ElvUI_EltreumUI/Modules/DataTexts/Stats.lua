1097820e727bfea1bb8f78428c12be533d7e0aa1
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-03-18 19:56:07 -0300

    fix the check

--- ElvUI_EltreumUI/Modules/DataTexts/Stats.lua
+++ ElvUI_EltreumUI/Modules/DataTexts/Stats.lua
@@ -342,7 +342,7 @@ local function EltruismStatsDatatext2(dt)
 		local tspellhit = HIT..": "..ElvUI[1].media.hexvaluecolor..string.format("%.1f%%", GetCombatRatingBonus(8)).."|r" --GetCombatRatingBonus(CR_HIT_SPELL)
 		local tmeleehit = HIT..": "..ElvUI[1].media.hexvaluecolor..string.format("%.1f%%", GetCombatRatingBonus(6)).."|r" --GetCombatRatingBonus(CR_HIT_MELEE)
 		local trangedhit = HIT..": "..ElvUI[1].media.hexvaluecolor..string.format("%.1f%%", GetCombatRatingBonus(7)).."|r" --GetCombatRatingBonus(CR_HIT_RANGED)
-		if E.Classic or E.TBC or E.Wrath then
+		if E.Classic then
 			tspellhit = HIT..": "..ElvUI[1].media.hexvaluecolor..string.format("%.1f%%", (GetSpellHitModifier())).."|r" --GetCombatRatingBonus(CR_HIT_SPELL)
 			tmeleehit = HIT..": "..ElvUI[1].media.hexvaluecolor..string.format("%.1f%%", GetHitModifier()).."|r" --GetCombatRatingBonus(CR_HIT_MELEE)
 			trangedhit = tmeleehit
