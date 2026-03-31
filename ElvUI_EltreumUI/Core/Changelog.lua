4526d4b3f54bd13330ef19cfbd81c985bfdc9081
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-03-31 20:02:11 -0300

    move changelog

--- ElvUI_EltreumUI/Core/Changelog.lua
+++ ElvUI_EltreumUI/Core/Changelog.lua
@@ -14,37 +14,23 @@ function ElvUI_EltreumUI:Changelog()
 	ElvUI_EltreumUI.Options.args.changelog.args.added = E.Libs.ACH:Group(E:TextGradient("Added", 0.50, 0.70, 1, 0.67, 0.95, 1), nil, 3)
 	ElvUI_EltreumUI.Options.args.changelog.args.added.inline = true
 	ElvUI_EltreumUI.Options.args.changelog.args.added.args.description = E.Libs.ACH:Description([[
-Added an option to use custom gradient colors in the Character Panel skin
-Added an option to disable skinning the Vehicle Leave Button
 ]], 3, "small", nil, nil, nil, nil, "full")
 
 	--updated
 	ElvUI_EltreumUI.Options.args.changelog.args.updated = E.Libs.ACH:Group(E:TextGradient("Updated", 0.50, 0.70, 1, 0.67, 0.95, 1), nil, 3)
 	ElvUI_EltreumUI.Options.args.changelog.args.updated.inline = true
 	ElvUI_EltreumUI.Options.args.changelog.args.updated.args.description = E.Libs.ACH:Description([[
-Updated [name:eltruism:gradient] to handle args ([name:eltruism:gradient{1}] for example) and use that to disable reversing target color
-Updated TBC cooking bag icon due to a change in its ID, thank you shadowtoots
-Updated the handling of quests in Arenas to try to avoid the spamming event
-Updated TBC's profession bag icons due to a change in how they work
-Updated Enchanting skin in TBC due to a change in the global value
-Updated thin aura bars to once again trim icon and use shadows
-Updated tags to better handle unit being connected or ghost
-Updated Combo Borders to rename it to Classbar Borders
-Updated tags to protect against some secrets
---]], 5, "small", nil, nil, nil, nil, "full")
+Updated Character Panel skin in Classic, TBC and Wrath to allow resizing in combat again
+Updated Damage Meter skin to avoid skinning in PVP areas
+]], 5, "small", nil, nil, nil, nil, "full")
 
 	--fixed
 	ElvUI_EltreumUI.Options.args.changelog.args.fixed = E.Libs.ACH:Group(E:TextGradient("Fixed", 0.50, 0.70, 1, 0.67, 0.95, 1), nil, 4)
 	ElvUI_EltreumUI.Options.args.changelog.args.fixed.inline = true
 	ElvUI_EltreumUI.Options.args.changelog.args.fixed.args.description = E.Libs.ACH:Description([[
-Fixed an issue with the profession icons where they couldn't be clicked in some game versions
-Fixed an issue where Spell Hit was not being displayed correctly in TBC
-Fixed an issue with gradient power due to some values now being secret
-Fixed Details custom gradient colors due to an update in Details
-Fixed an issue where custom textures were not applying correctly
-Fixed an issue with borders when a unit's reaction could be nil
-Fixed an issuew with the missing Level Up toast in TBC
-Fixed some issues with the Socket Gems in TBC
-Fixed an issue with the Quest Skin in Retail
+Fixed an issue with the Character Panel shadow in TBC
+Fixed an issue with the Character Panel Skin in PVP
+Fixed an issue related to secrets on the tooltip
+Fixed issues with tags due to secrets
 ]], 7, "small", nil, nil, nil, nil, "full")
 end
