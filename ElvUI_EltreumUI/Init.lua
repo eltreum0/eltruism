6419b526c524012fc01ef69445d0d6e45c9de9e8
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-04-01 08:33:54 -0300

    fix error in classic

--- ElvUI_EltreumUI/Init.lua
+++ ElvUI_EltreumUI/Init.lua
@@ -318,7 +318,9 @@ function ElvUI_EltreumUI:ZONE_CHANGED_INDOORS()
 	ElvUI_EltreumUI:FriendlyNameplates()
 	ElvUI_EltreumUI:BattleRes()
 	ElvUI_EltreumUI:ArenaQuest()
-	ElvUI_EltreumUI:DamageMeterZoneCheck()
+	if E.Retail then
+		ElvUI_EltreumUI:DamageMeterZoneCheck()
+	end
 	if E.db.ElvUI_EltreumUI.otherstuff.minimapcardinaldirections.rotate then
 		ElvUI_EltreumUI:MinimapCardinalDirectionsRotateInstance()
 	end
