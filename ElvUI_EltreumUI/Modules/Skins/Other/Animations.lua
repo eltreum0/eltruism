33461049d1f7f7b7fddb2db58a86aca7243c7780
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-08-19 13:06:27 -0300

    update to use only one frame

--- ElvUI_EltreumUI/Modules/Skins/Other/Animations.lua
+++ ElvUI_EltreumUI/Modules/Skins/Other/Animations.lua
@@ -5,7 +5,6 @@ local WorldFrame = _G.WorldFrame
 local UIParent = _G.UIParent
 local IsInInstance = _G.IsInInstance
 local select = _G.select
-local UnitAura = _G.C_UnitAuras and _G.C_UnitAuras.GetAuraDataByIndex or _G.UnitAura
 local IsEncounterSuppressingRelease = _G.IsEncounterSuppressingRelease
 local ObjectiveTrackerFrame = _G.ObjectiveTrackerFrame
 local Minimap = _G.Minimap
@@ -14,238 +13,204 @@ local UIFrameFadeIn = _G.UIFrameFadeIn
 local UIFrameFadeOut = _G.UIFrameFadeOut
 
 --Dark Souls Death, my first weakaura adapted
-local darksouls = CreateFrame("FRAME", nil, WorldFrame)
-local darksoulstex = darksouls:CreateTexture()
-darksouls:SetParent(WorldFrame)
-darksoulstex:SetParent(darksouls)
-darksouls:Hide()
-local darksoulsbar = darksouls:CreateTexture()
-darksouls.Text = darksouls:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-darksouls.Text:SetParent(darksouls)
-darksouls.scaler = darksouls.Text:CreateAnimationGroup()
-local scaleOut = darksouls.scaler:CreateAnimation("Scale")
-local moveOut = darksouls.scaler:CreateAnimation("Translation")
+local deathFrame = CreateFrame("FRAME", "EltruismDeathFrame", WorldFrame)
+deathFrame:SetParent(WorldFrame)
+deathFrame:Hide()
+local deathBg = deathFrame:CreateTexture()
+deathBg:SetAllPoints(deathFrame)
 
--- GTA version
-local gta = CreateFrame("FRAME", nil, WorldFrame)
-local gtatex = gta:CreateTexture()
-gta:SetParent(WorldFrame)
-gta:Hide()
-local gtatext = CreateFrame("FRAME", nil, WorldFrame)
-gtatext:SetParent(WorldFrame)
-gtatext:Hide()
-local gtabanner = gtatext:CreateTexture()
-gta.Text = gtatext:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+local deathContent = CreateFrame("FRAME", nil, deathFrame)
+deathContent:SetAllPoints(deathFrame)
+local deathBanner = deathContent:CreateTexture()
 
+deathFrame.Text = deathContent:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+deathFrame.scaler = deathFrame.Text:CreateAnimationGroup()
+
+local scaleOut = deathFrame.scaler:CreateAnimation("Scale")
+local moveOut = deathFrame.scaler:CreateAnimation("Translation")
 local isAnimationPlaying = false
 
-local function makeSpellId()
-	local spellId = {}
-	if not E:NotSecretValue(UnitAura("player", 1)) then
-		return
-	else
-		for i= 1, 10 do
-			local id = select(10, UnitAura("player", i))
-			spellId[id] = true
+local IGNORED_SPELLS = {
+	[20711] = true, -- Spirit of Redemption
+	[5384] = true, -- Feign Death
+}
+local function HasIgnoredDeathSpell()
+	for i = 1, 40 do
+		local spellId
+		if _G.C_UnitAuras and _G.C_UnitAuras.GetAuraDataByIndex then
+			local data = _G.C_UnitAuras.GetAuraDataByIndex("player", i)
+			if not data then break end
+			spellId = data.spellId
+		else
+			spellId = select(10, _G.UnitAura("player", i))
+			if not spellId then break end
 		end
-		return spellId
+
+		if IGNORED_SPELLS[spellId] then
+			return true
+		end
 	end
+	return false
 end
 
 function ElvUI_EltreumUI.PlayerDeathAnimation()
+	local db = E.db.ElvUI_EltreumUI.skins
+	local isDarkSouls = db.playerdeath or db.playerdeathcustom
+	local isGTA = db.playerdeathgta
 
-	--Dark Souls
-	if E.db.ElvUI_EltreumUI.skins.playerdeath or E.db.ElvUI_EltreumUI.skins.playerdeathcustom then
-		darksouls:RegisterEvent("PLAYER_DEAD")
-		--darksouls:RegisterEvent("PLAYER_STARTED_MOVING")
-		darksouls:SetScript("OnEvent", function()
-			if not isAnimationPlaying then
-				local _, instanceType = IsInInstance()
-				local spellId = makeSpellId()
-				if not IsEncounterSuppressingRelease() and instanceType == "none" and (not spellId[20711]) and (not spellId[5384]) then
-					E:Delay(0.08, function()
-						if _G["ElvUF_Player"] and _G["ElvUF_Player"].Portrait3D then
-							_G["ElvUF_Player"].Portrait3D:Hide()
-						end
-						if _G["EltruismPlayerEffect"] then
-							_G["EltruismPlayerEffect"]:SetAlpha(0)
-						end
-						if _G["EltruismPlayerPowerBarEffect"] then
-							_G["EltruismPlayerPowerBarEffect"]:SetAlpha(0)
-						end
-					end)
-
-					local x, y = E.UIParent:GetSize()
-					darksouls:SetSize(x,y)
-					darksoulstex:SetTexture(186185)
-					darksoulstex:SetVertexColor(0, 0, 0)
-					darksoulstex:SetAlpha(0.60)
-					darksoulstex:SetAllPoints(darksouls)
-					darksouls:SetPoint("Center", E.UIParent)
-					darksouls.Text:SetJustifyH("CENTER")
-					darksouls.Text:SetJustifyV("MIDDLE")
-					darksouls.Text:SetPoint("CENTER", E.UIParent, "CENTER", 0, 0)
-					darksouls.Text:SetTextColor(1, 0.1803921568627451, 0.196078431372549, 0.5)
-					local fontsize = 240
-					if E.db.ElvUI_EltreumUI.skins.playerdeathcustom then
-						darksouls.Text:SetText(E.db.ElvUI_EltreumUI.skins.playerdeathcustomtext)
-						darksouls.Text:SetFont("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Fonts\\OptimusPrinceps.TTF", 64, "OUTLINE")
-					else
-						darksouls.Text:SetText("YOU DIED")
-						darksouls.Text:SetFont("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Fonts\\OptimusPrinceps.TTF", fontsize, "OUTLINE")
-					end
-					darksoulsbar:SetSize(x,200)
-					darksoulsbar:SetTexture("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Textures\\YouDied.TGA")
-					darksoulsbar:SetAlpha(0.60)
-					darksoulsbar:SetPoint("CENTER", WorldFrame)
-					scaleOut:SetOrder(1)
-					scaleOut:SetDuration(5)
-					scaleOut:SetStartDelay(0.1)
-					scaleOut:SetSmoothing("OUT")
-					scaleOut:SetScaleFrom(1, 1)
-					scaleOut:SetScaleTo(2, 2)
-					moveOut:SetOrder(1)
-					moveOut:SetDuration(5)
-					moveOut:SetSmoothing("OUT")
-					moveOut:SetStartDelay(0.1)
-					if E.db.ElvUI_EltreumUI.skins.playerdeathcustom then
-						local textwidth = darksouls.Text:GetStringWidth()
-						moveOut:SetOffset(-textwidth/2, -fontsize/4)
-					else
-						moveOut:SetOffset(-fontsize*1.25, fontsize/8)
-					end
-
-					UIParent:SetAlpha(0)
-					if E.Retail then
-						ObjectiveTrackerFrame:SetAlpha(0)
-					end
-					Minimap:Hide()
-					Minimap:SetAlpha(0)
-					if not E.ClassicHC then
-						PlaySoundFile("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\sound\\You_Died.ogg" , "Master")
-					end
-					UIFrameFadeIn(darksouls, 1, 0, 1)
-					darksouls.scaler:Play()
-					isAnimationPlaying = true
-					E:Delay(4, function() UIFrameFadeOut(darksouls, 1, 1, 0) end)
-					E:Delay(5, function()
-						if _G["ElvUF_Player"].Portrait3D then
-							_G["ElvUF_Player"].Portrait3D:Show()
-						end
-						if _G["EltruismPlayerEffect"] then
-							_G["EltruismPlayerEffect"]:SetAlpha(E.db.ElvUI_EltreumUI.unitframes.models.ufalpha)
-						end
-						if _G["EltruismPlayerPowerBarEffect"] then
-							_G["EltruismPlayerPowerBarEffect"]:SetAlpha(0.4)
-						end
-						UIParent:SetAlpha(1)
-						if E.Retail then
-							ObjectiveTrackerFrame:SetAlpha(1)
-						end
-						Minimap:SetAlpha(1)
-						Minimap:Show()
-						isAnimationPlaying = false
-					end)
-				end
-			end
-		end)
+	if isDarkSouls or isGTA then
+		deathFrame:RegisterEvent("PLAYER_DEAD")
 	else
-		darksouls:UnregisterAllEvents()
+		deathFrame:UnregisterEvent("PLAYER_DEAD")
+		return
 	end
 
-	--Grand Theft Auto (5/Online)
-	if E.db.ElvUI_EltreumUI.skins.playerdeathgta then
-		gta:RegisterEvent("PLAYER_DEAD")
-		--gta:RegisterEvent("PLAYER_STARTED_MOVING")
-		gta:SetScript("OnEvent", function()
-			if not isAnimationPlaying then
-				local _, instanceType = IsInInstance()
-				local spellId = makeSpellId()
-				if not IsEncounterSuppressingRelease() and instanceType == "none" and (not spellId[20711]) and (not spellId[5384]) then
+	deathFrame:SetScript("OnEvent", function()
+		if isAnimationPlaying then return end
 
-					E:Delay(0.08, function()
-						if _G["ElvUF_Player"] and _G["ElvUF_Player"].Portrait3D then
-							_G["ElvUF_Player"].Portrait3D:Hide()
-						end
-						if _G["EltruismPlayerEffect"] then
-							_G["EltruismPlayerEffect"]:SetAlpha(0)
-						end
-						if _G["EltruismPlayerPowerBarEffect"] then
-							_G["EltruismPlayerPowerBarEffect"]:SetAlpha(0)
-						end
-					end)
+		local _, instanceType = IsInInstance()
 
-					local x, y = E.UIParent:GetSize()
-					gta:SetSize(x,y)
-					gta:SetPoint("Center", E.UIParent)
+		if IsEncounterSuppressingRelease() or instanceType ~= "none" or HasIgnoredDeathSpell() then
+			return
+		end
 
-					gtatex:SetTexture("Interface\\AddOns\\ElvUI\\Game\\Shared\\Media\\Textures\\White8x8.tga")
-					gtatex:SetVertexColor(0.03, 0.03, 0)
-					gtatex:SetAlpha(0.40)
-					gtatex:SetAllPoints(gta)
-					gtatex:SetDrawLayer("BACKGROUND")
+		isAnimationPlaying = true
 
-					gta.Text:SetJustifyH("CENTER")
-					gta.Text:SetJustifyV("MIDDLE")
-					gta.Text:SetTextColor(1, 0.1, 0.10, 1)
-					gta.Text:SetFont("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Fonts\\pricedown.TTF", 200, "OUTLINE")
-					gta.Text:SetShadowColor(0, 0, 0, 1)
-					gta.Text:SetShadowOffset(4, -4)
-					gta.Text:SetText("WASTED")
-					gta.Text:SetDrawLayer("OVERLAY")
-					if E.ClassicHC then
-						gtabanner:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Wasted.tga")
-					end
-					gtabanner:SetAlpha(0.60)
-					gtabanner:SetSize(x, 300)
-
-					--gta.Text:SetPoint("CENTER", WorldFrame, "CENTER", math.random(-200,200), math.random(-200,200))
-					gta.Text:SetPoint("CENTER", WorldFrame, "CENTER", 0, 0)
-					gtabanner:SetPoint("CENTER", gta.Text)
-
-					UIParent:SetAlpha(0)
-					if E.Retail then
-						ObjectiveTrackerFrame:SetAlpha(0)
-					end
-					Minimap:Hide()
-					Minimap:SetAlpha(0)
-					if E.ClassicHC then
-						PlaySoundFile("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\sound\\wasted.ogg" , "Master")
-					end
-					UIFrameFadeIn(gta, 1, 0, 1)
-					isAnimationPlaying = true
-
-					E:Delay(2.6, function()
-						UIFrameFadeIn(gtatext, 0, 0, 1)
-					end)
-
-					E:Delay(5, function()
-						UIFrameFadeOut(gta, 1, 1, 0)
-						UIFrameFadeOut(gtatext, 1, 1, 0)
-					end)
-
-					E:Delay(6, function()
-						if _G["ElvUF_Player"].Portrait3D then
-							_G["ElvUF_Player"].Portrait3D:Show()
-						end
-						if _G["EltruismPlayerEffect"] then
-							_G["EltruismPlayerEffect"]:SetAlpha(E.db.ElvUI_EltreumUI.unitframes.models.ufalpha)
-						end
-						if _G["EltruismPlayerPowerBarEffect"] then
-							_G["EltruismPlayerPowerBarEffect"]:SetAlpha(0.4)
-						end
-						UIParent:SetAlpha(1)
-						if E.Retail then
-							ObjectiveTrackerFrame:SetAlpha(1)
-						end
-						Minimap:SetAlpha(1)
-						Minimap:Show()
-						isAnimationPlaying = false
-					end)
-				end
+		E:Delay(0.08, function()
+			if _G["ElvUF_Player"] and _G["ElvUF_Player"].Portrait3D then
+				_G["ElvUF_Player"].Portrait3D:Hide()
+			end
+			if _G["EltruismPlayerEffect"] then
+				_G["EltruismPlayerEffect"]:SetAlpha(0)
+			end
+			if _G["EltruismPlayerPowerBarEffect"] then
+				_G["EltruismPlayerPowerBarEffect"]:SetAlpha(0)
 			end
 		end)
-	else
-		gta:UnregisterAllEvents()
-	end
+
+		local x, y = E.UIParent:GetSize()
+		deathFrame:SetSize(x, y)
+		deathFrame.Text:ClearAllPoints()
+		deathFrame.Text:SetPoint("CENTER", E.UIParent, "CENTER", 0, 0)
+		deathFrame.Text:SetJustifyH("CENTER")
+		deathFrame.Text:SetJustifyV("MIDDLE")
+		deathBanner:ClearAllPoints()
+
+		UIParent:SetAlpha(0)
+		if E.Retail then
+			ObjectiveTrackerFrame:SetAlpha(0)
+		end
+		Minimap:Hide()
+		Minimap:SetAlpha(0)
+
+		deathContent:SetAlpha(1)
+
+		local fadeOutDelay = 4
+		local uiRestoreDelay = 5
+
+		if isDarkSouls then
+			deathBg:SetTexture(186185)
+			deathBg:SetVertexColor(0, 0, 0)
+			deathBg:SetAlpha(0.60)
+			deathBg:SetDrawLayer("BACKGROUND")
+
+			deathFrame.Text:SetTextColor(1, 0.18, 0.20, 0.5)
+			deathFrame.Text:SetShadowColor(0, 0, 0, 0)
+
+			local fontsize = 240
+			if db.playerdeathcustom then
+				deathFrame.Text:SetText(db.playerdeathcustomtext)
+				deathFrame.Text:SetFont("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Fonts\\OptimusPrinceps.TTF", 64, "OUTLINE")
+				local textwidth = deathFrame.Text:GetStringWidth()
+				moveOut:SetOffset(-textwidth/2, -fontsize/4)
+			else
+				deathFrame.Text:SetText("YOU DIED")
+				deathFrame.Text:SetFont("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Fonts\\OptimusPrinceps.TTF", fontsize, "OUTLINE")
+				moveOut:SetOffset(-fontsize*1.25, fontsize/8)
+			end
+
+			deathBanner:SetSize(x, 200)
+			deathBanner:SetTexture("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Textures\\YouDied.TGA")
+			deathBanner:SetAlpha(0.60)
+			deathBanner:SetPoint("CENTER", WorldFrame)
+
+			scaleOut:SetOrder(1)
+			scaleOut:SetDuration(5)
+			scaleOut:SetStartDelay(0.1)
+			scaleOut:SetSmoothing("OUT")
+			scaleOut:SetScaleFrom(1, 1)
+			scaleOut:SetScaleTo(2, 2)
+			moveOut:SetOrder(1)
+			moveOut:SetDuration(5)
+			moveOut:SetSmoothing("OUT")
+			moveOut:SetStartDelay(0.1)
+
+			if not E.ClassicHC then
+				PlaySoundFile("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\sound\\You_Died.ogg", "Master")
+			end
+
+			UIFrameFadeIn(deathFrame, 1, 0, 1)
+			deathFrame.scaler:Play()
+		elseif isGTA then
+			fadeOutDelay = 5
+			uiRestoreDelay = 6
+
+			deathBg:SetTexture("Interface\\AddOns\\ElvUI\\Game\\Shared\\Media\\Textures\\White8x8.tga")
+			deathBg:SetVertexColor(0.03, 0.03, 0)
+			deathBg:SetAlpha(0.40)
+			deathBg:SetDrawLayer("BACKGROUND")
+
+			deathFrame.Text:SetTextColor(1, 0.1, 0.10, 1)
+			deathFrame.Text:SetFont("Interface\\Addons\\ElvUI_EltreumUI\\Media\\Fonts\\pricedown.TTF", 200, "OUTLINE")
+			deathFrame.Text:SetShadowColor(0, 0, 0, 1)
+			deathFrame.Text:SetShadowOffset(4, -4)
+			deathFrame.Text:SetText("WASTED")
+
+			if E.ClassicHC then
+				deathBanner:SetTexture("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\Textures\\Wasted.tga")
+			else
+				deathBanner:SetTexture(nil)
+			end
+
+			deathBanner:SetAlpha(0.60)
+			deathBanner:SetSize(x, 300)
+			deathBanner:SetPoint("CENTER", deathFrame.Text)
+
+			deathContent:SetAlpha(0)
+			UIFrameFadeIn(deathFrame, 1, 0, 1)
+
+			E:Delay(2.6, function()
+				UIFrameFadeIn(deathContent, 0, 0, 1)
+			end)
+
+			if E.ClassicHC then
+				PlaySoundFile("Interface\\AddOns\\ElvUI_EltreumUI\\Media\\sound\\wasted.ogg", "Master")
+			end
+		end
+
+		E:Delay(fadeOutDelay, function()
+			UIFrameFadeOut(deathFrame, 1, 1, 0)
+			if isGTA then
+				UIFrameFadeOut(deathContent, 1, 1, 0)
+			end
+		end)
+
+		E:Delay(uiRestoreDelay, function()
+			if _G["ElvUF_Player"] and _G["ElvUF_Player"].Portrait3D then
+				_G["ElvUF_Player"].Portrait3D:Show()
+			end
+			if _G["EltruismPlayerEffect"] then
+				_G["EltruismPlayerEffect"]:SetAlpha(E.db.ElvUI_EltreumUI.unitframes.models.ufalpha)
+			end
+			if _G["EltruismPlayerPowerBarEffect"] then
+				_G["EltruismPlayerPowerBarEffect"]:SetAlpha(0.4)
+			end
+			UIParent:SetAlpha(1)
+			if E.Retail then ObjectiveTrackerFrame:SetAlpha(1) end
+			Minimap:SetAlpha(1)
+			Minimap:Show()
+
+			isAnimationPlaying = false
+		end)
+	end)
 end
