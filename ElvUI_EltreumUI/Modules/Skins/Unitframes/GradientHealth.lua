907bf770ffeb8c107cfe553364e8e6810313781b
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-08-20 14:58:23 -0300

    try to use health in dark mode

--- ElvUI_EltreumUI/Modules/Skins/Unitframes/GradientHealth.lua
+++ ElvUI_EltreumUI/Modules/Skins/Unitframes/GradientHealth.lua
@@ -199,9 +199,16 @@ function ElvUI_EltreumUI:ApplyUnitGradient(unit,name,unitDB,noOrientation)
 					unitframe.Health.backdropTex:SetTexture(E.LSM:Fetch("statusbar", E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdroptexture))
 				end
 				if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdroptexturestaticsize then
-					unitframe.Health.backdropTex:SetAllPoints(unitframe.Health)
-					if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.fliptargetbackdrop and name == 'Target' then
-						unitframe.Health.backdropTex:SetTexCoord(1, 0, 0, 1)
+					if E.db.ElvUI_EltreumUI.unitframes.lightmode then
+						unitframe.Health.backdropTex:SetAllPoints(unitframe.Health)
+						if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.fliptargetbackdrop and name == 'Target' then
+							unitframe.Health.backdropTex:SetTexCoord(1, 0, 0, 1)
+						end
+					else
+						unitframe.Health:SetAllPoints(unitframe)
+						if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.fliptargetbackdrop and name == 'Target' then
+							unitframe.Health:SetTexCoord(1, 0, 0, 1)
+						end
 					end
 				end
 			end
@@ -320,7 +327,11 @@ function ElvUI_EltreumUI:ApplyGroupGradient(button,noOrientation)
 				end
 			end
 			if E.db.ElvUI_EltreumUI.unitframes.ufcustomtexture.backdroptexturestaticsize then
-				button.Health.backdropTex:SetAllPoints(button.Health)
+				if E.db.ElvUI_EltreumUI.unitframes.lightmode then
+					button.Health.backdropTex:SetAllPoints(button.Health)
+				else
+					button.Health:SetAllPoints(button)
+				end
 			end
 		end
 
