fb1d031b56830541c3a7c2e32a1c2d4984bc0e4b
Eltreum <30246110+eltreum0@users.noreply.github.com>
2026-08-30 23:21:06 -0300

    cl

--- ElvUI_EltreumUI/Core/Changelog.lua
+++ ElvUI_EltreumUI/Core/Changelog.lua
@@ -32,6 +32,7 @@ Updated Edit Mode Layout
 	ElvUI_EltreumUI.Options.args.changelog.args.fixed.args.description = E.Libs.ACH:Description([[
 Fixed an issue where in Burning Crusade Classic the Talent frames weren't scaling properly
 Fixed an issue (script ran too long) with the ElvUI Skin and Ace3 skins
+Fixed an issue with the Death Animation in Retail
 Fixed an issue with Actionbar glows in Classic
 Fixed an issue with Shadows and Secret Values
 ]], 7, "small", nil, nil, nil, nil, "full")
--- Latest.md
+++ Latest.md
@@ -6,5 +6,6 @@ Updated Death Animations to better handle changes in Retail
 Updated Edit Mode Layout  
 Fixed an issue where in Burning Crusade Classic the Talent frames weren't scaling properly  
 Fixed an issue (script ran too long) with the ElvUI Skin and Ace3 skins  
+Fixed an issue with the Death Animation in Retail  
 Fixed an issue with Actionbar glows in Classic  
 Fixed an issue with Shadows and Secret Values
